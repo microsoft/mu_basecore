01c1ba4dcbac52976bec4e3acc548ca473afbfd8
Sean Brogan <sean.brogan@microsoft.com>
2017-04-21 17:57:10 -0700

    Fix tests to report common name based on module name.  Change names so it follows standards. Fix capsule tests so memory is initialized correctly for sg list

--- MsUnitTestPkg/Sample/SampleUnitTestApp/SampleUnitTestApp.c
+++ MsUnitTestPkg/Sample/SampleUnitTestApp/SampleUnitTestApp.c
@@ -20,6 +20,7 @@ SPDX-License-Identifier: BSD-2-Clause-Patent
 **/
 #include <Uefi.h>
 #include <Library/UefiLib.h>
+#include <Library/PrintLib.h>
 #include <Library/DebugLib.h>
 #include <UnitTestTypes.h>
 #include <Library/UnitTestLib.h>
@@ -27,7 +28,6 @@ SPDX-License-Identifier: BSD-2-Clause-Patent
 
 
 #define UNIT_TEST_APP_NAME        L"Sample Unit Test Library Application"
-#define UNIT_TEST_APP_SHORT_NAME  L"Sample_Unit_Test_Lib_App"
 #define UNIT_TEST_APP_VERSION     L"0.1"
 
 
@@ -158,13 +158,16 @@ SampleUnitTestApp (
   EFI_STATUS                Status;
   UNIT_TEST_FRAMEWORK       *Fw = NULL;
   UNIT_TEST_SUITE           *SimpleMathTests, *GlobalVarTests;
+  CHAR16  ShortName[100];
+  ShortName[0] = L'\0';
 
+  UnicodeSPrint(&ShortName[0], sizeof(ShortName), L"%a", gEfiCallerBaseName); 
   DEBUG(( DEBUG_INFO, "%s v%s\n", UNIT_TEST_APP_NAME, UNIT_TEST_APP_VERSION ));
 
   //
   // Start setting up the test framework for running the tests.
   //
-  Status = InitUnitTestFramework( &Fw, UNIT_TEST_APP_NAME, UNIT_TEST_APP_SHORT_NAME, UNIT_TEST_APP_VERSION );
+  Status = InitUnitTestFramework( &Fw, UNIT_TEST_APP_NAME, ShortName, UNIT_TEST_APP_VERSION );
   if (EFI_ERROR( Status ))
   {
     DEBUG((DEBUG_ERROR, "Failed in InitUnitTestFramework. Status = %r\n", Status));
