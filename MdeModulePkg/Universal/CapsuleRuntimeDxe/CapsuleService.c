3b5ad6bea8f792f8902872620057d42b8a2671e4
Aaron Antone <aanton@microsoft.com>
2017-12-19 23:08:57 +0000

    Implemented SecurityLockLib to log security milestones.
    Related work items: #14103105, #15334968

--- MdeModulePkg/Universal/CapsuleRuntimeDxe/CapsuleService.c
+++ MdeModulePkg/Universal/CapsuleRuntimeDxe/CapsuleService.c
@@ -14,6 +14,7 @@ SPDX-License-Identifier: BSD-2-Clause-Patent
 // MS_CHANGE_161994
 #include <Guid/EventGroup.h>
 // END_MS_CHANGE_161994
+#include <Library/SecurityLockAuditLib.h> //MSCHANGE
 
 #include <Library/ResetUtilityLib.h>                  // MS_CHANGE_250018 - ResetSystem refactoring.
 
@@ -383,6 +384,7 @@ IN      VOID                      *Context
 )
 {
     mAfterLocked = TRUE;
+    SECURITY_LOCK_REPORT_EVENT("Lock Capsule Interface", SOFTWARE_LOCK); //MSCHANGE
     DEBUG((DEBUG_INFO, "Capsule Interface Locked!!\nMS_CHANGE_161994\n"));
 }
 // END MS_CHANGE_161994
