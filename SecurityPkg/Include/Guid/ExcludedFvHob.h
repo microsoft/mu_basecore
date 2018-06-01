4ea603700b064f008dd2bd6fadf60d46d799a053
Sean Brogan <sean.brogan@microsoft.com>
2018-06-01 00:21:35 +0000

    SecurityPkg: Add support for Excluded Fvs in Dxe Tpm2 MeasuredBootLib
    Add support for Excluded Fvs in Dxe Tpm2 MeasuredBootLib
    
    Signed-off-by: Sean Brogan <sean.brogan@microsoft.com>
    
    cherry-pick from bb19263869

---
+++ SecurityPkg/Include/Guid/ExcludedFvHob.h
@@ -0,0 +1,16 @@
+/** @file
+  Defines the HOB GUID used to pass all excluded FVs to DXE Driver.
+
+  Copyright (c) Microsoft Corporation.
+  SPDX-License-Identifier: BSD-2-Clause-Patent
+
+**/
+
+#pragma once
+
+extern EFI_GUID  gExcludedFvHobGuid;
+
+typedef struct {
+  UINT32                                                  Num;
+  EFI_PEI_FIRMWARE_VOLUME_INFO_MEASUREMENT_EXCLUDED_FV    ExcludedFvs[0];
+} EXCLUDED_HOB_DATA;
