a7e7965d9a637bb6c619b007adf94e3769b0f09a
Bret Barkelew <brbarkel@microsoft.com>
2018-06-21 00:16:52 +0000

    Fixed issue where PXE boot may cause system hangs.
    Fixed issue where PXE boot may cause system hangs.  PXE loader hooks interrupts but doesn’t restore them on return and thus causing hangs.  We can workaround it in UEFI by restoring these interrupts after boot loader returns.
    
    Cherry picked from !1989140
    
    Related work items: #17622676

--- MdeModulePkg/Include/Library/DebugAgentLib.h
+++ MdeModulePkg/Include/Library/DebugAgentLib.h
@@ -21,6 +21,7 @@ SPDX-License-Identifier: BSD-2-Clause-Patent
 #define DEBUG_AGENT_INIT_DXE_LOAD             10
 #define DEBUG_AGENT_INIT_DXE_UNLOAD           11
 #define DEBUG_AGENT_INIT_THUNK_PEI_IA32TOX64  12
+#define DEBUG_AGENT_INIT_REINITIALIZE         13      // MS_CHANGE
 
 //
 // Context for DEBUG_AGENT_INIT_POSTMEM_SEC
