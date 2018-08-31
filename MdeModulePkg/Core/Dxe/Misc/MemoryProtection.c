3b1eb5573a074e1ced76e7027e16160f5946da69
Max Knutsen <maknutse@microsoft.com>
2018-08-31 23:09:08 +0000

    Merged PR 2275624: HeapGuardDebugProtocol
    added protocol to debug heap guard memory protections
    
    Related work items: #18793312

--- MdeModulePkg/Core/Dxe/Misc/MemoryProtection.c
+++ MdeModulePkg/Core/Dxe/Misc/MemoryProtection.c
@@ -39,6 +39,7 @@ SPDX-License-Identifier: BSD-2-Clause-Patent
 
 #include <Protocol/FirmwareVolume2.h>
 #include <Protocol/SimpleFileSystem.h>
+#include <Protocol/HeapGuardDebug.h>
 
 #include "DxeMain.h"
 #include "Mem/HeapGuard.h"
@@ -69,6 +70,10 @@ UINT32   mImageProtectionPolicy;
 extern LIST_ENTRY         mGcdMemorySpaceMap;
 
 STATIC LIST_ENTRY         mProtectedImageRecordList;
+
+STATIC HEAP_GUARD_DEBUG_PROTOCOL mHeapGuardDebug = {
+  IsGuardPage
+};
 
 /**
   Sort code section in image record, based upon CodeSegmentBase from low to high.
@@ -1169,6 +1174,21 @@ CoreInitializeMemoryProtection (
                     );
     ASSERT_EFI_ERROR (Status);
   }
+
+  //
+  // MSCHANGE START
+  // Install protocol for validating Heap Guard if Heap Guard is turned on
+  //
+  if (PcdGet8(PcdHeapGuardPropertyMask)) {
+    EFI_HANDLE HgBmHandle = NULL;
+    Status = CoreInstallMultipleProtocolInterfaces (
+      &HgBmHandle,
+      &gHeapGuardDebugProtocolGuid,
+      &mHeapGuardDebug,
+      NULL);
+    DEBUG ((DEBUG_INFO, "Installed gHeapGuardDebugProtocolGuid - %r\n", Status));
+  }
+  // MSCHANGE END
 
   return ;
 }
