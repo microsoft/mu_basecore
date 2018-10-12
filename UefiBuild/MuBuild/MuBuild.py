e05360aff5d3ab1b89366122eceec65dde0cdc6a
Matthew Carlson <macarl@microsoft.com>
2018-10-12 10:04:24 -0700

    removed case insensitive dictionary

--- UefiBuild/MuBuild/MuBuild.py
+++ UefiBuild/MuBuild/MuBuild.py
@@ -75,41 +75,9 @@ def get_mu_config():
     args, sys.argv = parser.parse_known_args() 
     return args
 
-class CaseInsensitiveDict(dict):
-    @classmethod
-    def _k(cls, key):
-        return key.lower() if isinstance(key, str) else key
-
-    def __init__(self, *args, **kwargs):
-        super(CaseInsensitiveDict, self).__init__(*args, **kwargs)
-        self._convert_keys()
-    def __getitem__(self, key):
-        return super(CaseInsensitiveDict, self).__getitem__(self.__class__._k(key))
-    def __setitem__(self, key, value):
-        super(CaseInsensitiveDict, self).__setitem__(self.__class__._k(key), value)
-    def __delitem__(self, key):
-        return super(CaseInsensitiveDict, self).__delitem__(self.__class__._k(key))
-    def __contains__(self, key):
-        return super(CaseInsensitiveDict, self).__contains__(self.__class__._k(key))
-    def has_key(self, key):
-        return super(CaseInsensitiveDict, self).has_key(self.__class__._k(key))
-    def pop(self, key, *args, **kwargs):
-        return super(CaseInsensitiveDict, self).pop(self.__class__._k(key), *args, **kwargs)
-    def get(self, key, *args, **kwargs):
-        return super(CaseInsensitiveDict, self).get(self.__class__._k(key), *args, **kwargs)
-    def setdefault(self, key, *args, **kwargs):
-        return super(CaseInsensitiveDict, self).setdefault(self.__class__._k(key), *args, **kwargs)
-    def update(self, E={}, **F):
-        super(CaseInsensitiveDict, self).update(self.__class__(E))
-        super(CaseInsensitiveDict, self).update(self.__class__(**F))
-    def _convert_keys(self):
-        for k in list(self.keys()):
-            v = super(CaseInsensitiveDict, self).pop(k)
-            self.__setitem__(k, v)
-
 def merge_config(mu_config,pkg_config,descriptor={}):
     plugin_name = ""
-    config = CaseInsensitiveDict()
+    config = dict()
     if "module" in descriptor:
         plugin_name = descriptor["module"]
     if "config_name" in descriptor:
