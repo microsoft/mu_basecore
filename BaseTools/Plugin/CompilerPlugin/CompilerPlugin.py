61bed81937a724d7e5dee5e7c4a72509af3d00b4
Matthew Carlson <macarl@microsoft.com>
2018-11-29 22:38:12 +0000

    Merged PR 520: Improve CI compile plugin to capture failure messages from compiler when test fails and attach to junit test results
    Improve CI compile plugin to capture failure messages from compiler when test fails and attach to junit test results

--- BaseTools/Plugin/CompilerPlugin/CompilerPlugin.py
+++ BaseTools/Plugin/CompilerPlugin/CompilerPlugin.py
@@ -32,6 +32,7 @@ import time
 from MuEnvironment.UefiBuild import UefiBuilder
 import os
 import sys
+import re
 
 class CompilerPlugin(IMuBuildPlugin):
 
@@ -72,6 +73,23 @@ class CompilerPlugin(IMuBuildPlugin):
         #do all the steps
         ret = uefiBuilder.Go()
         if ret != 0: #failure:     
+            try:
+                #seek to the start of the output stream
+                uefiBuilder.OutputStream.seek(0,0)
+                error_exp = re.compile(r"error C(\d+):")
+                warning_exp = re.compile(r"warning C(\d+):")                
+                for line in uefiBuilder.OutputStream.readlines():
+                    
+                    match = error_exp.search(line)
+                    if match is not None:
+                        tc.LogStdError("Compile: Error: {0}".format(line))
+                    match = warning_exp.search(line)
+                    if match is not None:
+                        tc.LogStdOut("Compile: Warning: {0}".format(line))
+            #we might fail if uefiBuilder doesn't have the output stream (if we have an older mu_enviroment for whatever reason)
+            except AttributeError:
+                pass #if we do fail we can ignore it since it just means we can't put more explicit output into the xml
+            
             tc.SetFailed("Compile failed for {0}".format(packagename), "Compile_FAILED")
             tc.LogStdError("{0} Compile failed with error code {1}".format(AP_Path, ret))
             return 1
