1a883ec85d8d2f49663c76e1a1bc5068333f5508
Christopher Ruffin <v-chruff@microsoft.com>
2019-01-28 16:32:38 +0000

    Merged PR 750: fix include filename case issue

--- MdeModulePkg/Library/SecurityLockAuditDebugMessageLib/SecurityLockAuditDebugMessageLib.c
+++ MdeModulePkg/Library/SecurityLockAuditDebugMessageLib/SecurityLockAuditDebugMessageLib.c
@@ -1,11 +1,11 @@
 /** @file
-  This library implements the necessary functions 
+  This library implements the necessary functions
   to log hardware and software security locks for post-processing
 
   Copyright (c) 2018, Microsoft Corporation
 
   All rights reserved.
-  Redistribution and use in source and binary forms, with or without 
+  Redistribution and use in source and binary forms, with or without
   modification, are permitted provided that the following conditions are met:
   1. Redistributions of source code must retain the above copyright notice,
   this list of conditions and the following disclaimer.
@@ -18,15 +18,15 @@
   WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
   IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
   INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
-  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, 
+  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
   DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
   LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
-  OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF 
+  OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
   ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 **/
 
-#include <base.h>
+#include <Base.h> // MU_CHANGE
 #include <Library/SecurityLockAuditLib.h>
 #include <Library/DebugLib.h>
 
@@ -42,7 +42,7 @@ CHAR8* mLockName[] = {"SOFTWARE_LOCK",
   @param[in] Function                 Name of calling function
   @param[in] LockEventText            Debug message explaining what is locked
   @param[in] LockType                 Enumerated lock type for differentiation
-                                    
+
 **/
 VOID
 EFIAPI
