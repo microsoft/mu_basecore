34ad2e9b185a47f2cbf2fbd7238e94055d98201b
Bret Barkelew <brbarkel@microsoft.com>
2019-02-19 14:58:19 -0800

    More fixes.

--- MdeModulePkg/Bus/Pci/UhciPei/UhcPeim.c
+++ MdeModulePkg/Bus/Pci/UhciPei/UhcPeim.c
@@ -2843,7 +2843,7 @@ InitializeMemoryManagement (
   IN USB_UHC_DEV           *UhcDev
   )
 {
-  MEMORY_MANAGE_HEADER  *MemoryHeader;
+  MEMORY_MANAGE_HEADER  *MemoryHeader = NULL;
   EFI_STATUS            Status;
   UINTN                 MemPages;
 
@@ -2878,7 +2878,7 @@ UhcAllocatePool (
 {
   MEMORY_MANAGE_HEADER  *MemoryHeader;
   MEMORY_MANAGE_HEADER  *TempHeaderPtr;
-  MEMORY_MANAGE_HEADER  *NewMemoryHeader;
+  MEMORY_MANAGE_HEADER  *NewMemoryHeader = NULL;
   UINTN                 RealAllocSize;
   UINTN                 MemoryBlockSizeInPages;
   EFI_STATUS            Status;
