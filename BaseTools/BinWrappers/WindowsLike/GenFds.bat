fc10c85ff86426d162b1d894ba77be3529770c7b
Dmitry Antipov <dmanti@microsoft.com>
2019-04-03 00:36:35 +0000

    Merged PR 1065: Add quotes around PYTHON_COMMAND in WindowsLike wrappers
    Add quotes around PYTHON_COMMAND in build.bat and other bin wrappers

--- BaseTools/BinWrappers/WindowsLike/GenFds.bat
+++ BaseTools/BinWrappers/WindowsLike/GenFds.bat
@@ -1,4 +1,5 @@
 @setlocal
 @set ToolName=%~n0%
 @set PYTHONPATH=%PYTHONPATH%;%BASE_TOOLS_PATH%\Source\Python
-@%PYTHON_COMMAND% -m %ToolName%.%ToolName% %*
+REM MU_CHANGE: add quotes around %PYTHON_COMMAND% to support spaces in path
+@"%PYTHON_COMMAND%" -m %ToolName%.%ToolName% %*
--- BaseTools/BinWrappers/WindowsLike/GenerateCapsule.bat
+++ BaseTools/BinWrappers/WindowsLike/GenerateCapsule.bat
@@ -1,1 +1,2 @@
-@%PYTHON_COMMAND% %BASE_TOOLS_PATH%\Source\Python\Capsule\GenerateCapsule.py %*
+REM MU_CHANGE: add quotes around %PYTHON_COMMAND% to support spaces in path
+@"%PYTHON_COMMAND%" %BASE_TOOLS_PATH%\Source\Python\Capsule\GenerateCapsule.py %*
