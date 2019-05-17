338d7efc3dbbc2bf6bf75755d1045191a5fe9b69
Dmitry Antipov <dmanti@microsoft.com>
2019-05-17 23:43:47 +0000

    Merged PR 1189: Don't print the MU_CHANGE comments in batch wrappers to the log
    Don't print the MU_CHANGE comments in batch wrappers to the log

--- BaseTools/BinWrappers/WindowsLike/Rsa2048Sha256GenerateKeys.bat
+++ BaseTools/BinWrappers/WindowsLike/Rsa2048Sha256GenerateKeys.bat
@@ -1,2 +1,2 @@
-REM MU_CHANGE: add quotes around %PYTHON_COMMAND% to support spaces in path
+@REM MU_CHANGE: add quotes around %PYTHON_COMMAND% to support spaces in path
 @"%PYTHON_COMMAND%" %BASE_TOOLS_PATH%\Source\Python\Rsa2048Sha256Sign\Rsa2048Sha256GenerateKeys.py %*
--- BaseTools/BinWrappers/WindowsLike/build.bat
+++ BaseTools/BinWrappers/WindowsLike/build.bat
@@ -1,4 +1,4 @@
 @setlocal
 @set ToolName=%~n0%
-REM MU_CHANGE: add quotes around %PYTHON_COMMAND% to support spaces in path
+@REM MU_CHANGE: add quotes around %PYTHON_COMMAND% to support spaces in path
 @"%PYTHON_COMMAND%" %BASE_TOOLS_PATH%\Source\Python\%ToolName%\%ToolName%.py %*
