f0667ab6bd823d8bee950d1e70d364bc444a677e
Bret Barkelew <brbarkel@microsoft.com>
2019-08-04 23:14:59 -0700

    Small tweaks. Need to figure out how to import r_efi.

--- MdeModulePkg/Library/UefiVariablePolicyLibRust/UefiVariablePolicyLib.rs
+++ MdeModulePkg/Library/UefiVariablePolicyLibRust/UefiVariablePolicyLib.rs
@@ -1,3 +1,9 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std]
+
+#[no_mangle]
+#[export_name = "RegisterVariablePolicy"]
+pub extern "win64" fn register_variable_policy (
+    
+    ) -> Status {
+
 }
