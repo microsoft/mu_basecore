ecac2765d53810f8ced4e8761496383e4cfedc4c
Bret Barkelew <brbarkel@microsoft.com>
2019-08-07 17:21:41 -0700

    Moving towards something that works. Need to figure out how to import the types.

--- MdeModulePkg/Library/UefiVariablePolicyLibRust/UefiVariablePolicyLib.rs
+++ MdeModulePkg/Library/UefiVariablePolicyLibRust/UefiVariablePolicyLib.rs
@@ -1,9 +1,22 @@
 #![no_std]
+// #![no_main]
 
-#[no_mangle]
-#[export_name = "RegisterVariablePolicy"]
-pub extern "win64" fn register_variable_policy (
-    
-    ) -> Status {
+use core::panic::PanicInfo;
 
+// use r_efi::efi;
+// use r_efi::efi::{Status};
+
+// TODO: Figure out a common place for this to live. DebugLib?
+#[panic_handler]
+#[allow(clippy::empty_loop)]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
 }
+
+// #[no_mangle]
+// #[export_name = "RegisterVariablePolicy"]
+// pub extern "win64" fn register_variable_policy (
+//     int: Something    
+//     ) -> Status {
+
+// }
