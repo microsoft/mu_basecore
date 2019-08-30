b43f4fb192dd4fcb7d74640fbddeab4b2fdfbff5
Bret Barkelew <brbarkel@microsoft.com>
2019-08-30 16:02:00 -0700

    Who knows if it's busted as hell, but we're actually building with a defined structure now.

--- MdeModulePkg/Library/UefiVariablePolicyLibRust/UefiVariablePolicyLib.rs
+++ MdeModulePkg/Library/UefiVariablePolicyLibRust/UefiVariablePolicyLib.rs
@@ -1,6 +1,9 @@
+#![feature(alloc_error_handler)]
+
 #![no_std]
 
 extern crate r_efi;
+extern crate alloc;
 
 use core::panic::PanicInfo;
 
@@ -9,6 +12,11 @@ use r_efi::efi::{Status};
 use r_efi::efi::{Boolean};
 use r_efi::efi::{Guid};
 
+use core::alloc::{GlobalAlloc, Layout};
+use core::ffi::c_void;
+
+use alloc::string::String;
+
 // TODO: Figure out a common place for this to live. DebugLib?
 #[panic_handler]
 #[allow(clippy::empty_loop)]
@@ -16,19 +24,61 @@ fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
+extern "C" {
+  fn AllocatePool (Size: usize) -> *mut c_void;
+  fn AllocateZeroPool (Size: usize) -> *mut c_void;
+  fn FreePool (Buffer: *mut c_void);
+}
+
+pub struct MyAllocator;
+unsafe impl GlobalAlloc for MyAllocator {
+    unsafe fn alloc(&self, layout: Layout) -> *mut u8 {
+      let size = layout.size();
+      let align = layout.align();
+      if align > 8 {
+        return core::ptr::null_mut();
+      }
+
+      unsafe { AllocatePool (size) as *mut u8 }
+    }
+    unsafe fn dealloc(&self, ptr: *mut u8, _layout: Layout) {
+      unsafe { FreePool (ptr as *mut c_void); }
+    }
+}
+
+#[global_allocator]
+static ALLOCATOR: MyAllocator = MyAllocator;
+
+#[alloc_error_handler]
+fn alloc_error_handler(layout: core::alloc::Layout) -> !
+{
+    loop {}
+}
+
 #[repr(C)]
 #[derive(Copy, Clone, Debug)]
-pub struct VariablePolicyEntry {
-    pub version:        u32,
-    pub size:           u16,
-    pub offset_to_name: u16,
-    pub namespace:      Guid,
-    pub min_size:       u32,
-    pub max_size:       u32,
-    pub attr_must_have: u32,
-    pub attr_cant_have: u32,
+pub struct RawVariablePolicyEntry {
+    pub version:            u32,
+    pub size:               u16,
+    pub offset_to_name:     u16,
+    pub namespace:          Guid,
+    pub min_size:           u32,
+    pub max_size:           u32,
+    pub attr_must_have:     u32,
+    pub attr_cant_have:     u32,
     pub lock_policy_type:   u8,
-    pub padding_3:      [u8; 3]
+    pub padding_3:          [u8; 3]
+}
+
+struct VariablePolicyEntry {
+    version:            u32,
+    min_size:           u32,
+    max_size:           u32,
+    attr_must_have:     u32,
+    attr_cant_have:     u32,
+    lock_policy_type:   u8,
+    namespace:          Guid,
+    name:               Option<String>,
 }
 
 // TODO: Try to move this to share the r_efi::system::RuntimeServices::get_variable definition.
@@ -37,7 +87,7 @@ type EfiGetVariable = extern "win64" fn(_: *const efi::Char16, _: *const Guid, _
 #[no_mangle]
 #[export_name = "RegisterVariablePolicy"]
 pub extern "win64" fn register_variable_policy (
-    new_policy: *const VariablePolicyEntry
+    new_policy: *const RawVariablePolicyEntry
     ) -> Status {
     Status::SUCCESS
 }
