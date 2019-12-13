7e22d73e9b04a30241d0887afd5759e80ae6e3c0
wenbhou <58606445+wenbhou@users.noreply.github.com>
2019-12-12 17:32:11 -0800

    TCBZ2419: Add PcdProgressBarBackgroundColor to hold the background color of progress bar. (#46)

--- MdeModulePkg/Library/DisplayUpdateProgressLibGraphics/DisplayUpdateProgressLibGraphics.c
+++ MdeModulePkg/Library/DisplayUpdateProgressLibGraphics/DisplayUpdateProgressLibGraphics.c
@@ -76,10 +76,10 @@ const EFI_GRAPHICS_OUTPUT_BLT_PIXEL_UNION  mLogoDetectionColorMask = {
 //
 const EFI_GRAPHICS_OUTPUT_BLT_PIXEL_UNION  mProgressBarBackgroundColor = {
   {
-    0x80,  // Blue
-    0x80,  // Green
-    0x80,  // Red
-    0x00   // Reserved
+    ((UINT32)FixedPcdGet32 (PcdProgressBarBackgroundColor) >> 0)  & 0xFF, // Blue       // MU_CHANGE
+    ((UINT32)FixedPcdGet32 (PcdProgressBarBackgroundColor) >> 8)  & 0xFF, // Green      // MU_CHANGE
+    ((UINT32)FixedPcdGet32 (PcdProgressBarBackgroundColor) >> 16) & 0xFF, // Red        // MU_CHANGE
+    ((UINT32)FixedPcdGet32 (PcdProgressBarBackgroundColor) >> 24) & 0xFF  // Reserved   // MU_CHANGE
   }
 };
 
