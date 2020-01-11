a10e158d953fd00c1c749ccd60b88e2cdd603734
Mike Turner <miketur@microsoft.com>
2020-01-11 00:43:13 +0000

    Merged PR 1887: Add MuPreExitBootServices group event.
    Signal an event when ExitBootServices is called, but before any disabling of Boot Services features.  This event is only scheduled once.

--- MdeModulePkg/Core/Dxe/DxeMain/DxeMain.c
+++ MdeModulePkg/Core/Dxe/DxeMain/DxeMain.c
@@ -741,6 +741,16 @@ CoreExitBootServices (
 {
   EFI_STATUS                Status;
 
+  // MU_CHANGE begin
+  // NOTE: This must be before disabling the timer.
+  STATIC BOOLEAN            PreExitBootServicesSignaled = FALSE;
+
+  if (!PreExitBootServicesSignaled) {
+    CoreNotifySignalList (&gMuEventPreExitBootServicesGuid);
+    PreExitBootServicesSignaled = TRUE;
+  }
+  // MU_CHANGE end
+
   //
   // Disable Timer
   //
