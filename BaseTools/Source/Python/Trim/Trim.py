1f8900e7afc17f3a20fccc3cc3d8709ad3b0f8d5
prgeor <60914489+prgeor@users.noreply.github.com>
2020-02-17 11:38:47 +0530

    Add start and end delimiter for included ASL file (#49)

--- BaseTools/Source/Python/Trim/Trim.py
+++ BaseTools/Source/Python/Trim/Trim.py
@@ -248,6 +248,25 @@ def TrimPreprocessedVfr(Source, Target):
     except:
         EdkLogger.error("Trim", FILE_OPEN_FAILURE, ExtraData=Target)
 
+## MU_CHANGE - Create a banner for the included ASL file
+#
+# Create a banner to indicate the start and
+# end of the included ASL file. Banner looks like:-
+# 
+#   /*************************************
+#   *               @param               *
+#   *************************************/
+#
+# @param Pathname       File pathname to be included in the banner
+#
+def AddIncludeHeader(Pathname):
+    StartLine = "/*" + '*' * (len(Pathname) + 4)
+    EndLine = '*' * (len(Pathname) + 4) + "*/"
+    Banner = '\n' + StartLine
+    Banner += '\n' + ('{0}  {1}  {0}'.format('*', Pathname))
+    Banner += '\n' + EndLine + '\n'
+    return Banner
+
 ## Read the content  ASL file, including ASL included, recursively
 #
 # @param  Source            File to be read
@@ -322,7 +341,9 @@ def DoInclude(Source, Indent='', IncludePathList=[], LocalSearchPath=None, Inclu
 
             IncludedFile = IncludedFile.strip()
             IncludedFile = os.path.normpath(IncludedFile)
+            NewFileContent.append(AddIncludeHeader(IncludedFile+" --START")) #MU_CHANGE
             NewFileContent.extend(DoInclude(IncludedFile, '', IncludePathList, LocalSearchPath,IncludeFileList,filetype))
+            NewFileContent.append(AddIncludeHeader(IncludedFile+" --END")) #MU_CHANGE
             NewFileContent.append("\n")
 
     gIncludedAslFile.pop()
