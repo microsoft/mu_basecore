97a9d565be7e8c740da3206fa7f73136464055f1
Bret Barkelew <brbarkel@microsoft.com>
2020-02-27 16:46:01 -0800

    Tweak the GlobalAllocator to be the same as the VariablePolicy lib.

--- RustPkg/Library/UefiRustAllocationLib/src/lib.rs
+++ RustPkg/Library/UefiRustAllocationLib/src/lib.rs
@@ -1,4 +1,5 @@
 // Copyright (c) 2019 Intel Corporation
+// Copyright (c) Microsoft Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -12,25 +13,24 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#![feature(alloc_layout_extra)]
-#![feature(allocator_api)]
 #![feature(alloc_error_handler)]
 
 #![cfg_attr(not(test), no_std)]
 
-#![allow(unused)]
+// #![allow(unused)]
 
 extern crate uefi_rust_panic_lib;
 
-use core::alloc::{GlobalAlloc, Layout, Alloc};
-use r_efi::efi;
-use r_efi::efi::{Status};
+use core::alloc::{GlobalAlloc, Layout};
 use core::ffi::c_void;
 
-pub struct MyAllocator;
+extern "C" {
+  fn AllocatePool (Size: usize) -> *mut c_void;
+  // fn AllocateZeroPool (Size: usize) -> *mut c_void;
+  fn FreePool (Buffer: *mut c_void);
+}
 
-static mut ST : *mut efi::SystemTable = core::ptr::null_mut();
-static mut BS : *mut efi::BootServices = core::ptr::null_mut();
+pub struct MyAllocator;
 
 unsafe impl GlobalAlloc for MyAllocator {
     unsafe fn alloc(&self, layout: Layout) -> *mut u8 {
@@ -40,34 +40,20 @@ unsafe impl GlobalAlloc for MyAllocator {
         return core::ptr::null_mut();
       }
 
-      let mut address : *mut c_void = core::ptr::null_mut();
-      let status = ((*BS).allocate_pool) (
-                     efi::MemoryType::BootServicesData,
-                     size,
-                     &mut address as *mut *mut c_void
-                     );
-      if status != Status::SUCCESS {
-        return core::ptr::null_mut();
-      }
-      address as *mut u8
+      AllocatePool (size) as *mut u8
     }
     unsafe fn dealloc(&self, ptr: *mut u8, _layout: Layout) {
-      ((*BS).free_pool) (ptr as *mut c_void);
+      FreePool (ptr as *mut c_void);
     }
 }
 
+#[cfg(not(test))]
 #[global_allocator]
 static ALLOCATOR: MyAllocator = MyAllocator;
 
+#[cfg(not(test))]
 #[alloc_error_handler]
-fn alloc_error_handler(layout: core::alloc::Layout) -> !
+fn alloc_error_handler(_layout: core::alloc::Layout) -> !
 {
     loop {}
-}
-
-pub extern fn init(system_table: *mut efi::SystemTable) {
-    unsafe {
-      ST = system_table;
-      BS = (*ST).boot_services;
-    }
 }
