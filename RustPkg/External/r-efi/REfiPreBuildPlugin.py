a187d2473da8397641f7132685439ff2ecce4484
Matthew Carlson <macarl@microsoft.com>
2020-03-17 19:08:17 +0000

    Merged PR 2199: [RustPkg] updated plugin error case as well as readme
    When we fail the rust plugin, give users a link to the readme and dump the console output.

--- RustPkg/External/r-efi/REfiPreBuildPlugin.py
+++ RustPkg/External/r-efi/REfiPreBuildPlugin.py
@@ -8,9 +8,11 @@
 
 import logging
 import os
+from io import StringIO
 try:
     from edk2toolext.environment.plugintypes.uefi_build_plugin import IUefiBuildPlugin
     from edk2toollib.utility_functions import RunCmd
+    from edk2toolext import edk2_logging
 except Exception:
     pass
 
@@ -20,11 +22,19 @@ class REfiPreBuildPlugin(IUefiBuildPlugin):
         self.logger = logging.getLogger(__name__)
 
     def _do_cargo_build(self, directory, state_file):
+        edk2_logging.log_progress("Building R-EFI Rust Support package for you. This make take a second.")
         commands = ["xbuild --release --target x86_64-unknown-uefi", "xbuild --target x86_64-unknown-uefi", "xbuild --release --target i686-unknown-uefi", "xbuild --target i686-unknown-uefi"]
+        output_stream = StringIO()
         for command in commands:
-            ret = RunCmd("cargo", command, workingdir=directory)
+            ret = RunCmd("cargo", command, workingdir=directory, outstream=output_stream)
             if ret != 0:
-                self.logger.error("Failed to compile r-efi package. Is rust installed?")
+                rustpkg_path = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
+                document_path = os.path.join(rustpkg_path, "Readme.md")
+                self.logger.error(f"Failed to compile r-efi package. Is rust installed? See the document here for instructions: {document_path}")
+                output_stream.seek(0)
+                lines = output_stream.readlines()
+                for line in lines:
+                    self.logger.error(line)
                 return ret
         self.logger.info("Compiled r-efi rust package")
 
