fa0e65f65d4078afb2cabe02fdee5b06c305e015
Bret Barkelew <brbarkel@microsoft.com>
2020-03-28 22:49:33 -0700

    Add an EFI to test things with and add code to dump info.

--- MdePkg/Library/BasePeCoffLibRust/src/main.rs
+++ MdePkg/Library/BasePeCoffLibRust/src/main.rs
@@ -1,3 +1,21 @@
+use goblin::{error, Object};
+use std::path::Path;
+use std::env;
+use std::fs;
+
+fn run () -> error::Result<()> {
+    for (i, arg) in env::args().enumerate() {
+        if i == 1 {
+            let path = Path::new(arg.as_str());
+            let buffer = fs::read(path)?;
+            if let Object::PE(pe) = Object::parse(&buffer)? {
+                println!("pe: {:#?}", &pe);
+            }
+        }
+    }
+    Ok(())
+}
+
 fn main() {
-    println!("Hello, world!");
+    let result = run();
 }
