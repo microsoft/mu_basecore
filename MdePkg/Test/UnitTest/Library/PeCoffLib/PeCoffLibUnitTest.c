d1da97741bac4cae7f449ccd01d179375a7ee5b1
Bret Barkelew <brbarkel@microsoft.com>
2020-04-02 18:01:09 -0700

    Making forward progress with tests

--- MdePkg/Test/UnitTest/Library/PeCoffLib/PeCoffLibUnitTest.c
+++ MdePkg/Test/UnitTest/Library/PeCoffLib/PeCoffLibUnitTest.c
@@ -8,22 +8,96 @@
 **/
 
 #include <Uefi.h>
+#include <Library/BaseLib.h>
+#include <Library/BaseMemoryLib.h>
 #include <Library/DebugLib.h>
 #include <Library/UnitTestLib.h>
 
+#include <Library/PeCoffLib.h>
+#include <IndustryStandard/PeImage.h>
+
 #include <stdio.h>
+
+#include "RngDxe64Efi.h"
 
 #define UNIT_TEST_APP_NAME     "PeCoffLib UnitTest"
 #define UNIT_TEST_APP_VERSION  "1.0"
 
+typedef struct _TEST_FILE {
+  CONST CHAR8   *FileId;
+  // CONST CHAR8   *Filename;
+  CONST UINTN   Filesize;
+  CONST UINT8   *Filedata;
+} TEST_FILE;
+
+TEST_FILE mTestFiles[] = {
+  // {"RngDxe.efi", "X64/RngDxe.efi", 0, NULL}
+  {"RngDxe.efi", sizeof (gRngDxe64EfiBytes), gRngDxe64EfiBytes}   // Using Bin2H for now, for simplicity.
+};
+UINTN     mTestFilesCount = sizeof (mTestFiles) / sizeof (mTestFiles[0]);
 
 STATIC
-UINT8*
-InternalReadFile (
-  IN CONST CHAR8    *Filename
+TEST_FILE*
+GetTestFile (
+  IN CONST CHAR8   *FileId
   )
 {
-  return NULL;
+  TEST_FILE   *Result;
+  UINTN       Index;
+
+  Result = NULL;
+  for (Index = 0; Index < mTestFilesCount; Index++) {
+    if (AsciiStrCmp(mTestFiles[Index].FileId, FileId) == 0) {
+      Result = &mTestFiles[Index];
+      break;
+    }
+  }
+
+  return Result;
+}
+
+STATIC
+BOOLEAN
+InitTestFiles (
+  VOID
+  )
+{
+  BOOLEAN   Result;
+  UINTN     Index;
+
+  Result = TRUE;
+  for (Index = 0; Index < mTestFilesCount; Index++) {
+    if (mTestFiles[Index].Filesize == 0 && mTestFiles[Index].Filedata == NULL) {
+      // Attempt to open, allocate, and read.
+      // NOTE: Using Bin2H for now, for simplicity.
+    }
+    // else {
+    //   DEBUG ((DEBUG_VERBOSE, "%a - Found previously initialized file! '%a'\n", __FUNCTION__, mTestFiles[Index].FileId));
+    // }
+  }
+
+  return Result;
+}
+
+STATIC
+VOID
+DeinitTestFiles(
+  VOID
+  )
+{
+  UINTN     Index;
+
+  for (Index = 0; Index < mTestFilesCount; Index++) {
+    if (mTestFiles[Index].Filesize > 0) {
+      // Deallocate file contents.
+      // NOTE: Using Bin2H for now, for simplicity.
+      // FreePool (mTestFiles[Index].Filedata);
+      // mTestFiles[Index].Filesize = 0;
+      // mTestFiles[Index].Filedata = NULL;
+    }
+  }
+
+  return;
 }
 
 /**
@@ -34,10 +108,65 @@ InternalReadFile (
 STATIC
 UNIT_TEST_STATUS
 EFIAPI
-TempTest(
+GetImageInfoShouldFailOnNull (
   IN UNIT_TEST_CONTEXT  Context
   )
 {
+  UT_ASSERT_TRUE (EFI_ERROR (PeCoffLoaderGetImageInfo (NULL)));
+  return UNIT_TEST_PASSED;
+}
+
+/**
+  @retval  UNIT_TEST_PASSED             The Unit test has completed and the test
+                                        case was successful.
+  @retval  UNIT_TEST_ERROR_TEST_FAILED  A test case assertion has failed.
+**/
+STATIC
+UNIT_TEST_STATUS
+EFIAPI
+GetImageInfoShouldFailOnZeroBuffer (
+  IN UNIT_TEST_CONTEXT  Context
+  )
+{
+  PE_COFF_LOADER_IMAGE_CONTEXT    TestContext;
+  EFI_IMAGE_DOS_HEADER            DummyHeader;
+
+  ZeroMem (&TestContext, sizeof (TestContext));
+  ZeroMem (&DummyHeader, sizeof (DummyHeader));
+
+  // Use the libs read function, for consistency.
+  TestContext.ImageRead = PeCoffLoaderImageReadFromMemory;
+  TestContext.Handle    = &DummyHeader;
+
+  UT_ASSERT_TRUE (EFI_ERROR (PeCoffLoaderGetImageInfo (&TestContext)));
+  return UNIT_TEST_PASSED;
+}
+
+/**
+  @retval  UNIT_TEST_PASSED             The Unit test has completed and the test
+                                        case was successful.
+  @retval  UNIT_TEST_ERROR_TEST_FAILED  A test case assertion has failed.
+**/
+STATIC
+UNIT_TEST_STATUS
+EFIAPI
+GetImageInfoShouldPassOnLegitPeImage (
+  IN UNIT_TEST_CONTEXT  Context
+  )
+{
+  PE_COFF_LOADER_IMAGE_CONTEXT    TestContext;
+  TEST_FILE                       *TestFile;
+
+  UT_ASSERT_NOT_NULL (Context);
+  TestFile = (TEST_FILE*)Context;
+
+  ZeroMem (&TestContext, sizeof (TestContext));
+
+  // Use the libs read function, for consistency.
+  TestContext.ImageRead = PeCoffLoaderImageReadFromMemory;
+  TestContext.Handle    = (VOID*)TestFile->Filedata;
+
+  UT_ASSERT_NOT_EFI_ERROR (PeCoffLoaderGetImageInfo (&TestContext));
   return UNIT_TEST_PASSED;
 }
 
@@ -64,6 +193,8 @@ UnitTestingEntry (
 
   DEBUG ((DEBUG_INFO, "%a v%a\n", UNIT_TEST_APP_NAME, UNIT_TEST_APP_VERSION));
 
+  InitTestFiles ();
+
   //
   // Start setting up the test framework for running the tests.
   //
@@ -74,17 +205,19 @@ UnitTestingEntry (
   }
 
   //
-  // Populate the B64 Encode Unit Test Suite.
+  // Populate the GetImageInfo Unit Test Suite.
   //
-  Status = CreateUnitTestSuite (&GetImageInfoSuite, Fw, "Name Me", "PeCoffLib.GetImageInfoSuite", NULL, NULL);
+  Status = CreateUnitTestSuite (&GetImageInfoSuite, Fw, "PeCoffLoaderGetImageInfo Tests", "PeCoffLib.GetImageInfo", NULL, NULL);
   if (EFI_ERROR (Status)) {
-    DEBUG ((DEBUG_ERROR, "Failed in CreateUnitTestSuite for GetImageInfoSuite\n"));
+    DEBUG ((DEBUG_ERROR, "Failed in CreateUnitTestSuite for GetImageInfo\n"));
     Status = EFI_OUT_OF_RESOURCES;
     goto EXIT;
   }
 
   // --------------Suite-----------Description--------------Class Name----------Function--------Pre---Post-------------------Context-----------
-  AddTestCase (GetImageInfoSuite, "Dummy", "Dummy", TempTest, NULL, NULL, NULL);
+  AddTestCase (GetImageInfoSuite, "GetImageInfoShouldFailOnNull", "FailOnNull", GetImageInfoShouldFailOnNull, NULL, NULL, NULL);
+  AddTestCase (GetImageInfoSuite, "GetImageInfoShouldFailOnZeroBuffer", "FailOnEmptyBuffer", GetImageInfoShouldFailOnZeroBuffer, NULL, NULL, NULL);
+  AddTestCase (GetImageInfoSuite, "GetImageInfoShouldPassOnLegitPeImage", "PassOnLegitImage", GetImageInfoShouldPassOnLegitPeImage, NULL, NULL, GetTestFile ("RngDxe.efi"));
 
   //
   // Execute the tests.
@@ -95,6 +228,8 @@ EXIT:
   if (Fw) {
     FreeUnitTestFramework (Fw);
   }
+
+  DeinitTestFiles ();
 
   return Status;
 }
