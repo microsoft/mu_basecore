9678017f5eedd05c00d877275cf397fe2cd34f3d
Mike Turner <miketur@microsoft.com>
2020-06-20 00:21:52 +0000

    Merged PR 2534: Implementation of GetVariable for Advanced Logger
    Implementation of GetVariable for Advanced Logger
    
    (cherry picked from commit cde05af76244b6a51afe3fb797259fecfd1b31b8)

--- MdeModulePkg/Universal/Variable/RuntimeDxe/VariableSmmRuntimeDxe.c
+++ MdeModulePkg/Universal/Variable/RuntimeDxe/VariableSmmRuntimeDxe.c
@@ -821,7 +821,7 @@ RuntimeServiceGetVariable (
   }
 
   AcquireLockOnlyAtBootTime (&mVariableServicesLock);
-  if (FeaturePcdGet (PcdEnableVariableRuntimeCache)) {
+  if (FeaturePcdGet (PcdEnableVariableRuntimeCache) && !CompareGuid(VendorGuid, &gAdvLoggerAccessGuid)) { // MU_CHANGE
     Status = FindVariableInRuntimeCache (VariableName, VendorGuid, Attributes, DataSize, Data);
   } else {
     Status = FindVariableInSmm (VariableName, VendorGuid, Attributes, DataSize, Data);
@@ -1313,6 +1313,17 @@ VariableAddressChangeEvent (
   IN VOID                                   *Context
   )
 {
+  //
+  // Init the communicate buffer. The buffer data size is:
+  // SMM_COMMUNICATE_HEADER_SIZE + SMM_VARIABLE_COMMUNICATE_HEADER_SIZE.
+  //
+  InitCommunicateBuffer (NULL, 0, SMM_VARIABLE_FUNCTION_ADDRESS_CHANGE_EVENT);
+
+  //
+  // Send data to SMM.
+  //
+  SendCommunicateBuffer (0);
+
   EfiConvertPointer (0x0, (VOID **) &mVariableBuffer);
   EfiConvertPointer (0x0, (VOID **) &mMmCommunication2);
   EfiConvertPointer (EFI_OPTIONAL_PTR, (VOID **) &mVariableRuntimeHobCacheBuffer);
