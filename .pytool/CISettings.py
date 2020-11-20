b5fc3c8960ff0fbf7754cbad944e1045e90cc69a
Matthew Carlson <macarl@microsoft.com>
2020-11-20 20:58:26 +0000

    Merged PR 2979: Added Document for Version Info Build and Add BaseTools to CI
    added document

--- .pytool/CISettings.py
+++ .pytool/CISettings.py
@@ -51,7 +51,8 @@ class Settings(CiSetupSettingsManager, CiBuildSettingsManager, UpdateSettingsMan
         ''' return iterable of edk2 packages supported by this build.
         These should be edk2 workspace relative paths '''
 
-        return ("MdePkg",
+        return ("BaseTools", # MU_CHANGE
+                "MdePkg",
                 "MdeModulePkg",
                 "NetworkPkg",
                 "PcAtChipsetPkg",
@@ -182,7 +183,7 @@ class Settings(CiSetupSettingsManager, CiBuildSettingsManager, UpdateSettingsMan
 
     def GetDependencies(self):
         # MU_CHANGE BEGIN
-        ''' Return Git Repository Dependendencies
+        ''' Return Git Repository Dependencies
 
         Return an iterable of dictionary objects with the following fields
         {
