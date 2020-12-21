e88fbc1dee040b006141997dd9853625546402f7
Bret Barkelew <bret.barkelew@microsoft.com>
2020-12-20 23:40:36 -0800

    MdeModulePkg: Add the VariablePolicy shell-based test app
    When VariablePolicyFuncTestApp ends, the variable policy engine has been
    disabled. To accommodate automated testing, reboot the system at the end
    of the test suite to reset the variable policy state. The app already
    reboots several times during execution to reset the variable policy
    enablement/lock state.
    
    - [x] Impacts functionality?
    - **Functionality** - Does the change ultimately impact how firmware
    functions?
    - Examples: Add a new library, publish a new PPI, update an algorithm,
    ...
    - [ ] Impacts security?
    - **Security** - Does the change have a direct security impact on an
    application,
        flow, or firmware?
      - Examples: Crypto algorithm change, buffer overflow fix, parameter
        validation improvement, ...
    - [ ] Breaking change?
    - **Breaking change** - Will anyone consuming this change experience a
    break
        in build or boot behavior?
    - Examples: Add a new library class, move a module to a different repo,
    call
        a function in a new library class in a pre-existing module, ...
    - [ ] Includes tests?
      - **Tests** - Does the change include any explicit test code?
      - Examples: Unit tests, integration tests, robot tests, ...
    - [ ] Includes documentation?
    - **Documentation** - Does the change contain explicit documentation
    additions
        outside direct code modifications (and comments)?
    - Examples: Update readme file, add feature readme file, link to
    documentation
        on an a separate Web page, ...
    
    Tested on Q35
    
    N/A

---
+++ MdeModulePkg/Test/ShellTest/VariablePolicyFuncTestApp/VariablePolicyFuncTestInternal.h
@@ -0,0 +1,12 @@
+/** @file -- VariablePolicyFuncTestInternal.h
+Definitions used by the VarPolFuncTest.
+
+Copyright (c) Microsoft Corporation.
+SPDX-License-Identifier: BSD-2-Clause-Patent
+
+**/
+
+#pragma once
+
+extern UINT8  mTestAuthVarPayload[];
+extern UINTN  mTestAuthVarPayloadSize;
