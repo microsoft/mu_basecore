98a06a53ce7ca41f8d4d1093fa3792102101871b
Kenneth Lautner <klautner@microsoft.com>
2021-06-02 22:26:56 +0000

    Merged PR 4033: Changed BdsEntry to use Variable Policy.
    Changed from use of Variable Lock to Variable Policy to remove DEPRECATED INTERFACE messages.

--- MdeModulePkg/Universal/BdsDxe/BdsEntry.c
+++ MdeModulePkg/Universal/BdsDxe/BdsEntry.c
@@ -15,6 +15,10 @@ SPDX-License-Identifier: BSD-2-Clause-Patent
 #include "Bds.h"
 #include "Language.h"
 #include "HwErrRecSupport.h"
+// MU_CHANGE - START - [TCBZ3421] Updated to follow variable policy
+#include <Library/VariablePolicyLib.h>
+#include <Library/VariablePolicyHelperLib.h>
+// MU_CHANGE - END - [TCBZ3421] Updated to follow variable policy
 
 #define SET_BOOT_OPTION_SUPPORT_KEY_COUNT(a, c) {  \
       (a) = ((a) & ~EFI_BOOT_OPTION_SUPPORT_COUNT) | (((c) << LowBitSet32 (EFI_BOOT_OPTION_SUPPORT_COUNT)) & EFI_BOOT_OPTION_SUPPORT_COUNT); \
@@ -675,7 +679,10 @@ BdsEntry (
   EFI_STATUS                      Status;
   UINT32                          BootOptionSupport;
   UINT16                          BootTimeOut;
-  EDKII_VARIABLE_LOCK_PROTOCOL    *VariableLock;
+  // MU_CHANGE - START - [TCBZ3421] Updated to follow variable policy
+  //EDKII_VARIABLE_LOCK_PROTOCOL    *VariableLock;
+  EDKII_VARIABLE_POLICY_PROTOCOL  *VariablePolicy;
+  // MU_CHANGE - END - [TCBZ3421] Updated to follow variable policy
   UINTN                           Index;
   EFI_BOOT_MANAGER_LOAD_OPTION    LoadOption;
   UINT16                          *BootNext;
@@ -723,12 +730,45 @@ BdsEntry (
   //
   // Mark the read-only variables if the Variable Lock protocol exists
   //
-  Status = gBS->LocateProtocol (&gEdkiiVariableLockProtocolGuid, NULL, (VOID **) &VariableLock);
-  DEBUG ((EFI_D_INFO, "[BdsDxe] Locate Variable Lock protocol - %r\n", Status));
+  // MU_CHANGE - START - [TCBZ3421] Updated to follow variable policy
+  //Status = gBS->LocateProtocol (&gEdkiiVariableLockProtocolGuid, NULL, (VOID **) &VariableLock);
+  //DEBUG ((EFI_D_INFO, "[BdsDxe] Locate Variable Lock protocol - %r\n", Status));
+  Status = gBS->LocateProtocol (&gEdkiiVariablePolicyProtocolGuid, NULL, (VOID**) &VariablePolicy);
+  DEBUG ((DEBUG_INFO, "[BdsDxe] Locate Variable Policy protocol - %r\n", Status));
+  // MU_CHANGE - END - [TCBZ3421] Updated to follow variable policy
   if (!EFI_ERROR (Status)) {
     for (Index = 0; Index < ARRAY_SIZE (mReadOnlyVariables); Index++) {
-      Status = VariableLock->RequestToLock (VariableLock, mReadOnlyVariables[Index], &gEfiGlobalVariableGuid);
+      // MU_CHANGE - START - [TCBZ3421] Updated to follow variable policy
+      //Status = VariableLock->RequestToLock (VariableLock, mReadOnlyVariables[Index], &gEfiGlobalVariableGuid);
+      //ASSERT_EFI_ERROR (Status);
+      Status = RegisterBasicVariablePolicy (
+                 VariablePolicy,
+                 &gEfiGlobalVariableGuid,
+                 mReadOnlyVariables[Index],
+                 VARIABLE_POLICY_NO_MIN_SIZE,
+                 VARIABLE_POLICY_NO_MAX_SIZE,
+                 VARIABLE_POLICY_NO_MUST_ATTR,
+                 VARIABLE_POLICY_NO_CANT_ATTR,
+                 VARIABLE_POLICY_TYPE_LOCK_NOW
+                 );
+      //
+      // If the error returned is EFI_ALREADY_STARTED, we need to check the
+      // current database for the variable and see whether it's locked. If it's
+      // locked, we're still fine, but also generate a DEBUG_WARN message so the
+      // duplicate lock can be removed.
+      //
+      if (Status == EFI_ALREADY_STARTED) {
+        Status = ValidateSetVariable (mReadOnlyVariables[Index], &gEfiGlobalVariableGuid, 0, 0, NULL);
+        if (Status == EFI_WRITE_PROTECTED) {
+          DEBUG ((DEBUG_WARN, "  Variable: %g %s is already locked!\n", &gEfiGlobalVariableGuid, mReadOnlyVariables[Index]));
+          Status = EFI_SUCCESS;
+        } else {
+          DEBUG ((DEBUG_ERROR, "  Variable: %g %s can not be locked!\n", &gEfiGlobalVariableGuid, mReadOnlyVariables[Index]));
+          Status = EFI_ACCESS_DENIED;
+        }
+      }
       ASSERT_EFI_ERROR (Status);
+      // MU_CHANGE - END - [TCBZ3421] Updated to follow variable policy
     }
   }
 
