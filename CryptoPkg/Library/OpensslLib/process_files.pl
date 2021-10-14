c2f82b249c836ad526e92f19dde97a18c512634b
Kenneth Lautner <klautner@microsoft.com>
2021-10-14 22:53:52 +0000

    Add x64 and VS2019 IA32 openssl native instructions to cryptopkg
    Added binaries for IA32 and X64 Openssl native instructions to CryptoPkg.
    Tested on both the local build and shell with CryptoPkg tests.

--- CryptoPkg/Library/OpensslLib/process_files.pl
+++ CryptoPkg/Library/OpensslLib/process_files.pl
@@ -113,6 +113,14 @@ BEGIN {
             $uefi_config = "UEFI-x86_64-GCC";
             $extension = "S";
             $comment_character = "#";
+        ## MU_CHANGE - START - [BUG 3654] Temporary implimentation of 32-bit native instructions until official version released
+        } elsif (uc ($arch) eq "IA32") {
+            $arch = "IA32";
+            $inf_file = "OpensslLibIa32.inf";
+            $uefi_config = "UEFI-x86";
+            $extension = "nasm";
+            $comment_character = ";";
+        ## MU_CHANGE - END - [BUG 3654]
         } else {
             die "Unsupported architecture \"" . $arch . "\"!";
         }
