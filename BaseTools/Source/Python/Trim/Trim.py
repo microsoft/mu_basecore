49c09e958b1476e71ae074974011074ca3554cb2
Joey Vagedes <joeyvagedes@microsoft.com>
2021-12-17 18:56:37 +0000

    Add Mu Trim.py Updates
    -Add start and end delimiter for included ASL files
    -Raise an error when the file within an include statement cannot be found or opened

--- BaseTools/Source/Python/Trim/Trim.py
+++ BaseTools/Source/Python/Trim/Trim.py
@@ -248,6 +248,25 @@ def TrimPreprocessedVfr(Source, Target):
     except:
         EdkLogger.error("Trim", FILE_OPEN_FAILURE, ExtraData=Target)
 
+## MU_CHANGE - Create a banner for the included ASL file
+#
+# Create a banner to indicate the start and
+# end of the included ASL file. Banner looks like:-
+# 
+#   /*************************************
+#   *               @param               *
+#   *************************************/
+#
+# @param Pathname       File pathname to be included in the banner
+#
+def AddIncludeHeader(Pathname):
+    StartLine = "/*" + '*' * (len(Pathname) + 4)
+    EndLine = '*' * (len(Pathname) + 4) + "*/"
+    Banner = '\n' + StartLine
+    Banner += '\n' + ('{0}  {1}  {0}'.format('*', Pathname))
+    Banner += '\n' + EndLine + '\n'
+    return Banner
+
 ## Read the content  ASL file, including ASL included, recursively
 #
 # @param  Source            File to be read
@@ -281,11 +300,14 @@ def DoInclude(Source, Indent='', IncludePathList=[], LocalSearchPath=None, Inclu
                         F = File.readlines()
                 break
         else:
-            EdkLogger.warn("Trim", "Failed to find include file %s" % Source)
-            return []
-    except:
-        EdkLogger.warn("Trim", FILE_OPEN_FAILURE, ExtraData=Source)
-        return []
+    # MU_CHANGE begin
+            EdkLogger.error("Trim",FILE_NOT_FOUND, ExtraData="Failed to find include file %s" % Source)
+    except Exception as e: 
+        if str(e) == str(FILE_NOT_FOUND):
+            raise    
+        else:
+            EdkLogger.error("Trim", FILE_OPEN_FAILURE, ExtraData=Source)
+    # MU_CHANGE end
 
 
     # avoid A "include" B and B "include" A
@@ -312,7 +334,9 @@ def DoInclude(Source, Indent='', IncludePathList=[], LocalSearchPath=None, Inclu
                     LocalSearchPath = os.path.dirname(IncludeFile)
             CurrentIndent = Indent + Result[0][0]
             IncludedFile = Result[0][1]
+            NewFileContent.append(AddIncludeHeader(IncludedFile+" --START")) #MU_CHANGE
             NewFileContent.extend(DoInclude(IncludedFile, CurrentIndent, IncludePathList, LocalSearchPath,IncludeFileList,filetype))
+            NewFileContent.append(AddIncludeHeader(IncludedFile+" --END")) #MU_CHANGE
             NewFileContent.append("\n")
         elif filetype == "ASM":
             Result = gIncludePattern.findall(Line)
@@ -324,7 +348,9 @@ def DoInclude(Source, Indent='', IncludePathList=[], LocalSearchPath=None, Inclu
 
             IncludedFile = IncludedFile.strip()
             IncludedFile = os.path.normpath(IncludedFile)
+            NewFileContent.append(AddIncludeHeader(IncludedFile+" --START")) #MU_CHANGE
             NewFileContent.extend(DoInclude(IncludedFile, '', IncludePathList, LocalSearchPath,IncludeFileList,filetype))
+            NewFileContent.append(AddIncludeHeader(IncludedFile+" --END")) #MU_CHANGE
             NewFileContent.append("\n")
 
     gIncludedAslFile.pop()
