ced993e8c0aff140f3004ba4490e24053ad18c71
Bret Barkelew <brbarkel@microsoft.com>
2022-02-01 23:26:40 -0800

    Basetool changes to make uncrustify work and increased DevicePath length to support more PCDs.

--- BaseTools/Source/C/DevicePath/DevicePath.c
+++ BaseTools/Source/C/DevicePath/DevicePath.c
@@ -16,26 +16,27 @@ SPDX-License-Identifier: BSD-2-Clause-Patent
 //
 // Utility version information
 //
-#define UTILITY_MAJOR_VERSION 0
-#define UTILITY_MINOR_VERSION 1
+#define UTILITY_MAJOR_VERSION  0
+#define UTILITY_MINOR_VERSION  1
 
-EFI_GUID gEfiDebugPortProtocolGuid = DEVICE_PATH_MESSAGING_DEBUGPORT;
-EFI_GUID gEfiPcAnsiGuid = EFI_PC_ANSI_GUID;
-EFI_GUID gEfiVT100Guid = EFI_VT_100_GUID;
-EFI_GUID gEfiVT100PlusGuid = EFI_VT_100_PLUS_GUID;
-EFI_GUID gEfiVTUTF8Guid = EFI_VT_UTF8_GUID;
-EFI_GUID gEfiUartDevicePathGuid = EFI_UART_DEVICE_PATH_GUID;
-EFI_GUID gEfiSasDevicePathGuid = EFI_SAS_DEVICE_PATH_GUID;
-EFI_GUID gEfiVirtualDiskGuid = EFI_VIRTUAL_DISK_GUID;
-EFI_GUID gEfiVirtualCdGuid = EFI_VIRTUAL_CD_GUID;
-EFI_GUID gEfiPersistentVirtualDiskGuid = EFI_PERSISTENT_VIRTUAL_DISK_GUID;
-EFI_GUID gEfiPersistentVirtualCdGuid = EFI_PERSISTENT_VIRTUAL_CD_GUID;
+EFI_GUID  gEfiDebugPortProtocolGuid     = DEVICE_PATH_MESSAGING_DEBUGPORT;
+EFI_GUID  gEfiPcAnsiGuid                = EFI_PC_ANSI_GUID;
+EFI_GUID  gEfiVT100Guid                 = EFI_VT_100_GUID;
+EFI_GUID  gEfiVT100PlusGuid             = EFI_VT_100_PLUS_GUID;
+EFI_GUID  gEfiVTUTF8Guid                = EFI_VT_UTF8_GUID;
+EFI_GUID  gEfiUartDevicePathGuid        = EFI_UART_DEVICE_PATH_GUID;
+EFI_GUID  gEfiSasDevicePathGuid         = EFI_SAS_DEVICE_PATH_GUID;
+EFI_GUID  gEfiVirtualDiskGuid           = EFI_VIRTUAL_DISK_GUID;
+EFI_GUID  gEfiVirtualCdGuid             = EFI_VIRTUAL_CD_GUID;
+EFI_GUID  gEfiPersistentVirtualDiskGuid = EFI_PERSISTENT_VIRTUAL_DISK_GUID;
+EFI_GUID  gEfiPersistentVirtualCdGuid   = EFI_PERSISTENT_VIRTUAL_CD_GUID;
 
 STATIC
 VOID
 Version (
   VOID
-)
+  )
+
 /*++
 
 Routine Description:
@@ -60,6 +61,7 @@ VOID
 Usage (
   VOID
   )
+
 /*++
 
 Routine Description:
@@ -89,35 +91,38 @@ Returns:
   // Details Option
   //
   fprintf (stdout, "Options:\n");
-  fprintf (stdout, "  DevicePathString      Device Path string is specified, no space character.\n"
-                   "                        Example: \"PciRoot(0)/Pci(0,0)\"\n");
+  fprintf (
+    stdout,
+    "  DevicePathString      Device Path string is specified, no space character.\n"
+    "                        Example: \"PciRoot(0)/Pci(0,0)\"\n"
+    );
 
   fprintf (stdout, "  --version             Show program's version number and exit.\n");
   fprintf (stdout, "  -h, --help            Show this help message and exit.\n");
 }
 
-
 STATIC
 VOID
 PrintMem (
-  CONST VOID *Buffer,
-  UINTN      Count
+  CONST VOID  *Buffer,
+  UINTN       Count
   )
 {
-  CONST UINT8 *Bytes;
-  UINTN       Idx;
+  CONST UINT8  *Bytes;
+  UINTN        Idx;
 
   Bytes = Buffer;
   for (Idx = 0; Idx < Count; Idx++) {
-    printf("0x%02x ", Bytes[Idx]);
+    printf ("0x%02x ", Bytes[Idx]);
   }
 }
 
 VOID
 Ascii2UnicodeString (
-  CHAR8    *String,
-  CHAR16   *UniString
- )
+  CHAR8   *String,
+  CHAR16  *UniString
+  )
+
 /*++
 
 Routine Description:
@@ -136,25 +141,31 @@ Returns:
 --*/
 {
   while (*String != '\0') {
-    *(UniString++) = (CHAR16) *(String++);
+    *(UniString++) = (CHAR16)*(String++);
   }
+
   //
   // End the UniString with a NULL.
   //
   *UniString = '\0';
 }
 
-int main(int argc, CHAR8 *argv[])
+int
+main (
+  int    argc,
+  CHAR8  *argv[]
+  )
 {
-  CHAR8 * Str;
-  CHAR16* Str16;
-  EFI_DEVICE_PATH_PROTOCOL *DevicePath;
+  CHAR8                     *Str;
+  CHAR16                    *Str16;
+  EFI_DEVICE_PATH_PROTOCOL  *DevicePath;
 
   if (argc == 1) {
     Error (NULL, 0, 1001, "Missing options", "No input options specified.");
     Usage ();
     return STATUS_ERROR;
   }
+
   if ((stricmp (argv[1], "-h") == 0) || (stricmp (argv[1], "--help") == 0)) {
     Version ();
     Usage ();
@@ -165,30 +176,34 @@ int main(int argc, CHAR8 *argv[])
     Version ();
     return STATUS_SUCCESS;
   }
+
   Str = argv[1];
   if (Str == NULL) {
-    fprintf(stderr, "Invalid option value, Device Path can't be NULL");
+    fprintf (stderr, "Invalid option value, Device Path can't be NULL");
     return STATUS_ERROR;
   }
+
   Str16 = (CHAR16 *)malloc((strlen (Str) + 1) * sizeof (CHAR16));
   if (Str16 == NULL) {
-    fprintf(stderr, "Resource, memory cannot be allocated");
+    fprintf (stderr, "Resource, memory cannot be allocated");
     return STATUS_ERROR;
   }
-  Ascii2UnicodeString(Str, Str16);
-  DevicePath = UefiDevicePathLibConvertTextToDevicePath(Str16);
+
+  Ascii2UnicodeString (Str, Str16);
+  DevicePath = UefiDevicePathLibConvertTextToDevicePath (Str16);
   if (DevicePath == NULL) {
-    fprintf(stderr, "Convert fail, Cannot convert text to a device path");
-    free(Str16);
+    fprintf (stderr, "Convert fail, Cannot convert text to a device path");
+    free (Str16);
     return STATUS_ERROR;
   }
-  while (!((DevicePath->Type == END_DEVICE_PATH_TYPE) && (DevicePath->SubType == END_ENTIRE_DEVICE_PATH_SUBTYPE)) )
-  {
+
+  while (!((DevicePath->Type == END_DEVICE_PATH_TYPE) && (DevicePath->SubType == END_ENTIRE_DEVICE_PATH_SUBTYPE))) {
     PrintMem (DevicePath, DevicePath->Length[0] | DevicePath->Length[1] << 8);
     DevicePath = (EFI_DEVICE_PATH_PROTOCOL *)((UINT8 *)DevicePath + (DevicePath->Length[0] | DevicePath->Length[1] << 8));
   }
+
   PrintMem (DevicePath, DevicePath->Length[0] | DevicePath->Length[1] << 8);
-  putchar('\n');
-  free(Str16);
+  putchar ('\n');
+  free (Str16);
   return STATUS_SUCCESS;
 }
