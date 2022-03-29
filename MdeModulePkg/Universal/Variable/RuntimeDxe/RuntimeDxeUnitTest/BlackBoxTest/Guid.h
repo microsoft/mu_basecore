d43b67d2e5a9572748d58ab306e4c03bafcfeff4
Bret Barkelew <brbarkel@microsoft.com>
2022-03-29 16:09:35 -0700

    Directly copy required files from SCT
    NOTE: Will not build yet. This import is kept separate from the
    changes to get things building so that it's easy to track the
    build changes.

---
+++ MdeModulePkg/Universal/Variable/RuntimeDxe/RuntimeDxeUnitTest/BlackBoxTest/Guid.h
@@ -0,0 +1,457 @@
+/** @file
+
+  Copyright 2006 - 2016 Unified EFI, Inc.<BR>
+  Copyright (c) 2010 - 2018, Intel Corporation. All rights reserved.<BR>
+
+  This program and the accompanying materials
+  are licensed and made available under the terms and conditions of the BSD License
+  which accompanies this distribution.  The full text of the license may be found at
+  http://opensource.org/licenses/bsd-license.php
+
+  THE PROGRAM IS DISTRIBUTED UNDER THE BSD LICENSE ON AN "AS IS" BASIS,
+  WITHOUT WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.
+
+**/
+/*++
+
+Module Name:
+
+  guid.h
+
+Abstract:
+
+  GUIDs auto-generated for EFI test assertion.
+
+--*/
+
+#define EFI_TEST_VARIABLESERVICESBBTESTSTRESS_ASSERTION_001_GUID \
+{ 0x22470570, 0x61d5, 0x4459, {0xb0, 0x09, 0xdd, 0x30, 0x1c, 0xc2, 0x90, 0xda }}
+
+extern EFI_GUID  gVariableServicesBbTestStressAssertionGuid001;
+
+#define EFI_TEST_VARIABLESERVICESBBTESTSTRESS_ASSERTION_002_GUID \
+{ 0x27fb1769, 0xf56c, 0x44a7, {0x9d, 0x50, 0x7b, 0xdf, 0x70, 0x46, 0x49, 0x2e }}
+
+extern EFI_GUID  gVariableServicesBbTestStressAssertionGuid002;
+
+#define EFI_TEST_VARIABLESERVICESBBTESTSTRESS_ASSERTION_003_GUID \
+{ 0x1f093ebc, 0x566a, 0x4b16, {0x92, 0x13, 0xad, 0xe3, 0x37, 0x6d, 0x4a, 0xc5 }}
+
+extern EFI_GUID  gVariableServicesBbTestStressAssertionGuid003;
+
+#define EFI_TEST_VARIABLESERVICESBBTESTSTRESS_ASSERTION_004_GUID \
+{ 0x28f4b832, 0xf034, 0x42ef, {0xb5, 0xfc, 0xcb, 0xb9, 0xbe, 0xc7, 0x17, 0x24 }}
+
+extern EFI_GUID  gVariableServicesBbTestStressAssertionGuid004;
+
+#define EFI_TEST_VARIABLESERVICESBBTESTSTRESS_ASSERTION_005_GUID \
+{ 0x1aa0cb2f, 0xb4e6, 0x4d29, {0x91, 0x79, 0x03, 0x9f, 0x5f, 0x63, 0xb1, 0xfd }}
+
+extern EFI_GUID  gVariableServicesBbTestStressAssertionGuid005;
+
+#define EFI_TEST_VARIABLESERVICESBBTESTSTRESS_ASSERTION_006_GUID \
+{ 0x5962ed49, 0x7fad, 0x4db4, {0xa6, 0xe1, 0x9c, 0xc8, 0x99, 0xc7, 0x3c, 0x81 }}
+
+extern EFI_GUID  gVariableServicesBbTestStressAssertionGuid006;
+
+#define EFI_TEST_VARIABLESERVICESBBTESTCONFORMANCE_ASSERTION_001_GUID \
+{ 0xb0d54fee, 0x2787, 0x4d2d, {0xbf, 0x98, 0x73, 0xa0, 0xcd, 0x7f, 0xe9, 0x5d }}
+
+extern EFI_GUID  gVariableServicesBbTestConformanceAssertionGuid001;
+
+#define EFI_TEST_VARIABLESERVICESBBTESTCONFORMANCE_ASSERTION_002_GUID \
+{ 0x390c5e26, 0x9b46, 0x4974, {0xb3, 0x2d, 0x2b, 0xb1, 0xd4, 0x05, 0xb0, 0xd7 }}
+
+extern EFI_GUID  gVariableServicesBbTestConformanceAssertionGuid002;
+
+#define EFI_TEST_VARIABLESERVICESBBTESTCONFORMANCE_ASSERTION_003_GUID \
+{ 0x176354a6, 0x1088, 0x474f, {0xbf, 0x6f, 0x95, 0x8c, 0x1c, 0xc3, 0x40, 0x8f }}
+
+extern EFI_GUID  gVariableServicesBbTestConformanceAssertionGuid003;
+
+#define EFI_TEST_VARIABLESERVICESBBTESTCONFORMANCE_ASSERTION_004_GUID \
+{ 0x400ab801, 0xf6c6, 0x4d04, {0xa0, 0x42, 0xa2, 0x15, 0x0b, 0xd5, 0xb6, 0x2a }}
+
+extern EFI_GUID  gVariableServicesBbTestConformanceAssertionGuid004;
+
+#define EFI_TEST_VARIABLESERVICESBBTESTCONFORMANCE_ASSERTION_005_GUID \
+{ 0x9b704b3d, 0x05a4, 0x4147, {0xb2, 0x55, 0x35, 0xbc, 0x3d, 0xd6, 0xcc, 0x24 }}
+
+extern EFI_GUID  gVariableServicesBbTestConformanceAssertionGuid005;
+
+#define EFI_TEST_VARIABLESERVICESBBTESTCONFORMANCE_ASSERTION_006_GUID \
+{ 0xd3d915a5, 0xe7b0, 0x4417, {0x9c, 0x2e, 0x1a, 0xa8, 0x42, 0x4d, 0x22, 0x2c }}
+
+extern EFI_GUID  gVariableServicesBbTestConformanceAssertionGuid006;
+
+#define EFI_TEST_VARIABLESERVICESBBTESTCONFORMANCE_ASSERTION_007_GUID \
+{ 0x1562ce35, 0x83e7, 0x48a7, {0xad, 0x71, 0xfa, 0xa4, 0xbe, 0x17, 0x88, 0x46 }}
+
+extern EFI_GUID  gVariableServicesBbTestConformanceAssertionGuid007;
+
+#define EFI_TEST_VARIABLESERVICESBBTESTCONFORMANCE_ASSERTION_008_GUID \
+{ 0x121c17d1, 0xbb0e, 0x4e2e, {0xb2, 0xa5, 0x03, 0x86, 0x2f, 0x46, 0xc0, 0x39 }}
+
+extern EFI_GUID  gVariableServicesBbTestConformanceAssertionGuid008;
+
+#define EFI_TEST_VARIABLESERVICESBBTESTCONFORMANCE_ASSERTION_009_GUID \
+{ 0x5826847a, 0x9067, 0x4f9f, {0x88, 0x38, 0x0b, 0xf8, 0xec, 0x20, 0x17, 0x1c }}
+
+extern EFI_GUID  gVariableServicesBbTestConformanceAssertionGuid009;
+
+#define EFI_TEST_VARIABLESERVICESBBTESTCONFORMANCE_ASSERTION_010_GUID \
+{ 0x8e8258dc, 0x6634, 0x4de1, {0x85, 0x7a, 0x60, 0x45, 0x7e, 0xfa, 0x7c, 0x21 }}
+
+extern EFI_GUID  gVariableServicesBbTestConformanceAssertionGuid010;
+
+#define EFI_TEST_VARIABLESERVICESBBTESTCONFORMANCE_ASSERTION_011_GUID \
+{ 0x99a357f0, 0xb6c5, 0x4aec, {0x96, 0x48, 0x34, 0x73, 0x2d, 0x2a, 0x49, 0x50 }}
+
+extern EFI_GUID  gVariableServicesBbTestConformanceAssertionGuid011;
+
+#define EFI_TEST_VARIABLESERVICESBBTESTCONFORMANCE_ASSERTION_012_GUID \
+{ 0x51c19dba, 0xbaf6, 0x4854, {0xac, 0x09, 0x60, 0x45, 0x47, 0x88, 0x67, 0x98 }}
+
+extern EFI_GUID  gVariableServicesBbTestConformanceAssertionGuid012;
+
+#define EFI_TEST_VARIABLESERVICESBBTESTCONFORMANCE_ASSERTION_013_GUID \
+{ 0xfe09ff82, 0xb289, 0x449f, {0xb0, 0x83, 0x98, 0x1d, 0x68, 0xd9, 0x17, 0xb1 }}
+
+extern EFI_GUID  gVariableServicesBbTestConformanceAssertionGuid013;
+
+#define EFI_TEST_VARIABLESERVICESBBTESTCONFORMANCE_ASSERTION_014_GUID \
+{ 0x73af529b, 0x3ebe, 0x464a, {0xba, 0x6a, 0xfb, 0x04, 0x7b, 0x56, 0x4f, 0x74 }}
+
+extern EFI_GUID  gVariableServicesBbTestConformanceAssertionGuid014;
+
+#define EFI_TEST_VARIABLESERVICESBBTESTCONFORMANCE_ASSERTION_015_GUID \
+{ 0x39e95cbb, 0x6b89, 0x473e, {0x91, 0xba, 0x92, 0x08, 0x2d, 0x1b, 0x94, 0xad }}
+
+extern EFI_GUID  gVariableServicesBbTestConformanceAssertionGuid015;
+
+#define EFI_TEST_VARIABLESERVICESBBTESTCONFORMANCE_ASSERTION_016_GUID \
+{ 0xf6ef5087, 0x4962, 0x4d71, {0x80, 0x09, 0xdb, 0xe2, 0x78, 0x94, 0x53, 0xe6 }}
+
+extern EFI_GUID  gVariableServicesBbTestConformanceAssertionGuid016;
+
+#define EFI_TEST_VARIABLESERVICESBBTESTCONFORMANCE_ASSERTION_017_GUID \
+{ 0x65973462, 0x6877, 0x408f, {0x9b, 0xe1, 0x46, 0x69, 0x3e, 0xab, 0x03, 0x84 }}
+
+extern EFI_GUID  gVariableServicesBbTestConformanceAssertionGuid017;
+
+#define EFI_TEST_VARIABLESERVICESBBTESTCONFORMANCE_ASSERTION_018_GUID \
+{ 0x98ca8089, 0x7f55, 0x4427, {0x8c, 0x15, 0xaf, 0xa6, 0x3d, 0x78, 0x48, 0xb0 }}
+
+extern EFI_GUID  gVariableServicesBbTestConformanceAssertionGuid018;
+
+#define EFI_TEST_VARIABLESERVICESBBTESTCONFORMANCE_ASSERTION_019_GUID \
+{ 0xe9893bcb, 0xef2b, 0x495c, { 0x82, 0xf0, 0xd0, 0x63, 0xd, 0xa7, 0x94, 0x76 }}
+
+extern EFI_GUID  gVariableServicesBbTestConformanceAssertionGuid019;
+
+#define EFI_TEST_VARIABLESERVICESBBTESTCONFORMANCE_ASSERTION_020_GUID \
+{ 0x2534abc0, 0x1f01, 0x48a0, { 0x96, 0xde, 0xf8, 0xbb, 0xa7, 0x45, 0xc3, 0x64 }}
+
+extern EFI_GUID  gVariableServicesBbTestConformanceAssertionGuid020;
+
+#define EFI_TEST_VARIABLESERVICESBBTESTCONFORMANCE_ASSERTION_021_GUID \
+{ 0x896f8325, 0xed28, 0x4af5, { 0x96, 0xba, 0x3b, 0xe3, 0xf2, 0x97, 0x74, 0x8b }}
+
+extern EFI_GUID  gVariableServicesBbTestConformanceAssertionGuid021;
+
+#define EFI_TEST_VARIABLESERVICESBBTESTCONFORMANCE_ASSERTION_022_GUID \
+{ 0xe542e81c, 0x2020, 0x4f3e, { 0xa9, 0xb, 0x67, 0xd4, 0xa8, 0xd1, 0x70, 0xb4 }}
+
+extern EFI_GUID  gVariableServicesBbTestConformanceAssertionGuid022;
+
+#define EFI_TEST_VARIABLESERVICESBBTESTCONFORMANCE_ASSERTION_023_GUID \
+{ 0x55af8fad, 0x1b12, 0x45ad, { 0xb0, 0x42, 0xa6, 0x43, 0x96, 0x2f, 0x4, 0x70 }}
+
+extern EFI_GUID  gVariableServicesBbTestConformanceAssertionGuid023;
+
+#define EFI_TEST_VARIABLESERVICESBBTESTCONFORMANCE_ASSERTION_024_GUID \
+{ 0x6d0740fb, 0x388a, 0x491f, { 0xb8, 0x18, 0x9b, 0x5c, 0xb5, 0x17, 0xf, 0x95 }}
+
+extern EFI_GUID  gVariableServicesBbTestConformanceAssertionGuid024;
+
+#define EFI_TEST_VARIABLESERVICESBBTESTFUNCTION_ASSERTION_001_GUID \
+{ 0xaa35cc00, 0xc55c, 0x42d8, {0xa6, 0xd4, 0x1e, 0xb4, 0x9d, 0xe3, 0xd7, 0x54 }}
+
+extern EFI_GUID  gVariableServicesBbTestFunctionAssertionGuid001;
+
+#define EFI_TEST_VARIABLESERVICESBBTESTFUNCTION_ASSERTION_002_GUID \
+{ 0x742a9651, 0x9783, 0x43b8, {0x8c, 0x18, 0x47, 0x04, 0xae, 0x41, 0xc3, 0x34 }}
+
+extern EFI_GUID  gVariableServicesBbTestFunctionAssertionGuid002;
+
+#define EFI_TEST_VARIABLESERVICESBBTESTFUNCTION_ASSERTION_003_GUID \
+{ 0x90e959d0, 0xbe2c, 0x45fd, {0x85, 0x32, 0x85, 0x21, 0xe4, 0xe0, 0xfb, 0x72 }}
+
+extern EFI_GUID  gVariableServicesBbTestFunctionAssertionGuid003;
+
+#define EFI_TEST_VARIABLESERVICESBBTESTFUNCTION_ASSERTION_004_GUID \
+{ 0x5c8b43b7, 0xec6f, 0x4621, {0xb8, 0x48, 0x6a, 0x40, 0x0f, 0xd8, 0xb3, 0x43 }}
+
+extern EFI_GUID  gVariableServicesBbTestFunctionAssertionGuid004;
+
+#define EFI_TEST_VARIABLESERVICESBBTESTFUNCTION_ASSERTION_005_GUID \
+{ 0x12071508, 0x16c7, 0x4e5e, {0xa4, 0x22, 0x59, 0xe0, 0x24, 0x1c, 0xc6, 0x28 }}
+
+extern EFI_GUID  gVariableServicesBbTestFunctionAssertionGuid005;
+
+#define EFI_TEST_VARIABLESERVICESBBTESTFUNCTION_ASSERTION_006_GUID \
+{ 0xa85043bc, 0x4f0d, 0x47b3, {0x8e, 0x9d, 0x2d, 0xb6, 0xc8, 0xf8, 0xfa, 0xef }}
+
+extern EFI_GUID  gVariableServicesBbTestFunctionAssertionGuid006;
+
+#define EFI_TEST_VARIABLESERVICESBBTESTFUNCTION_ASSERTION_007_GUID \
+{ 0x6c9cf2ea, 0xcabd, 0x4312, {0xb9, 0xcf, 0x0a, 0x96, 0xc4, 0xf1, 0xea, 0x8b }}
+
+extern EFI_GUID  gVariableServicesBbTestFunctionAssertionGuid007;
+
+#define EFI_TEST_VARIABLESERVICESBBTESTFUNCTION_ASSERTION_008_GUID \
+{ 0x3ae09eaf, 0x07cd, 0x4320, {0x92, 0xfd, 0xe9, 0xe6, 0x4b, 0x31, 0x6f, 0xe1 }}
+
+extern EFI_GUID  gVariableServicesBbTestFunctionAssertionGuid008;
+
+#define EFI_TEST_VARIABLESERVICESBBTESTFUNCTION_ASSERTION_009_GUID \
+{ 0x7ccde75b, 0x4ef2, 0x40ec, {0x9a, 0xcb, 0x84, 0x7b, 0xb5, 0x29, 0x73, 0xbe }}
+
+extern EFI_GUID  gVariableServicesBbTestFunctionAssertionGuid009;
+
+#define EFI_TEST_VARIABLESERVICESBBTESTFUNCTION_ASSERTION_010_GUID \
+{ 0x5b720ad1, 0xd0cc, 0x4be0, {0x93, 0x18, 0x20, 0x1b, 0xac, 0x32, 0x8d, 0x4f }}
+
+extern EFI_GUID  gVariableServicesBbTestFunctionAssertionGuid010;
+
+#define EFI_TEST_VARIABLESERVICESBBTESTFUNCTION_ASSERTION_011_GUID \
+{ 0x2dee62d3, 0xbab7, 0x4d91, {0x8b, 0x47, 0x3e, 0x38, 0x35, 0xd3, 0x88, 0xae }}
+
+extern EFI_GUID  gVariableServicesBbTestFunctionAssertionGuid011;
+
+#define EFI_TEST_VARIABLESERVICESBBTESTFUNCTION_ASSERTION_012_GUID \
+{ 0x861a0691, 0x6590, 0x4a28, {0xae, 0x56, 0xaa, 0xcb, 0xf3, 0xf2, 0xbe, 0x99 }}
+
+extern EFI_GUID  gVariableServicesBbTestFunctionAssertionGuid012;
+
+#define EFI_TEST_VARIABLESERVICESBBTESTFUNCTION_ASSERTION_013_GUID \
+{ 0x76198a1a, 0xc63a, 0x4a3b, {0x88, 0xb0, 0xc4, 0x45, 0x39, 0xdd, 0xff, 0x5d }}
+
+extern EFI_GUID  gVariableServicesBbTestFunctionAssertionGuid013;
+
+#define EFI_TEST_VARIABLESERVICESBBTESTFUNCTION_ASSERTION_014_GUID \
+{ 0xcefbdb2c, 0x0c7d, 0x4dcf, {0xae, 0x16, 0x32, 0xa8, 0x78, 0xca, 0x2d, 0x3e }}
+
+extern EFI_GUID  gVariableServicesBbTestFunctionAssertionGuid014;
+
+#define EFI_TEST_VARIABLESERVICESBBTESTFUNCTION_ASSERTION_015_GUID \
+{ 0xc457149c, 0x75d0, 0x48b5, {0xa1, 0x6c, 0x7e, 0x9f, 0x14, 0x4a, 0xab, 0x15 }}
+
+extern EFI_GUID  gVariableServicesBbTestFunctionAssertionGuid015;
+
+#define EFI_TEST_VARIABLESERVICESBBTESTFUNCTION_ASSERTION_016_GUID \
+{ 0x89f533da, 0x20ee, 0x41f8, {0x8c, 0x60, 0xc3, 0xc4, 0x14, 0x19, 0x05, 0x15 }}
+
+extern EFI_GUID  gVariableServicesBbTestFunctionAssertionGuid016;
+
+#define EFI_TEST_VARIABLESERVICESBBTESTFUNCTION_ASSERTION_017_GUID \
+{ 0xfc5f89d1, 0x4fce, 0x4fe9, {0xa2, 0xfd, 0xa2, 0xfe, 0x69, 0x5b, 0xaa, 0x35 }}
+
+extern EFI_GUID  gVariableServicesBbTestFunctionAssertionGuid017;
+
+#define EFI_TEST_VARIABLESERVICESBBTESTFUNCTION_ASSERTION_018_GUID \
+{ 0xfa5f4961, 0xdfaf, 0x425f, {0x95, 0x14, 0x14, 0x52, 0x5c, 0x69, 0xc7, 0x83 }}
+
+extern EFI_GUID  gVariableServicesBbTestFunctionAssertionGuid018;
+
+#define EFI_TEST_VARIABLESERVICESBBTESTFUNCTION_ASSERTION_019_GUID \
+{ 0x3cf290ca, 0x49e9, 0x43c0, {0x8a, 0x0c, 0x46, 0xea, 0x17, 0x53, 0x41, 0x08 }}
+
+extern EFI_GUID  gVariableServicesBbTestFunctionAssertionGuid019;
+
+#define EFI_TEST_VARIABLESERVICESBBTESTFUNCTION_ASSERTION_020_GUID \
+{ 0xc1f69f8f, 0xa6ed, 0x4823, {0x88, 0xd9, 0x9a, 0x23, 0x8e, 0x6a, 0x11, 0x00 }}
+
+extern EFI_GUID  gVariableServicesBbTestFunctionAssertionGuid020;
+
+#define EFI_TEST_VARIABLESERVICESBBTESTFUNCTION_ASSERTION_021_GUID \
+{ 0x7b893a77, 0x70ca, 0x48e4, {0xad, 0x1d, 0xe4, 0x31, 0x15, 0xb1, 0xce, 0x5e }}
+
+extern EFI_GUID  gVariableServicesBbTestFunctionAssertionGuid021;
+
+#define EFI_TEST_VARIABLESERVICESBBTESTFUNCTION_ASSERTION_022_GUID \
+{ 0x8fcc7182, 0x4f77, 0x4841, {0xbb, 0x81, 0x20, 0xe5, 0x30, 0x5e, 0xa9, 0xda }}
+
+extern EFI_GUID  gVariableServicesBbTestFunctionAssertionGuid022;
+
+#define EFI_TEST_VARIABLESERVICESBBTESTFUNCTION_ASSERTION_023_GUID \
+{ 0x931b363e, 0x8ab4, 0x49db, {0x82, 0x21, 0x2f, 0xdd, 0x9d, 0xa4, 0x36, 0x6c }}
+
+extern EFI_GUID  gVariableServicesBbTestFunctionAssertionGuid023;
+
+#define EFI_TEST_VARIABLESERVICESBBTESTFUNCTION_ASSERTION_024_GUID \
+{ 0x7eac83e5, 0x0e54, 0x4812, {0x9b, 0xb0, 0x6f, 0xf6, 0xdc, 0x7d, 0xeb, 0x8f }}
+
+extern EFI_GUID  gVariableServicesBbTestFunctionAssertionGuid024;
+
+#define EFI_TEST_VARIABLESERVICESBBTESTFUNCTION_ASSERTION_025_GUID \
+{ 0x6afdea5e, 0x1030, 0x48ab, {0x91, 0xdd, 0x7c, 0xd3, 0x53, 0x7c, 0xad, 0x3b }}
+
+extern EFI_GUID  gVariableServicesBbTestFunctionAssertionGuid025;
+
+#define EFI_TEST_VARIABLESERVICESBBTESTFUNCTION_ASSERTION_026_GUID \
+{ 0x653f14cc, 0x8ecd, 0x4aaf, {0xad, 0xd6, 0x96, 0xc5, 0x07, 0x11, 0x2d, 0x67 }}
+
+extern EFI_GUID  gVariableServicesBbTestFunctionAssertionGuid026;
+
+#define EFI_TEST_VARIABLESERVICESBBTESTFUNCTION_ASSERTION_027_GUID \
+{ 0xb93d2b03, 0x5943, 0x4c7d, {0x98, 0xec, 0xc5, 0xfe, 0x4c, 0x6e, 0x10, 0xc9 }}
+
+extern EFI_GUID  gVariableServicesBbTestFunctionAssertionGuid027;
+
+#define EFI_TEST_VARIABLESERVICESBBTESTFUNCTION_ASSERTION_028_GUID \
+{ 0x9ec88dbe, 0xa0e4, 0x43a2, {0xaa, 0x2b, 0x60, 0xbd, 0xe6, 0xb0, 0x14, 0x1a }}
+
+extern EFI_GUID  gVariableServicesBbTestFunctionAssertionGuid028;
+
+#define EFI_TEST_VARIABLESERVICESBBTESTFUNCTION_ASSERTION_029_GUID \
+{ 0x8e18a5, 0xc345, 0x48ae, { 0x91, 0x34, 0x61, 0xa6, 0x92, 0xe3, 0xb, 0x87 }}
+
+extern EFI_GUID  gVariableServicesBbTestFunctionAssertionGuid029;
+
+#define EFI_TEST_VARIABLESERVICESBBTESTFUNCTION_ASSERTION_030_GUID \
+{ 0x20678b3e, 0xbcca, 0x4186, { 0x84, 0xaf, 0x47, 0x16, 0xe7, 0xaf, 0xde, 0x85 }}
+
+extern EFI_GUID  gVariableServicesBbTestFunctionAssertionGuid030;
+
+#define EFI_TEST_VARIABLESERVICESBBTESTFUNCTION_ASSERTION_031_GUID \
+{ 0xaa6bf36f, 0xdae5, 0x43ed, { 0x95, 0x4d, 0xc1, 0xc7, 0x97, 0x9d, 0x32, 0xa0 }}
+
+extern EFI_GUID  gVariableServicesBbTestFunctionAssertionGuid031;
+
+#define EFI_TEST_VARIABLESERVICESBBTESTFUNCTION_ASSERTION_032_GUID \
+{ 0x2bc131ec, 0x530, 0x4994, { 0xbb, 0x81, 0x15, 0x35, 0x5c, 0xef, 0xe5, 0x88 }}
+
+extern EFI_GUID  gVariableServicesBbTestFunctionAssertionGuid032;
+
+#define EFI_TEST_VARIABLESERVICESBBTESTFUNCTION_ASSERTION_033_GUID \
+{ 0xe49b21e, 0x409c, 0x4502, { 0x9e, 0xc6, 0x55, 0xfe, 0x85, 0xf8, 0x54, 0x95 }}
+
+extern EFI_GUID  gVariableServicesBbTestFunctionAssertionGuid033;
+
+#define EFI_TEST_VARIABLESERVICESBBTESTFUNCTION_ASSERTION_034_GUID \
+{ 0xadabac45, 0x1e0d, 0x40b0, { 0x9b, 0xd1, 0x8c, 0x3a, 0xd7, 0xfb, 0x69, 0xd6 }}
+
+extern EFI_GUID  gVariableServicesBbTestFunctionAssertionGuid034;
+
+#define EFI_TEST_VARIABLESERVICESBBTESTFUNCTION_ASSERTION_035_GUID \
+{ 0x6339807b, 0x741, 0x45c4, { 0x81, 0xa8, 0xe2, 0xde, 0x5a, 0xb, 0xfb, 0x55 }}
+
+extern EFI_GUID  gVariableServicesBbTestFunctionAssertionGuid035;
+
+#define EFI_TEST_VARIABLESERVICESBBTESTFUNCTION_ASSERTION_036_GUID \
+{ 0xa2d53dea, 0x8275, 0x4b9a, { 0xbd, 0xa0, 0xac, 0x86, 0xfb, 0x4e, 0xf, 0x30 }}
+
+extern EFI_GUID  gVariableServicesBbTestFunctionAssertionGuid036;
+
+#define EFI_TEST_VARIABLESERVICESBBTESTFUNCTION_ASSERTION_037_GUID \
+{ 0x28c7f0db, 0x2546, 0x4374, { 0x8f, 0xf9, 0x75, 0x80, 0xc4, 0x68, 0x9b, 0x93 }}
+
+extern EFI_GUID  gVariableServicesBbTestFunctionAssertionGuid037;
+
+#define EFI_TEST_VARIABLESERVICESBBTESTFUNCTION_ASSERTION_038_GUID \
+{ 0x1e87dbe9, 0x234b, 0x4c82, { 0x8c, 0x86, 0x2f, 0x26, 0xfa, 0xc6, 0x60, 0x2e }}
+
+extern EFI_GUID  gVariableServicesBbTestFunctionAssertionGuid038;
+
+#define EFI_TEST_VARIABLESERVICESBBTESTFUNCTION_ASSERTION_039_GUID \
+{ 0x3cc4add2, 0xed7, 0x4837, { 0xb4, 0x63, 0xbc, 0x46, 0xd1, 0x3b, 0x2f, 0x65 }}
+
+extern EFI_GUID  gVariableServicesBbTestFunctionAssertionGuid039;
+
+#define EFI_TEST_VARIABLESERVICESBBTESTFUNCTION_ASSERTION_040_GUID \
+{ 0xfa50a705, 0x5d95, 0x4cad, { 0xb4, 0x6c, 0xa0, 0x12, 0x9b, 0x68, 0x22, 0x8e }}
+
+extern EFI_GUID  gVariableServicesBbTestFunctionAssertionGuid040;
+
+#define EFI_TEST_VARIABLESERVICESBBTESTFUNCTION_ASSERTION_041_GUID \
+{ 0x27e8e4de, 0x56ed, 0x4710, { 0xa6, 0x3a, 0xc6, 0x35, 0xe3, 0x9d, 0x33, 0x64 }}
+
+extern EFI_GUID  gVariableServicesBbTestFunctionAssertionGuid041;
+
+#define EFI_TEST_VARIABLESERVICESBBTESTFUNCTION_ASSERTION_042_GUID \
+{ 0xba99e7f8, 0x8018, 0x46a2, { 0xb2, 0xe5, 0x8b, 0xde, 0x42, 0xc1, 0xe6, 0xd5 }}
+
+extern EFI_GUID  gVariableServicesBbTestFunctionAssertionGuid042;
+
+#define EFI_TEST_VARIABLESERVICESBBTESTFUNCTION_ASSERTION_043_GUID \
+{ 0xc764906d, 0x73bb, 0x44b7, {0xae, 0x40, 0xc, 0x51, 0xde, 0xc3, 0xc7, 0x51 }}
+
+extern EFI_GUID  gVariableServicesBbTestFunctionAssertionGuid043;
+
+#define EFI_TEST_VARIABLESERVICESBBTESTFUNCTION_ASSERTION_044_GUID \
+{ 0x1a28fa01, 0x135c, 0x4aeb, {0xa1, 0xb4, 0x68, 0x6a, 0xb, 0x53, 0xb2, 0x9 }}
+
+extern EFI_GUID  gVariableServicesBbTestFunctionAssertionGuid044;
+
+//
+// QueryVariableInfo
+//
+#if (EFI_SPECIFICATION_VERSION >= 0x00020000)
+#define QUERY_VAR_INFO_CONF_TEST_GUID \
+{ 0x61758774, 0x91a3, 0x47dd, {0xbd, 0xbd, 0xb8, 0x10, 0x94, 0xa5, 0xf6, 0x2d }}
+
+#define QUERY_VAR_INFO_FUNC_TEST_GUID \
+{ 0x9adc0f36, 0xfc24, 0x4731, {0x89, 0x81, 0x4a, 0xa5, 0xcd, 0xe3, 0x16, 0x34 }}
+
+#define EFI_TEST_VARIABLESERVICESBBTEST_QUERYVARINFO_ASSERTION_001_GUID \
+{ 0xad9578bf, 0x7a02, 0x4ef0, {0x8f, 0xe8, 0xd9, 0x45, 0x91, 0xa1, 0xe9, 0x31 }}
+
+extern EFI_GUID  gVariableServicesBbTestQueryVarInfoAssertionGuid001;
+
+#define EFI_TEST_VARIABLESERVICESBBTEST_QUERYVARINFO_ASSERTION_002_GUID \
+{ 0x5d13a732, 0x60ea, 0x42d5, {0xa0, 0x01, 0x43, 0x63, 0xd9, 0xb1, 0x8b, 0xf4 }}
+
+extern EFI_GUID  gVariableServicesBbTestQueryVarInfoAssertionGuid002;
+
+#define EFI_TEST_VARIABLESERVICESBBTEST_QUERYVARINFO_ASSERTION_003_GUID \
+{ 0xd3247b73, 0x5eb9, 0x4594, {0x8a, 0xb3, 0x27, 0xd9, 0x38, 0x4f, 0x3f, 0x13 }}
+
+extern EFI_GUID  gVariableServicesBbTestQueryVarInfoAssertionGuid003;
+
+#define EFI_TEST_VARIABLESERVICESBBTEST_QUERYVARINFO_ASSERTION_004_GUID \
+{ 0xe7f2eb9f, 0x1624, 0x45a9, {0xa2, 0x87, 0x3e, 0xa6, 0xf2, 0xf7, 0x4c, 0x5f }}
+
+extern EFI_GUID  gVariableServicesBbTestQueryVarInfoAssertionGuid004;
+
+#define EFI_TEST_VARIABLESERVICESBBTEST_QUERYVARINFO_ASSERTION_005_GUID \
+{ 0x2f9966ba, 0x0091, 0x4085, {0xbf, 0x9d, 0x09, 0xaa, 0x80, 0x9f, 0x94, 0x2e }}
+
+extern EFI_GUID  gVariableServicesBbTestQueryVarInfoAssertionGuid005;
+
+#define EFI_TEST_VARIABLESERVICESBBTEST_QUERYVARINFO_ASSERTION_007_GUID \
+{ 0xad6e6a8f, 0x3a05, 0x4183, {0xb6, 0x90, 0x40, 0xa8, 0x91, 0xd8, 0x62, 0xae }}
+
+extern EFI_GUID  gVariableServicesBbTestQueryVarInfoAssertionGuid007;
+
+#endif
+
+#define EFI_TEST_VARIABLESERVICESBBTEST_HWERRREC_ASSERTION_001_GUID \
+{ 0xc8126edc, 0x7197, 0x4113, {0xb7, 0xb6, 0xd5, 0x3d, 0x53, 0xe6, 0x72, 0xea }}
+
+extern EFI_GUID  gHwErrRecBbTestAssertionGuid001;
+
+#define EFI_TEST_VARIABLESERVICESBBTEST_HWERRREC_ASSERTION_002_GUID \
+{ 0xd8bd5c0a, 0x192f, 0x4501, {0xbc, 0x58, 0x89, 0xd3, 0x18, 0x60, 0x24, 0x5e }}
+
+extern EFI_GUID  gHwErrRecBbTestAssertionGuid002;
+
+#define EFI_TEST_VARIABLESERVICESBBTEST_HWERRREC_ASSERTION_003_GUID \
+{ 0xe1259932, 0xf39c, 0x465b, {0xb4, 0xe3, 0xa1, 0xb2, 0x77, 0x8b, 0xa1, 0x4 }}
+
+extern EFI_GUID  gHwErrRecBbTestAssertionGuid003;
+
+#define EFI_TEST_VARIABLESERVICESBBTEST_HWERRREC_ASSERTION_004_GUID \
+{ 0xf5b942c9, 0x1f0c, 0x4c45, {0x85, 0x72, 0xc4, 0x53, 0x79, 0x51, 0x50, 0xdf }}
+
+extern EFI_GUID  gHwErrRecBbTestAssertionGuid004;
