0a17e75b380b5e4ef50aebf9dbcd616e80d971aa
Bret Barkelew <brbarkel@microsoft.com>
2022-05-06 00:17:48 -0700

    Improve logging for Linux exceptions in unit tests
    Adding this define to the Cmocka lib enables Linux systems
    to produce more informative errors when a system exception
    occurs during a test run.
    
    Additionally, by adding an extra print we are assured of getting
    the line number cause of a potential hang during ASSERT.

--- UnitTestFrameworkPkg/Library/UnitTestLib/AssertCmocka.c
+++ UnitTestFrameworkPkg/Library/UnitTestLib/AssertCmocka.c
@@ -49,6 +49,7 @@ UnitTestAssertTrue (
   CHAR8  TempStr[MAX_STRING_SIZE];
 
   snprintf (TempStr, sizeof (TempStr), "UT_ASSERT_TRUE(%s:%x)", Description, Expression);
+  printf ("%s:%d %s\n", FileName, (INT32)LineNumber, TempStr);     // MU_CHANGE - Report source of ASSERTs in tests
   _assert_true (Expression, TempStr, FileName, (INT32)LineNumber);
 
   return Expression;
