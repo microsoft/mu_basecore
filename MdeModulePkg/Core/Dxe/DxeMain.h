47273abc00f86c68a74a97b28b4ba90e40d4a498
Joey Vagedes <joeyvagedes@microsoft.com>
2022-09-01 17:34:25 +0000

    Restore TPL requirements for WaitForEvent()
    Restore TPL Requirements for WaitForEvent() and allow a platform to conditionally install a protocol that provides
    an interface to the internal WaitForEvent() that does not have TPL requirements.

--- MdeModulePkg/Core/Dxe/DxeMain.h
+++ MdeModulePkg/Core/Dxe/DxeMain.h
@@ -14,6 +14,7 @@ SPDX-License-Identifier: BSD-2-Clause-Patent
 
 #include <Protocol/LoadedImage.h>
 #include <Protocol/GuidedSectionExtraction.h>
+#include <Protocol/InternalEventServices.h>
 #include <Protocol/DevicePath.h>
 #include <Protocol/Runtime.h>
 #include <Protocol/LoadFile.h>
@@ -1596,6 +1597,27 @@ CoreWaitForEvent (
 // MU_CHANGE begin
 
 /**
+  Stops execution until an event is signaled.
+
+  @param  NumberOfEvents         The number of events in the UserEvents array
+  @param  UserEvents             An array of EFI_EVENT
+  @param  UserIndex              Pointer to the index of the event which
+                                 satisfied the wait condition
+
+  @retval EFI_SUCCESS            The event indicated by Index was signaled.
+  @retval EFI_INVALID_PARAMETER  The event indicated by Index has a notification
+                                 function or Event was not a valid type
+
+**/
+EFI_STATUS
+EFIAPI
+CoreWaitForEventInternal (
+  IN UINTN      NumberOfEvents,
+  IN EFI_EVENT  *UserEvents,
+  OUT UINTN     *UserIndex
+  );
+
+/**
   Initialize the memory protection special region reporting.
 **/
 VOID
@@ -1605,6 +1627,23 @@ CoreInitializeMemoryProtectionSpecialRegions (
   );
 
 // MU_CHANGE end
+
+/**
+  Installs the internal version of Event Services that does not require
+  TPL_APPLICATION to execute.
+
+  @param  ImageHandle   A handle for the image that is initializing this protocol
+  @param  SystemTable   A pointer to the EFI system table
+
+  @retval EFI_SUCCESS           Driver initialized successfully
+  @retval EFI_OUT_OF_RESOURCES  Could not allocate needed resources
+
+**/
+EFI_STATUS
+EFIAPI
+InternalEventServicesInit (
+  VOID
+  );
 
 /**
   Closes an event and frees the event structure.
