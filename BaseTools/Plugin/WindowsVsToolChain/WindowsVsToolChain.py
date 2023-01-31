f6ba44028a7e9171ac5d921c7279fa5f4cf5bf11
Joey Vagedes <joey.vagedes@gmail.com>
2023-01-31 11:11:20 -0800

    Update edk2-pytool-library (#237)
    edk2-pytool-library v0.13.0 introduces a breaking change to the function
    FindWithVsWhere, affecting the WindowsVsToolChain plugin. This commit
    upgrades edk2-pytool-library and performs necessary integration steps.
    
    Signed-off-by: Joey Vagedes <joeyvagedes@microsoft.com>

--- BaseTools/Plugin/WindowsVsToolChain/WindowsVsToolChain.py
+++ BaseTools/Plugin/WindowsVsToolChain/WindowsVsToolChain.py
@@ -339,12 +339,17 @@ class WindowsVsToolChain(IUefiBuildPlugin):
         
         if(path is None):
             # Not specified...find latest
-            (rc, path) = FindWithVsWhere(vs_version=vs_version)
-            if rc == 0 and path is not None and os.path.exists(path):
+            try:
+                path = FindWithVsWhere(vs_version=vs_version)
+            except (EnvironmentError, ValueError, RuntimeError) as e:
+                self.Logger.error(str(e))
+                return None
+
+            if path is not None and os.path.exists(path):
                 self.Logger.debug("Found VS instance for %s", vs_version)
             else:
                 self.Logger.error(
-                    "Failed to find VS instance with VsWhere (%d)" % rc)
+                    f"VsWhere successfully executed, but could not find VS instance for {vs_version}.")
         return path
 
     def _get_vc_version(self, path, varname):
