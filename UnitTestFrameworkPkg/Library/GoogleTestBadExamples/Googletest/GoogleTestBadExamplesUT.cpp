aebd2771af1290dcc941f245e0693ae432821a8a
Kun Qin <kuqin@microsoft.com>
2023-05-05 18:17:45 -0700

    Added solution

--- UnitTestFrameworkPkg/Library/GoogleTestBadExamples/Googletest/GoogleTestBadExamplesUT.cpp
+++ UnitTestFrameworkPkg/Library/GoogleTestBadExamples/Googletest/GoogleTestBadExamplesUT.cpp
@@ -25,6 +25,7 @@ GetPolicy (
 // *----------------------------------------------------------------------------------*
 
 using namespace testing;
+using ::testing::HasSubstr;
 
 //
 // Declarations to handle usage of the UefiBootServiceTableLib by creating mock
@@ -45,7 +46,13 @@ MOCK_INTERFACE_DEFINITION (MockUefiBootServicesTableLib);
 MOCK_FUNCTION_DEFINITION (MockUefiBootServicesTableLib, gBS_LocateProtocol, 3, EFIAPI);
 
 static EFI_BOOT_SERVICES  LocalBs = {
+  // This does not build on MSVC due to std:c++ usage
   .LocateProtocol = gBS_LocateProtocol
+
+  // Solution: add below to test inf
+  // [BuildOptions]
+  //   MSFT:*_*_*_CC_FLAGS = /std:c++latest
+
 };
 
 EFI_BOOT_SERVICES  *gBS = &LocalBs;
@@ -78,7 +85,21 @@ MockUefiBootServicesTableLib UefiBootServicesTableLib;
 TEST_F (GoogleTestBadExample, GetPolicyExpectedAssertNotHappen) {
   EXPECT_DEATH (GetPolicy (), "");
 
-  // TODO: Solution here.
+  // Reason:
+  //   The reason this passed is due to the fact that the LocateProtocol did not modify the pointer on its way out.
+  //   Thus the call of policy protocol will fail. However, since this is a death test, the output will not be dumped
+  //   visibly.
+
+  // Solution:
+  //   - One should NEVER EVER use "" for failed string matching!!
+  //     Instead inspect what the real failure is and put that in the matcher field:
+  //     `EXPECT_DEATH (GetPolicy (), HasSubstr("GoogleTestBadExamplesSrc.c, line 42"));`
+  //   - The MdePkg/Include/Library/DebugLib.h should hook up to the system assert pipeline
+  //     so that the gtest can read error from stderr:
+  //     ```
+  //       [BuildOptions]
+  //         *_*_*_CC_FLAGS = -D GOOGLE_TESTING
+  //     ```
 }
 
 /**
@@ -93,7 +114,26 @@ TEST_F (GoogleTestBadExample, GetPolicyExpectAssert) {
 
   EXPECT_DEATH (GetPolicy (), "");
 
-  // TODO: Solution here.
+  // Reason:
+  //   The reason this failed is due to the GetPolicy () runs in child process, and the access to gBS_LocateProtocol
+  //   from the child process will not have the parent process notified, thus the parent process will percieve that
+  //   the gBS_LocateProtocol was never invoked.
+
+  // Solution:
+  //   - The MdePkg/Include/Library/DebugLib.h should hook up to the system assert pipeline
+  //     so that the gtest can read error from stderr:
+  //     ```
+  //       [BuildOptions]
+  //         *_*_*_CC_FLAGS = -D GOOGLE_TESTING
+  //     ```
+  //   - Move the expect call into the child process as well, so that it can be detected:
+  //     ```
+  //       EXPECT_DEATH ({
+  //       EXPECT_CALL (UefiBootServicesTableLib, gBS_LocateProtocol (&gPolicyProtocolGuid,_,_))
+  //         .WillOnce(Return (EFI_NOT_FOUND));
+  //       GetPolicy ();
+  //       }, HasSubstr("GoogleTestBadExamplesSrc.c, line 42"));
+  //     ```
 }
 
 
