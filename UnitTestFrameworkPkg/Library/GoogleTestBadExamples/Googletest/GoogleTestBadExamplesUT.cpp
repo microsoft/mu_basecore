7245b09ddda7d658e7974ddcea52bbd16f29d294
Kun Qin <kuqin@microsoft.com>
2023-06-01 00:36:41 -0700

    Update test with remove comments

--- UnitTestFrameworkPkg/Library/GoogleTestBadExamples/Googletest/GoogleTestBadExamplesUT.cpp
+++ UnitTestFrameworkPkg/Library/GoogleTestBadExamples/Googletest/GoogleTestBadExamplesUT.cpp
@@ -59,8 +59,6 @@ static EFI_BOOT_SERVICES  LocalBs = {
 
 EFI_BOOT_SERVICES  *gBS = &LocalBs;
 
-// TODO: Populate mocked policy here.
-
 /// ================================================================================================
 /// ================================================================================================
 ///
@@ -84,18 +82,17 @@ UTShimTest (
   IN UNIT_TEST_CONTEXT  Context
   )
 {
-  // pass in bad data, expect an assert
-  // mKnown_Bad_Config_Data is not in varlist format, so it should fail
+  // Use UT_EXPECT_ASSERT* to do death test.
   UT_EXPECT_ASSERT_FAILURE (GetPolicy (), NULL);
 
   return UNIT_TEST_PASSED;
 }
 
 /**
-  Unit test for GetPolicy, expected death will pass with just a warning if no mocked values supplied.
+  Unit test for GetPolicy, expected death by putting the expect_call
+  in the subprocess as well.
 
-  Expected: FAILED
-  Actual:   PASSED, with a warning
+  Status: PASSED
 **/
 TEST_F (GoogleTestBadExample, GetPolicyExpectedAssertNotHappen) {
   EXPECT_DEATH ({
@@ -103,29 +100,12 @@ TEST_F (GoogleTestBadExample, GetPolicyExpectedAssertNotHappen) {
       .WillOnce(Return (EFI_NOT_FOUND));
     GetPolicy ();
     }, HasSubstr("GoogleTestBadExamplesSrc.c, line 39"));
-
-  // Reason:
-  //   The reason this passed is due to the fact that the LocateProtocol did not modify the pointer on its way out.
-  //   Thus the call of policy protocol will fail. However, since this is a death test, the output will not be dumped
-  //   visibly.
-
-  // Solution:
-  //   - One should NEVER EVER use "" for failed string matching!!
-  //     Instead inspect what the real failure is and put that in the matcher field:
-  //     `EXPECT_DEATH (GetPolicy (), HasSubstr("GoogleTestBadExamplesSrc.c, line 39"));`
-  //   - The MdePkg/Include/Library/DebugLib.h should hook up to the system assert pipeline
-  //     so that the gtest can read error from stderr:
-  //     ```
-  //       [BuildOptions]
-  //         *_*_*_CC_FLAGS = -D GOOGLE_TESTING
-  //     ```
 }
 
 /**
-  Unit test for GetPolicy, expected death will have "1 leaked mock object".
+  Unit test for GetPolicy, expected death using UT_ASSERT*.
 
-  Expected: PASSED
-  Actual:   FAILED, due to an error regarding "1 leaked mock object".
+  Status: PASSED
 **/
 TEST_F (GoogleTestBadExample, GetPolicyExpectAssert) {
   UNIT_TEST_STATUS TestStatus;
@@ -135,27 +115,6 @@ TEST_F (GoogleTestBadExample, GetPolicyExpectAssert) {
 
   TestStatus = UTShimTest (NULL);
   EXPECT_EQ (TestStatus, (UNIT_TEST_STATUS)UNIT_TEST_PASSED);
-
-  // Reason:
-  //   The reason this failed is due to the GetPolicy () runs in child process, and the access to gBS_LocateProtocol
-  //   from the child process will not have the parent process notified, thus the parent process will percieve that
-  //   the gBS_LocateProtocol was never invoked.
-
-  // Solution:
-  //   - The MdePkg/Include/Library/DebugLib.h should hook up to the system assert pipeline
-  //     so that the gtest can read error from stderr:
-  //     ```
-  //       [BuildOptions]
-  //         *_*_*_CC_FLAGS = -D GOOGLE_TESTING
-  //     ```
-  //   - Move the expect call into the child process as well, so that it can be detected:
-  //     ```
-  //       EXPECT_DEATH ({
-  //       EXPECT_CALL (UefiBootServicesTableLib, gBS_LocateProtocol (&gEfiBootManagerPolicyProtocolGuid,_,_))
-  //         .WillOnce(Return (EFI_NOT_FOUND));
-  //       GetPolicy ();
-  //       }, HasSubstr("GoogleTestBadExamplesSrc.c, line 39"));
-  //     ```
 }
 
 
