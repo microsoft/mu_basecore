f7bfac53367b53e6e8b859bf823b0f9d581844d2
Chris Fernald <chfernal@microsoft.com>
2023-06-05 14:23:31 -0700

    Add ability for DXE library to be linked into a runtime driver

--- PolicyServicePkg/Library/DxePolicyLib/DxePolicy.c
+++ PolicyServicePkg/Library/DxePolicyLib/DxePolicy.c
@@ -16,6 +16,9 @@
 
 #include "../PolicyLibCommon.h"
 
+EFI_EVENT  mEfiExitBootServicesEvent;
+BOOLEAN    mEfiAtRuntime = FALSE;
+
 /**
   A private helper function to retrieve the policy service protocol.
 
@@ -23,6 +26,7 @@
 
   @retval         EFI_SUCCESS       Policy protocol was found.
   @retval         EFI_NOT_FOUND     Policy protocol was not found.
+  @retval         EFI_UNSUPPORTED   Policy service was called at runtime.
 **/
 EFI_STATUS
 GetPolicyInterface (
@@ -31,6 +35,11 @@ GetPolicyInterface (
 {
   EFI_STATUS              Status;
   STATIC POLICY_PROTOCOL  *mPolicyProtocol = NULL;
+
+  if (mEfiAtRuntime) {
+    mPolicyProtocol = NULL;
+    return EFI_UNSUPPORTED;
+  }
 
   Status = EFI_SUCCESS;
   if (mPolicyProtocol == NULL) {
@@ -49,5 +58,54 @@ GetPolicyInterface (
     *PolicyInterface = mPolicyProtocol;
   }
 
+  return Status;
+}
+
+/**
+  Set AtRuntime flag as TRUE after ExitBootServices.
+
+  @param[in]  Event   The Event that is being processed.
+  @param[in]  Context The Event Context.
+
+**/
+VOID
+EFIAPI
+RuntimeLibExitBootServicesEvent (
+  IN EFI_EVENT  Event,
+  IN VOID       *Context
+  )
+{
+  mEfiAtRuntime = TRUE;
+}
+
+/**
+  This constructor sets up a callback for ExitBootServices to ensure policy
+  service is not used at runtime.
+
+  @param[in] ImageHandle  The firmware allocated handle for the EFI image.
+  @param[in] SystemTable  A pointer to the EFI System Table.
+
+  @retval EFI_SUCCESS   Successfully initialized the policy library.
+  @retval Other         Error returned by a subroutine.
+
+**/
+EFI_STATUS
+EFIAPI
+PolicyLibConstructor (
+  IN EFI_HANDLE        ImageHandle,
+  IN EFI_SYSTEM_TABLE  *SystemTable
+  )
+{
+  EFI_STATUS  Status;
+
+  Status = gBS->CreateEvent (
+                  EVT_SIGNAL_EXIT_BOOT_SERVICES,
+                  TPL_NOTIFY,
+                  RuntimeLibExitBootServicesEvent,
+                  NULL,
+                  &mEfiExitBootServicesEvent
+                  );
+
+  ASSERT_EFI_ERROR (Status);
   return Status;
 }
