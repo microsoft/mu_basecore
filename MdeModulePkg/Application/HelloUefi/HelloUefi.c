249cc3461e28838fbe5786fdd34169a34c36d407
Douglas Flick <dougflick@microsoft.com>
2023-06-23 14:32:01 -0700

    init

---
+++ MdeModulePkg/Application/HelloUefi/HelloUefi.c
@@ -0,0 +1,50 @@
+/** @file
+  This sample application bases on HelloWorld PCD setting
+  to print "UEFI Hello World!" to the UEFI Console.
+
+  Copyright (c) 2006 - 2018, Intel Corporation. All rights reserved.<BR>
+  SPDX-License-Identifier: BSD-2-Clause-Patent
+
+**/
+
+#include <Uefi.h>
+#include <DebugLib.h>
+#include <Library/UefiBootServicesTableLib.h>
+#include <Library/UefiApplicationEntryPoint.h>
+
+/**
+  The user Entry Point for Application. The user code starts with this function
+  as the real entry point for the application.
+
+  @param[in] ImageHandle    The firmware allocated handle for the EFI image.
+  @param[in] SystemTable    A pointer to the EFI System Table.
+
+  @retval EFI_SUCCESS       The entry point is executed successfully.
+  @retval other             Some error occurs when executing this entry point.
+
+**/
+EFI_STATUS
+EFIAPI
+UefiMain (
+  IN EFI_HANDLE        ImageHandle,
+  IN EFI_SYSTEM_TABLE  *SystemTable
+  )
+{
+  EFI_STATUS  Status;
+  UINT32  Index;
+  EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL  *mTextOut;
+
+  Status = gBS->LocateProtocol (
+                  &gEfiSimpleTextOutProtocolGuid,
+                  NULL,
+                  (VOID **)&mTextOut
+                  );
+  if (EFI_ERROR (Status)) {
+    DEBUG ((DEBUG_ERROR, "Failed to locate Simple Text Out: Status(%r)\n", Status));
+    return Status;
+  }
+
+  mTextOut->OutputString (mTextOut, L"Hello Uefi\r\n");
+
+  return EFI_SUCCESS;
+}
