eab3322f3f159f7f0eb1586d6fcfafef5c96e427
Joey Vagedes <joey.vagedes@gmail.com>
2023-07-06 13:48:37 -0700

    MdeModulePkg/PciHostBridgeDxe: Ignore TypeIo for non X86/X64 architectures.
    Throughout PciHostBridgeDxe, functions iterate through all entries in
    the PCI_RESOURCE_TYPE enum. However TypeIo only applies for x86_64
    platforms. This changes the starting point of the PCI_RESOURCE_TYPE enum
    loops from 0 to PCI_RESOURCE_TYPE_ENUM_START and conditionally sets that
    value to TypeIo or TypeMem32 based on if the platform is x86_64 or not.
    
    Signed-off-by: Joey Vagedes <joey.vagedes@gmail.com>
    Co-authored-by: Michael Kubacki <michael.kubacki@microsoft.com>

--- MdeModulePkg/Bus/Pci/PciHostBridgeDxe/PciHostBridge.c
+++ MdeModulePkg/Bus/Pci/PciHostBridgeDxe/PciHostBridge.c
@@ -678,7 +678,8 @@ ResourceConflict (
        )
   {
     RootBridge = ROOT_BRIDGE_FROM_LINK (Link);
-    for (Index = TypeIo; Index < TypeMax; Index++) {
+    // MU_CHANGE - Start from PCI_RESOURCE_TYPE_ENUM_START
+    for (Index = PCI_RESOURCE_TYPE_ENUM_START; Index < TypeMax; Index++) {
       ResAllocNode = &RootBridge->ResAllocNode[Index];
 
       Descriptor->Desc         = ACPI_ADDRESS_SPACE_DESCRIPTOR;
@@ -921,7 +922,8 @@ NotifyPhase (
         RootBridge = ROOT_BRIDGE_FROM_LINK (Link);
         DEBUG ((DEBUG_INFO, " RootBridge: %s\n", RootBridge->DevicePathStr));
 
-        for (Index1 = TypeIo; Index1 < TypeBus; Index1++) {
+        // MU_CHANGE - Start from PCI_RESOURCE_TYPE_ENUM_START
+        for (Index1 = PCI_RESOURCE_TYPE_ENUM_START; Index1 < TypeBus; Index1++) {
           if (RootBridge->ResAllocNode[Index1].Status == ResNone) {
             ResNodeHandled[Index1] = TRUE;
           } else {
@@ -930,7 +932,8 @@ NotifyPhase (
             //
             MaxAlignment = 0;
             Index        = TypeMax;
-            for (Index2 = TypeIo; Index2 < TypeBus; Index2++) {
+            // MU_CHANGE - Start from PCI_RESOURCE_TYPE_ENUM_START
+            for (Index2 = PCI_RESOURCE_TYPE_ENUM_START; Index2 < TypeBus; Index2++) {
               if (ResNodeHandled[Index2]) {
                 continue;
               }
@@ -1135,7 +1138,8 @@ NotifyPhase (
            )
       {
         RootBridge = ROOT_BRIDGE_FROM_LINK (Link);
-        for (Index = TypeIo; Index < TypeBus; Index++) {
+        // MU_CHANGE - Start from PCI_RESOURCE_TYPE_ENUM_START
+        for (Index = PCI_RESOURCE_TYPE_ENUM_START; Index < TypeBus; Index++) {
           if (RootBridge->ResAllocNode[Index].Status == ResAllocated) {
             switch (Index) {
               case TypeIo:
@@ -1643,7 +1647,8 @@ GetProposedResources (
       }
 
       Descriptor = (EFI_ACPI_ADDRESS_SPACE_DESCRIPTOR *)Buffer;
-      for (Index = 0; Index < TypeBus; Index++) {
+      // MU_CHANGE - Start from PCI_RESOURCE_TYPE_ENUM_START
+      for (Index = PCI_RESOURCE_TYPE_ENUM_START; Index < TypeBus; Index++) {
         ResStatus = RootBridge->ResAllocNode[Index].Status;
         if (ResStatus != ResNone) {
           Descriptor->Desc    = ACPI_ADDRESS_SPACE_DESCRIPTOR;
