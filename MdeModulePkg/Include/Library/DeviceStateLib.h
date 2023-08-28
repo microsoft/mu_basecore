b2b8b054bbdbbaa8467f9626e9584af188c72340
Taylor Beebe <taylor.d.beebe@gmail.com>
2023-08-28 13:32:03 -0700

    Update DeviceStateLib Formatting to Match Coding Standards
    Description
    
    - [ ] Impacts functionality?
      - **Functionality** - Does the change ultimately impact how firmware functions?
      - Examples: Add a new library, publish a new PPI, update an algorithm, ...
    - [ ] Impacts security?
      - **Security** - Does the change have a direct security impact on an application,
        flow, or firmware?
      - Examples: Crypto algorithm change, buffer overflow fix, parameter
        validation improvement, ...
    - [ ] Breaking change?
      - **Breaking change** - Will anyone consuming this change experience a break
        in build or boot behavior?
      - Examples: Add a new library class, move a module to a different repo, call
        a function in a new library class in a pre-existing module, ...
    - [ ] Includes tests?
      - **Tests** - Does the change include any explicit test code?
      - Examples: Unit tests, integration tests, robot tests, ...
    - [ ] Includes documentation?
      - **Documentation** - Does the change contain explicit documentation additions
        outside direct code modifications (and comments)?
      - Examples: Update readme file, add feature readme file, link to documentation
        on an a separate Web page, ...
    
    How This Was Tested
    
    N/A
    
    Integration Instructions
    
    N/A

--- MdeModulePkg/Include/Library/DeviceStateLib.h
+++ MdeModulePkg/Include/Library/DeviceStateLib.h
@@ -1,58 +1,61 @@
 /** @file
-Functions used to support Getting and Setting Device States.
+  Library to get and set the device state
 
-Copyright (C) Microsoft Corporation. All rights reserved.
-SPDX-License-Identifier: BSD-2-Clause-Patent
-
-MU_CHANGE: new file
+  Copyright (C) Microsoft Corporation. All rights reserved.
+  SPDX-License-Identifier: BSD-2-Clause-Patent
 **/
 
 #ifndef __DEVICE_STATE_LIB_H__
 #define __DEVICE_STATE_LIB_H__
 
 //
-// DEFINE the possible device states here.
-// Current API is defined as a 31bit bitmask  (bit 32 is reserved for MAX)
+// DEFINE the possible device states.
+// Current API is defined as a 31-bit bitmask  (The 32nd bit is reserved for MAX)
 //
-#define DEVICE_STATE_SECUREBOOT_OFF             (1 << (0))
-#define DEVICE_STATE_MANUFACTURING_MODE         (1 << (1))
-#define DEVICE_STATE_DEVELOPMENT_BUILD_ENABLED  (1 << (2))
-#define DEVICE_STATE_SOURCE_DEBUG_ENABLED       (1 << (3))
-#define DEVICE_STATE_UNDEFINED                  (1 << (4))
-#define DEVICE_STATE_UNIT_TEST_MODE             (1 << (5))
+#define DEVICE_STATE_SECUREBOOT_OFF             BIT0
+#define DEVICE_STATE_MANUFACTURING_MODE         BIT1
+#define DEVICE_STATE_DEVELOPMENT_BUILD_ENABLED  BIT2
+#define DEVICE_STATE_SOURCE_DEBUG_ENABLED       BIT3
+#define DEVICE_STATE_UNDEFINED                  BIT4
+#define DEVICE_STATE_UNIT_TEST_MODE             BIT5
 
-#define DEVICE_STATE_PLATFORM_MODE_0  (1 << (20))
-#define DEVICE_STATE_PLATFORM_MODE_1  (1 << (21))
-#define DEVICE_STATE_PLATFORM_MODE_2  (1 << (22))
-#define DEVICE_STATE_PLATFORM_MODE_3  (1 << (23))
-#define DEVICE_STATE_PLATFORM_MODE_4  (1 << (24))
-#define DEVICE_STATE_PLATFORM_MODE_5  (1 << (25))
-#define DEVICE_STATE_PLATFORM_MODE_6  (1 << (26))
-#define DEVICE_STATE_PLATFORM_MODE_7  (1 << (27))
+#define DEVICE_STATE_PLATFORM_MODE_0  BIT20
+#define DEVICE_STATE_PLATFORM_MODE_1  BIT21
+#define DEVICE_STATE_PLATFORM_MODE_2  BIT22
+#define DEVICE_STATE_PLATFORM_MODE_3  BIT23
+#define DEVICE_STATE_PLATFORM_MODE_4  BIT24
+#define DEVICE_STATE_PLATFORM_MODE_5  BIT25
+#define DEVICE_STATE_PLATFORM_MODE_6  BIT26
+#define DEVICE_STATE_PLATFORM_MODE_7  BIT27
 
-#define DEVICE_STATE_MAX  (1 << (31))
+#define DEVICE_STATE_MAX  BIT31
 
 typedef UINT32 DEVICE_STATE;
 
 /**
-Function to Get current device state
-@retval the current DEVICE_STATE
+  Function to get current device state
+
+  @retval the current DEVICE_STATE
 **/
 DEVICE_STATE
 EFIAPI
 GetDeviceState (
+  VOID
   );
 
 /**
-Function to Add additional bits to the device state
+  Function to add additional bits to the device state
 
-@param AdditionalState - additional state to set active
-@retval Status of operation.  EFI_SUCCESS on successful update.
+  @param[in] AdditionalState    State to set active
+
+  @retval EFI_SUCCESS           Update was successful
+  @retval EFI_DEVICE_ERROR      Error updating device state
+  @retval EFI_OUT_OF_RESOURCES  Unable to set device state
 **/
-RETURN_STATUS
+EFI_STATUS
 EFIAPI
 AddDeviceState (
-  DEVICE_STATE  AdditionalState
+  IN DEVICE_STATE  AdditionalState
   );
 
 #endif
--- MdeModulePkg/Library/DeviceStateLib/DeviceStateLib.c
+++ MdeModulePkg/Library/DeviceStateLib/DeviceStateLib.c
@@ -1,11 +1,8 @@
 /** @file
-Functions used to support Getting and Setting Device States.
-This library uses the PcdDeviceStateBitmask dynamic PCD to store the device state
+  Library to get and set the device state
 
-Copyright (C) Microsoft Corporation. All rights reserved.
-SPDX-License-Identifier: BSD-2-Clause-Patent
-
-MU_CHANGE: new file
+  Copyright (C) Microsoft Corporation. All rights reserved.
+  SPDX-License-Identifier: BSD-2-Clause-Patent
 **/
 
 #include <PiPei.h>
@@ -14,29 +11,32 @@ MU_CHANGE: new file
 #include <Library/DebugLib.h>
 
 /**
-Function to Get current device state
-@retval the current DEVICE_STATE
+  Function to get current device state
+
+  @retval the current DEVICE_STATE
 **/
 DEVICE_STATE
 EFIAPI
 GetDeviceState (
+  VOID
   )
 {
-  DEVICE_STATE  DevState = PcdGet32 (PcdDeviceStateBitmask);
-
-  return DevState;
+  return (DEVICE_STATE)PcdGet32 (PcdDeviceStateBitmask);
 }
 
 /**
-Function to Add additional bits to the device state
+  Function to add additional bits to the device state
 
-@param AdditionalState - additional state to set active
-@retval Status of operation.  EFI_SUCCESS on successful update.
+  @param[in] AdditionalState    State to set active
+
+  @retval EFI_SUCCESS           Update was successful
+  @retval EFI_DEVICE_ERROR      Error updating device state
+  @retval EFI_OUT_OF_RESOURCES  Unable to set device state
 **/
-RETURN_STATUS
+EFI_STATUS
 EFIAPI
 AddDeviceState (
-  DEVICE_STATE  AdditionalState
+  IN DEVICE_STATE  AdditionalState
   )
 {
   DEVICE_STATE  DevState;
@@ -51,10 +51,10 @@ AddDeviceState (
   Status    = PcdSet32S (PcdDeviceStateBitmask, DevState);
   if (EFI_ERROR (Status)) {
     DEBUG ((DEBUG_ERROR, "%a - Error setting device state - %r\n", __FUNCTION__, Status));
-    return RETURN_DEVICE_ERROR;
+    return EFI_DEVICE_ERROR;
   }
 
   SetValue = PcdGet32 (PcdDeviceStateBitmask);
 
-  return ((SetValue == DevState) ? RETURN_SUCCESS : RETURN_OUT_OF_RESOURCES);
+  return ((SetValue == DevState) ? EFI_SUCCESS : EFI_OUT_OF_RESOURCES);
 }
