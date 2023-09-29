e93dbc746108e12250dfd55fb954e71f589a39b1
Ken Lautner <klautner@microsoft.com>
2023-09-29 15:22:54 -0700

    Updated copyright message and fixed grammar error

--- MdePkg/Library/BasePanicLib/PanicLib.c
+++ MdePkg/Library/BasePanicLib/PanicLib.c
@@ -2,7 +2,6 @@
   Base Panic Library.
 
   Copyright (c) Microsoft Corporation.
-  SPDX-License-Identifier: BSD-2-Clause-Patent
 
 **/
 
--- MdePkg/Library/BasePanicLibNull/PanicLib.c
+++ MdePkg/Library/BasePanicLibNull/PanicLib.c
@@ -2,7 +2,6 @@
   Base Panic Library with empty functions.
 
   Copyright (c) Microsoft Corporation.
-  SPDX-License-Identifier: BSD-2-Clause-Patent
 
 **/
 
@@ -15,14 +14,8 @@
 #define MAX_PANIC_MESSAGE_LENGTH  0x100
 
 /**
-  Prints a panic message containing a filename, line number, and description.
-  This is always followed by a dead loop.
-
-  Print a message of the form "PANIC <FileName>(<LineNumber>): <Description>\n"
-  to the debug output device.  Immediately after that CpuDeadLoop() is called.
-
-  If FileName is NULL, then a <FileName> string of "(NULL) Filename" is printed.
-  If Description is NULL, then a <Description> string of "(NULL) Description" is printed.
+  This would usually print a panic message containing a filename, line number,
+  and description.  For the NULL implementation nothing is done when panicking.
 
   @param  FileName     The pointer to the name of the source file that generated the panic condition.
   @param  LineNumber   The line number in the source file that generated the panic condition
