551935536bc25009a03c8f9a426d4910f501d666
Joey Vagedes <joeyvagedes@microsoft.com>
2023-11-02 15:36:58 -0700

    Force pipelines

--- .pytool/CISettings.py
+++ .pytool/CISettings.py
@@ -1,7 +1,7 @@
 # @file
 #
 # Copyright (c) Microsoft Corporation.
-# Copyright (c) 2020, Hewlett Packard Enterprise Development LP. All rights reserved.<BR>
+# Copyright (c) 2020, Hewlett Packard Enterprise Development LP. All rights reserved.<BR>.
 # Copyright (c) 2020 - 2021, ARM Limited. All rights reserved.<BR>
 # SPDX-License-Identifier: BSD-2-Clause-Patent
 ##
