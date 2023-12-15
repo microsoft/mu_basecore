b5f35bdf7c54827078634518a05eb7fc1561215a
Jiaxin Wu <jiaxin.wu@intel.com>
2023-12-15 09:18:57 +0800

    [CHERRY-PICK] UefiCpuPkg/PiSmmCpuDxeSmm: Simplify RunningApCount decrement
    To decrease the count of RunningApCount, InterlockedDecrement is
    enough to achieve that.
    
    This patch is to simplify RunningApCount decrement.
    
    Cc: Laszlo Ersek <lersek@redhat.com>
    Cc: Eric Dong <eric.dong@intel.com>
    Cc: Ray Ni <ray.ni@intel.com>
    Cc: Zeng Star <star.zeng@intel.com>
    Cc: Gerd Hoffmann <kraxel@redhat.com>
    Cc: Rahul Kumar <rahul1.kumar@intel.com>
    Signed-off-by: Jiaxin Wu <jiaxin.wu@intel.com>
    Reviewed-by: Ray Ni <ray.ni@intel.com>
    (cherry picked from commit ff9bd3fdcbda0b49fa78e7e527b1483ec8f2cd94)

--- UefiCpuPkg/PiSmmCpuDxeSmm/MpService.c
+++ UefiCpuPkg/PiSmmCpuDxeSmm/MpService.c
@@ -1325,7 +1325,7 @@ InternalSmmStartupAllAPs (
       // Decrease the count to mark this processor(AP or BSP) as finished.
       //
       if (ProcToken != NULL) {
-        WaitForSemaphore (&ProcToken->RunningApCount);
+        InterlockedDecrement (&ProcToken->RunningApCount);
       }
     }
   }
