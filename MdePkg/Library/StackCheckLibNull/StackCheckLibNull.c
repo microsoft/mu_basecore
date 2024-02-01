9e554f4a3e6e9a60c3525d2132b2a01145711779
Taylor Beebe <taylor.d.beebe@gmail.com>
2024-01-31 16:52:10 -0800

    Update StackCheckLibNull to Fix MSVC IA32 Builds and add GCC ARM Support
    Description
    
    MSVC IA32 requires the __security_check_cookie function to specify
    byte size (@__security_check_cookie@4). This change also declares
    __stack_chk_fail() in StackCheckLibNull.c to support GCC.
    
    - [x] Impacts functionality?
      - **Functionality** - Does the change ultimately impact how firmware functions?
      - Examples: Add a new library, publish a new PPI, update an algorithm, ...
    - [ ] Impacts security?
      - **Security** - Does the change have a direct security impact on an application,
        flow, or firmware?
      - Examples: Crypto algorithm change, buffer overflow fix, parameter
        validation improvement, ...
    - [ ] Breaking change?
      - **Breaking change** - Will anyone consuming this change experience a break
        in build or boot behavior?
      - Examples: Add a new library class, move a module to a different repo, call
        a function in a new library class in a pre-existing module, ...
    - [ ] Includes tests?
      - **Tests** - Does the change include any explicit test code?
      - Examples: Unit tests, integration tests, robot tests, ...
    - [ ] Includes documentation?
      - **Documentation** - Does the change contain explicit documentation additions
        outside direct code modifications (and comments)?
      - Examples: Update readme file, add feature readme file, link to documentation
        on an a separate Web page, ...
    
    How This Was Tested
    
    Tested on Q35 GCC and MSVC builds and an SBSA GCC build
    by purposefully overflowing the stack when the NULL library
    is in use.
    
    Integration Instructions
    
    N/A

--- MdePkg/Library/StackCheckLibNull/StackCheckLibNull.c
+++ MdePkg/Library/StackCheckLibNull/StackCheckLibNull.c
@@ -8,7 +8,16 @@
 #include <Uefi.h>
 
 #if defined (__GNUC__) || defined (__clang__)
-VOID  *__stack_chk_guard = (VOID *)0x0;
+VOID  *__stack_chk_guard = (VOID *)(UINTN)0x0;
+
+VOID
+EFIAPI
+__stack_chk_fail (
+  VOID
+  )
+{
+}
+
 #elif defined (_MSC_VER)
-UINTN  __security_cookie = 0x0;
+VOID  *__security_cookie = (VOID *)(UINTN)0x0;
 #endif
