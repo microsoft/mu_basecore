e3a139fa066c52271ac490a19b9d5bad6f9c7a18
Joey Vagedes <joey.vagedes@gmail.com>
2024-02-16 10:05:50 -0800

    remove edk2-basetools (#732)
    ## Description
    
    Removes edk2-basetools from pip-requirements.txt and any usage of it in
    the CISettings.py. The is done as there are changes in the build tools
    python source code that are available locally in BaseTools (as it is
    managed by Project Mu) that is not available in edk2-basetools.
    
    - [ ] Impacts functionality?
    - **Functionality** - Does the change ultimately impact how firmware
    functions?
    - Examples: Add a new library, publish a new PPI, update an algorithm,
    ...
    - [ ] Impacts security?
    - **Security** - Does the change have a direct security impact on an
    application,
        flow, or firmware?
      - Examples: Crypto algorithm change, buffer overflow fix, parameter
        validation improvement, ...
    - [ ] Breaking change?
    - **Breaking change** - Will anyone consuming this change experience a
    break
        in build or boot behavior?
    - Examples: Add a new library class, move a module to a different repo,
    call
        a function in a new library class in a pre-existing module, ...
    - [ ] Includes tests?
      - **Tests** - Does the change include any explicit test code?
      - Examples: Unit tests, integration tests, robot tests, ...
    - [ ] Includes documentation?
    - **Documentation** - Does the change contain explicit documentation
    additions
        outside direct code modifications (and comments)?
    - Examples: Update readme file, add feature readme file, link to
    documentation
        on an a separate Web page, ...
    
    ## How This Was Tested
    
    Verified the build system continues to use the local python source
    
    ## Integration Instructions
    
    N/A - only effects this repository's CI system.

--- .pytool/CISettings.py
+++ .pytool/CISettings.py
@@ -27,7 +27,6 @@ class Settings(CiBuildSettingsManager, UpdateSettingsManager, SetupSettingsManag
         self.ActualTargets = []
         self.ActualArchitectures = []
         self.ActualToolChainTag = ""
-        self.UseBuiltInBaseTools = None
         self.ActualScopes = None
 
     # ####################################################################################### #
@@ -35,10 +34,6 @@ class Settings(CiBuildSettingsManager, UpdateSettingsManager, SetupSettingsManag
     # ####################################################################################### #
 
     def AddCommandLineOptions(self, parserObj):
-        group = parserObj.add_mutually_exclusive_group()
-        group.add_argument("-force_piptools", "--fpt", dest="force_piptools", action="store_true", default=False, help="Force the system to use pip tools")
-        group.add_argument("-no_piptools", "--npt", dest="no_piptools", action="store_true", default=False, help="Force the system to not use pip tools")
-
         try:
             codeql_helpers.add_command_line_option(parserObj)
         except NameError:
@@ -46,11 +41,6 @@ class Settings(CiBuildSettingsManager, UpdateSettingsManager, SetupSettingsManag
 
     def RetrieveCommandLineOptions(self, args):
         super().RetrieveCommandLineOptions(args)
-        if args.force_piptools:
-            self.UseBuiltInBaseTools = True
-        if args.no_piptools:
-            self.UseBuiltInBaseTools = False
-
         try:
             self.codeql = codeql_helpers.is_codeql_enabled_on_command_line(args)
         except NameError:
@@ -150,22 +140,6 @@ class Settings(CiBuildSettingsManager, UpdateSettingsManager, SetupSettingsManag
             self.ActualToolChainTag = shell_environment.GetBuildVars().GetValue("TOOL_CHAIN_TAG", "")
 
             is_linux = GetHostInfo().os.upper() == "LINUX"
-
-            if self.UseBuiltInBaseTools is None:
-                is_linux = GetHostInfo().os.upper() == "LINUX"
-                # try and import the pip module for basetools
-                try:
-                    import edk2basetools
-                    self.UseBuiltInBaseTools = True
-                except ImportError:
-                    self.UseBuiltInBaseTools = False
-                    pass
-
-            if self.UseBuiltInBaseTools == True:
-                scopes += ('pipbuild-unix',) if is_linux else ('pipbuild-win',)
-                logging.warning("Using Pip Tools based BaseTools")
-            else:
-                logging.warning("Falling back to using in-tree BaseTools")
 
             if is_linux and self.ActualToolChainTag.upper().startswith("GCC"):
                 if "AARCH64" in self.ActualArchitectures:
