402589deb2fdb32ef18ababc88b87b33531ed36f
Michael Kubacki <michael.kubacki@microsoft.com>
2024-03-11 16:41:40 -0400

    MdeModulePkg: Define FVB Variable Storage PPI and Protocol GUIDs
    Defines the GUIDs that can be used by a Firmware Volume Block (FVB)
    variable storage implementations.
    
    Signed-off-by: Michael Kubacki <michael.kubacki@microsoft.com>

---
+++ MdeModulePkg/Include/Guid/FvbVariableStoragePpiInstance.h
@@ -0,0 +1,15 @@
+/** @file
+  GUID for the Firmware Volume Block (FVB) Variable Storage PPI instance.
+
+  Copyright (c) 2018, Intel Corporation. All rights reserved.<BR>
+  Copyright (c) Microsoft Corporation.
+
+  SPDX-License-Identifier: BSD-2-Clause-Patent
+**/
+
+#ifndef FVB_VARIABLE_STORAGE_PPI_INSTANCE_GUID_H_
+#define FVB_VARIABLE_STORAGE_PPI_INSTANCE_GUID_H_
+
+extern EFI_GUID  gFvbVariableStoragePpiInstanceGuid;
+
+#endif
---
+++ MdeModulePkg/Include/Guid/FvbVariableStorageProtocolInstance.h
@@ -0,0 +1,15 @@
+/** @file
+  GUID for the Firmware Volume Block (FVB) Variable Storage Protocol instance.
+
+  Copyright (c) 2018, Intel Corporation. All rights reserved.<BR>
+  Copyright (c) Microsoft Corporation.
+
+  SPDX-License-Identifier: BSD-2-Clause-Patent
+**/
+
+#ifndef FVB_VARIABLE_STORAGE_PROTOCOL_INSTANCE_GUID_H_
+#define FVB_VARIABLE_STORAGE_PROTOCOL_INSTANCE_GUID_H_
+
+extern EFI_GUID  gFvbVariableStorageProtocolInstanceGuid;
+
+#endif
