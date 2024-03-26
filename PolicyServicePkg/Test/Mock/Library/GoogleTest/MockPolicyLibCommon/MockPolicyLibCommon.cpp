a70532802b6e27d247eaf19dc06a70528d6f31df
Bhavani Subramanian <v-bhavanisu@microsoft.com>
2024-03-26 16:27:04 -0700

    Copyright information corrected as per PR comments

--- PolicyServicePkg/Test/Mock/Library/GoogleTest/MockPolicyLibCommon/MockPolicyLibCommon.cpp
+++ PolicyServicePkg/Test/Mock/Library/GoogleTest/MockPolicyLibCommon/MockPolicyLibCommon.cpp
@@ -2,7 +2,7 @@
   Google Test mocks for PolicyLibCommon
 
   Copyright (c) Microsoft Corporation.
-  Your use of this software is governed by the terms of the Microsoft agreement under which you obtained the software.
+  SPDX-License-Identifier: BSD-2-Clause-Patent
 **/
 
 #include <GoogleTest/Library/MockPolicyLibCommon.h>
