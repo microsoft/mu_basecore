1ce9fdb666610810b74e175cacbb7a64de9d957c
Ken Lautner <klautner@microsoft.com>
2024-04-04 12:14:42 -0700

    Updated LinuxCFLAGS to match the change made from edk2

--- BaseTools/Source/Python/Workspace/DscBuildData.py
+++ BaseTools/Source/Python/Workspace/DscBuildData.py
@@ -91,7 +91,7 @@ PcdMakefileHeader = '''
 '''
 
 WindowsCFLAGS = 'CFLAGS = $(CFLAGS) /wd4200 /wd4034 /wd4101 '
-LinuxCFLAGS = 'BUILD_CFLAGS += -Wno-pointer-to-int-cast -Wno-unused-variable '
+LinuxCFLAGS = 'CFLAGS += -Wno-pointer-to-int-cast -Wno-unused-variable '
 PcdMakefileEnd = r'''
 !INCLUDE $(BASE_TOOLS_PATH)\Source\C\Makefiles\ms.common
 !INCLUDE $(BASE_TOOLS_PATH)\Source\C\Makefiles\ms.app
