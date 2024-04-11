3ce26948d0d8c755b01de17f2735ae7b0b2a6e86
Taylor Beebe <taylor.d.beebe@gmail.com>
2024-04-11 08:16:30 -0700

    update based on feedback

--- MdeModulePkg/Core/Dxe/Image/Image.c
+++ MdeModulePkg/Core/Dxe/Image/Image.c
@@ -663,13 +663,15 @@ CoreLoadPeImage (
   // Or, if our memory protection policy specifies that we shouldn't allow such images, return a failure.
   if (!(Image->ImageContext.SupportsNx) && (Image->ImageContext.ImageType == EFI_IMAGE_SUBSYSTEM_EFI_APPLICATION)) {
     if (gDxeMps.ImageProtectionPolicy.Fields.BlockImagesWithoutNxFlag) {
-      DEBUG ((DEBUG_ERROR, "\n"));
-      DEBUG ((DEBUG_ERROR, "The platform attempted to load an EFI application which does not have the NX_COMPAT DLL Charactersistic.\n"));
-      DEBUG ((DEBUG_ERROR, "This is not allowed by the platform memory protection policy and so the image will not be loaded.\n"));
-      DEBUG ((DEBUG_ERROR, "This can be fixed by setting the NX_COMPAT flag in the image's PE header after validating that the\n"));
-      DEBUG ((DEBUG_ERROR, "image is safe to run with enhanced memory protections enabled. The image can also be executed in compatibility\n"));
-      DEBUG ((DEBUG_ERROR, "mode by updating the platform DXE_MEMORY_PROTECTION_SETTINGS so ImageProtectionPolicy.BlockImagesWithoutNxFlag is 0.\n"));
-      DEBUG ((DEBUG_ERROR, "Compatibility mode reduces firmware security so careful consideration should be made.\n"));
+      DEBUG ((DEBUG_ERROR, "\nThe platform attempted to load an EFI application which does not have the NX_COMPAT DLL\n"));
+      DEBUG ((DEBUG_ERROR, "Characteristic. This is not allowed by the platform memory protection policy so the\n"));
+      DEBUG ((DEBUG_ERROR, "image will not be loaded. This can be fixed by setting the NX_COMPAT flag in the image's\n"));
+      DEBUG ((DEBUG_ERROR, "PE header after validating that the image is safe to run with enhanced memory\n"));
+      DEBUG ((DEBUG_ERROR, "protection enabled (see https://microsoft.github.io/mu/WhatAndWhy/enhancedmemoryprotection\n"));
+      DEBUG ((DEBUG_ERROR, "for more info on this mode). The image can also be executed in compatibility mode by\n"));
+      DEBUG ((DEBUG_ERROR, "updating the platform DXE_MEMORY_PROTECTION_SETTINGS so BlockImagesWithoutNxFlag is 0.\n"));
+      DEBUG ((DEBUG_ERROR, "Compatibility mode reduces firmware security so careful consideration should be made\n"));
+      DEBUG ((DEBUG_ERROR, "before allowing it to be enabled.\n"));
       Status = EFI_SECURITY_VIOLATION;
       ASSERT_EFI_ERROR (Status);
       return Status;
