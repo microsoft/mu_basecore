171e515c6daaa9a06c0d0ffc7d2a4de0c48b2fe3
Ken Lautner <klautner@microsoft.com>
2024-06-05 16:56:23 -0700

    Changed CpuDeadLoops to panic calls

--- UefiCpuPkg/PiSmmCpuDxeSmm/PiSmmCpuDxeSmm.c
+++ UefiCpuPkg/PiSmmCpuDxeSmm/PiSmmCpuDxeSmm.c
@@ -1153,7 +1153,7 @@ PiCpuSmmEntry (
   Status                 = GetSmBase (mMaxNumberOfCpus, &mCpuHotPlugData.SmBase);
   if (Status == EFI_OUT_OF_RESOURCES) {
     ASSERT (Status != EFI_OUT_OF_RESOURCES);
-    CpuDeadLoop ();
+    PANIC ("Not enough space for mCpuHotPlugData.SmBase");
   }
 
   if (!EFI_ERROR (Status)) {
@@ -1165,7 +1165,7 @@ PiCpuSmmEntry (
       DEBUG ((DEBUG_ERROR, "The Range of Smbase in SMRAM is not enough -- Required TileSize = 0x%08x, Actual TileSize = 0x%08x\n", TileSize, SIZE_8KB));
       FreePool (mCpuHotPlugData.SmBase);
       FreePool (gSmmCpuPrivate->ProcessorInfo);
-      CpuDeadLoop ();
+      PANIC ("TileSize larger than 8KB");
       return RETURN_BUFFER_TOO_SMALL;
     }
 
@@ -1181,7 +1181,7 @@ PiCpuSmmEntry (
     mCpuHotPlugData.SmBase = (UINTN *)AllocatePool (sizeof (UINTN) * mMaxNumberOfCpus);
     if (mCpuHotPlugData.SmBase == NULL) {
       ASSERT (mCpuHotPlugData.SmBase != NULL);
-      CpuDeadLoop ();
+      PANIC ("mCpuHotPlugData.SmBase is NULL");
     }
 
     //
@@ -1195,7 +1195,7 @@ PiCpuSmmEntry (
     Buffer      = AllocateAlignedCodePages (BufferPages, SIZE_4KB);
     if (Buffer == NULL) {
       DEBUG ((DEBUG_ERROR, "Failed to allocate %Lu pages.\n", (UINT64)BufferPages));
-      CpuDeadLoop ();
+      PANIC ("Failed to allocate buffer for all the tiles");
       return EFI_OUT_OF_RESOURCES;
     }
 
