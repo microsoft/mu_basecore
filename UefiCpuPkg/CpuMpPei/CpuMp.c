7d47eefb0588c3d9c052052108c73fb90c1d29f5
John Schock <joschock@microsoft.com>
2024-06-25 08:43:52 -0700

    UefiCpuPkg: Implement PEI Non-Blocking AP Dispatch
    This patch adds the support for the previously defined MP services
    non-blocking dispatch in PEI functionality.

--- UefiCpuPkg/CpuMpPei/CpuMp.c
+++ UefiCpuPkg/CpuMpPei/CpuMp.c
@@ -20,6 +20,7 @@ EFI_PEI_MP_SERVICES_PPI  mMpServicesPpi = {
   PeiSwitchBSP,
   PeiEnableDisableAP,
   PeiWhoAmI,
+  PeiStartupThisAPNonBlocking,  // MU_CHANGE
 };
 
 /**
@@ -396,3 +397,69 @@ PeiWhoAmI (
 {
   return MpInitLibWhoAmI (ProcessorNumber);
 }
+
+// MU_CHANGE - Add basic support for non-blocking AP dispatch in PEI.
+
+/**
+  This service lets the caller get one enabled AP to execute a caller-provided
+  function. This service may only be called from the BSP.
+
+  This function is used to dispatch one enabled AP to the function specified by
+  Procedure passing in the argument specified by ProcedureArgument.
+  The execution is in non-blocking mode. The BSP continues executing immediately
+  after starting the AP.
+
+  If an attempt is made to dispatch a blocking or non-blocking task on the AP
+  while it is running a non-blocking task, that dispatch will block until the
+  AP completes the current task.
+
+  No timeout is specified - failure of the AP to complete the task is fatal. If
+  the AP crashes or fails to return from Procedure, then the next attempt to
+  dispatch blocking or non-blocking tasks on the AP will hang waiting on the AP.
+  No attempt is made to reset or recover the AP in this state.
+
+  @param[in] PeiServices          An indirect pointer to the PEI Services Table
+                                  published by the PEI Foundation.
+  @param[in] This                 A pointer to the EFI_PEI_MP_SERVICES_PPI instance.
+  @param[in] Procedure            A pointer to the function to be run on enabled APs of
+                                  the system.
+  @param[in] ProcessorNumber      The handle number of the AP. The range is from 0 to the
+                                  total number of logical processors minus 1. The total
+                                  number of logical processors can be retrieved by
+                                  EFI_PEI_MP_SERVICES_PPI.GetNumberOfProcessors().
+  @param[in] ProcedureArgument    The parameter passed into Procedure for all APs.
+
+  @retval EFI_SUCCESS             Indicates that the procedure was successfully
+                                  started on the AP
+  @retval EFI_DEVICE_ERROR        The calling processor is an AP.
+  @retval EFI_NOT_FOUND           The processor with the handle specified by
+                                  ProcessorNumber does not exist.
+  @retval EFI_INVALID_PARAMETER   ProcessorNumber specifies the BSP or disabled AP.
+  @retval EFI_INVALID_PARAMETER   Procedure is NULL.
+**/
+EFI_STATUS
+EFIAPI
+PeiStartupThisAPNonBlocking (
+  IN  CONST EFI_PEI_SERVICES   **PeiServices,
+  IN  EFI_PEI_MP_SERVICES_PPI  *This,
+  IN  EFI_AP_PROCEDURE         Procedure,
+  IN  UINTN                    ProcessorNumber,
+  IN  VOID                     *ProcedureArgument      OPTIONAL
+  )
+{
+  // Note: MpLib uses WaitEvent!=NULL as a trigger to execute in non-blocking
+  // mode but delegates actual usage of it as an event to the DxeMpLib wrapper.
+  // So we set it to '1' to allow startup of the AP in non-blocking mode, but
+  // in the PEI implementation its role is simply as a boolean flag indicating
+  // non-blocking mode - it is not an actual "Event" in the DXE sense.
+  return MpInitLibStartupThisAP (
+           Procedure,
+           ProcessorNumber,
+           (EFI_EVENT)1,
+           0,
+           ProcedureArgument,
+           NULL
+           );
+}
+
+// MU_CHANGE - End Add basic support for non-blocking AP dispatch in PEI.
