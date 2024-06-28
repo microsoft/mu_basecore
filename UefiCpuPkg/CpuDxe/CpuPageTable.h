1bd23f300c2efca4daf6ea5fbec64dde20ac97fe
Taylor Beebe <taylor.d.beebe@gmail.com>
2024-06-28 15:44:30 -0700

    UefiCpuPkg: Produce MemoryNonStopProtocol
    This patch provides an x86 implementation of the memory nonstop protocol
    to clear page faults when they occur to ease debugging and testing of
    memory attributes.

--- UefiCpuPkg/CpuDxe/CpuPageTable.h
+++ UefiCpuPkg/CpuDxe/CpuPageTable.h
@@ -11,6 +11,7 @@
 
 #include <IndustryStandard/PeImage.h>
 #include <Protocol/MemoryAttribute.h>
+#include <Protocol/MemoryProtectionNonstopMode.h> // MU_CHANGE
 
 #define PAGE_TABLE_LIB_PAGING_CONTEXT_IA32_X64_ATTRIBUTES_PSE              BIT0
 #define PAGE_TABLE_LIB_PAGING_CONTEXT_IA32_X64_ATTRIBUTES_PAE              BIT1
@@ -165,4 +166,17 @@ InstallEfiMemoryAttributeProtocol (
   IN EFI_HANDLE  Handle
   );
 
+// MU_CHANGE START
+
+/**
+  Install Memory Protection Nonstop Protocol.
+
+  @param Handle A pointer to the EFI_HANDLE on which the interface is to be installed
+**/
+VOID
+InstallMemoryProtectionNonstopModeProtocol (
+  IN EFI_HANDLE  Handle
+  );
+
+// MU_CHANGE END
 #endif
