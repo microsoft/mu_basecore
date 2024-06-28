f3a3282d6efb4af0bc3b2778c68e7673ca6c4be4
Taylor Beebe <taylor.d.beebe@gmail.com>
2024-06-28 13:02:58 -0700

    MdeModulePkg: DxeMain: Use DxeMemoryProtectionHobLib
    Update DxeMain to use the DxeMemoryProtectionHobLib to get
    memory protection information from instead of PCDs.

--- MdeModulePkg/Core/Dxe/DxeMain.h
+++ MdeModulePkg/Core/Dxe/DxeMain.h
@@ -88,6 +88,7 @@ SPDX-License-Identifier: BSD-2-Clause-Patent
 #include <Library/DebugAgentLib.h>
 #include <Library/CpuExceptionHandlerLib.h>
 #include <Library/OrderedCollectionLib.h>
+#include <Library/DxeMemoryProtectionHobLib.h>   // MU_CHANGE
 
 //
 // attributes for reserved memory before it is promoted to system memory
