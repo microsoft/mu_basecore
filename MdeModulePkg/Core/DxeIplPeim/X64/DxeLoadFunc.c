f1f343b74ee4393b425bcfb213a9dc4498a36ba5
Taylor Beebe <taylor.d.beebe@gmail.com>
2024-06-28 13:14:11 -0700

    [CHERRY-PICK] MdeModulePkg: DxeIpl: Update to Memory Protection Hob
    Now that the Memory Protection HOB exists, update DxeIpl to defer
    decisions on memory protections to it. This includes always
    building the page table, move null page detection to DXE, and
    deferring all memory protection decisions to DXE.
    
    This commit is cherry-picked from
    Mu commit 7d5a58b7168a7ef5b99e41874010913409538a41 but revised to
    apply on top of edk2 commit
    91bb5cee36fe52ef012ddbead4f5932b3f9fd862.

--- MdeModulePkg/Core/DxeIplPeim/X64/DxeLoadFunc.c
+++ MdeModulePkg/Core/DxeIplPeim/X64/DxeLoadFunc.c
@@ -81,24 +81,25 @@ HandOffToDxeCore (
   GhcbSize = PcdGet64 (PcdGhcbSize);
 
   PageTables = 0;
-  if (FeaturePcdGet (PcdDxeIplBuildPageTables)) {
-    //
-    // Create page table and save PageMapLevel4 to CR3
-    //
-    PageTables = CreateIdentityMappingPageTables (
-                   (EFI_PHYSICAL_ADDRESS)(UINTN)BaseOfStack,
-                   STACK_SIZE,
-                   (EFI_PHYSICAL_ADDRESS)(UINTN)GhcbBase,
-                   GhcbSize
-                   );
-  } else {
-    //
-    // Set NX for stack feature also require PcdDxeIplBuildPageTables be TRUE
-    // for the DxeIpl and the DxeCore are both X64.
-    //
-    ASSERT (PcdGetBool (PcdSetNxForStack) == FALSE);
-    ASSERT (PcdGetBool (PcdCpuStackGuard) == FALSE);
-  }
+  // MU_CHANGE START Always build page tables
+  // if (FeaturePcdGet (PcdDxeIplBuildPageTables)) {
+  //
+  // Create page table and save PageMapLevel4 to CR3
+  //
+  PageTables = CreateIdentityMappingPageTables (
+                 (EFI_PHYSICAL_ADDRESS)(UINTN)BaseOfStack,
+                 STACK_SIZE,
+                 (EFI_PHYSICAL_ADDRESS)(UINTN)GhcbBase,
+                 GhcbSize
+                 );
+  // } else {
+  //   //
+  //   // Set NX for stack feature also require PcdDxeIplBuildPageTables be TRUE
+  //   // for the DxeIpl and the DxeCore are both X64.
+  //   //
+  //   ASSERT (PcdGetBool (PcdSetNxForStack) == FALSE);
+  //   ASSERT (PcdGetBool (PcdCpuStackGuard) == FALSE);
+  // }
 
   //
   // End of PEI phase signal
@@ -106,10 +107,10 @@ HandOffToDxeCore (
   Status = PeiServicesInstallPpi (&gEndOfPeiSignalPpi);
   ASSERT_EFI_ERROR (Status);
 
-  if (FeaturePcdGet (PcdDxeIplBuildPageTables)) {
-    AsmWriteCr3 (PageTables);
-  }
-
+  // if (FeaturePcdGet (PcdDxeIplBuildPageTables)) {
+  AsmWriteCr3 (PageTables);
+  // }
+  // MU_CHANGE END
   //
   // Update the contents of BSP stack HOB to reflect the real stack info passed to DxeCore.
   //
