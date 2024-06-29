b531faa6194d9f637d84d10f75f69dcfde72f0fc
Bret Barkelew <brbarkel@microsoft.com>
2024-06-28 17:23:03 -0700

    MdeModulePkg: Removes short packet message and resets the RingDequeue
    The UEFI XHCI implemenation does not check for or respect the case in
    which an endpoint transfer event ring becomes full. It will overwrite
    existing entries that have not yet been or are currently being processed
    by the XHCI controller. This can lead to data corruption and instability
    in the XHCI/USB stack.
    
    The XHCI spec makes clear the importance of ensuring that the event ring
    is not full. Will attempt to force the ring full condition to determine
    how the system handles it.
    
    This change was split from this commit:
    https://github.com/microsoft/mu_basecore/commit/7a38833070cf4499e8b7c2d485b5b6e1070e1eb0

--- MdeModulePkg/Bus/Pci/XhciDxe/XhciSched.c
+++ MdeModulePkg/Bus/Pci/XhciDxe/XhciSched.c
@@ -1207,6 +1207,8 @@ XhcCheckUrbResult (
       continue;
     }
 
+    CheckedUrb->Ring->RingDequeue = TRBPtr;         // MU_CHANGE - 274185
+
     switch (EvtTrb->Completecode) {
       case TRB_COMPLETION_STALL_ERROR:
         CheckedUrb->Result  |= EFI_USB_ERR_STALL;
@@ -1247,9 +1249,11 @@ XhcCheckUrbResult (
 
       case TRB_COMPLETION_SHORT_PACKET:
       case TRB_COMPLETION_SUCCESS:
-        if (EvtTrb->Completecode == TRB_COMPLETION_SHORT_PACKET) {
-          DEBUG ((DEBUG_VERBOSE, "XhcCheckUrbResult: short packet happens!\n"));
-        }
+        // MU_CHANGE [BEGIN] - 161866
+        // if (EvtTrb->Completecode == TRB_COMPLETION_SHORT_PACKET) {
+        // DEBUG ((DEBUG_VERBOSE, "XhcCheckUrbResult: short packet happens!\n"));
+        // }
+        // MU_CHANGE [END]
 
         TRBType = (UINT8)(TRBPtr->Type);
         if ((TRBType == TRB_TYPE_DATA_STAGE) ||
@@ -2079,6 +2083,7 @@ XhcSyncTrsRing (
 
   if (TrsTrb != TrsRing->RingEnqueue) {
     TrsRing->RingEnqueue = TrsTrb;
+    ASSERT (TrsTrb != TrsRing->RingDequeue);           // MU_CHANGE 274185
   }
 
   //
@@ -3663,6 +3668,12 @@ XhcSetTrDequeuePointer (
   if (EFI_ERROR (Status) || (EvtTrb == NULL)) {
     DEBUG ((DEBUG_ERROR, "XhcSetTrDequeuePointer: Set TR Dequeue Pointer Failed, Status = %r\n", Status));
   }
+  // MU_CHANGE [BEGIN] - 274185
+  else {
+    Urb->Ring->RingDequeue = Urb->Ring->RingEnqueue;
+  }
+
+  // MU_CHANGE [END]
 
   return Status;
 }
