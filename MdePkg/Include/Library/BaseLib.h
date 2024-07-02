4969b55336aa45859a8f594fca5c175506bc81a8
Sean Brogan <sean.brogan@microsoft.com>
2024-07-02 13:14:31 -0700

    MdePkg: Add EnableInterruptsAndSleep Function
    This patch adds support for IA32 and X64 platforms to enable
    interrupts and sleep.

--- MdePkg/Include/Library/BaseLib.h
+++ MdePkg/Include/Library/BaseLib.h
@@ -5160,6 +5160,20 @@ LongJump (
   IN      UINTN                     Value
   );
 
+// MU_CHANGE: Start
+
+/**
+  Enables CPU interrupts and then waits for an interrupt to arrive.
+
+**/
+VOID
+EFIAPI
+EnableInterruptsAndSleep (
+  VOID
+  );
+
+// MU_CHANGE: End
+
 /**
   Enables CPU interrupts.
 
