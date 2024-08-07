d51939b817ed0159a6bc30f891d3b9836ffd5701
TsunFeng <v-tshuang@microsoft.com>
2024-08-07 23:01:39 +0800

    Create mocks for PlatformHookLib and PciLib

---
+++ MdeModulePkg/Test/Mock/Library/GoogleTest/MockPlatformHookLib/MockPlatformHookLib.cpp
@@ -0,0 +1,12 @@
+/** @file MockPlatformHookLib.cpp
+  Mock instance of the PCI Host Bridge Library.
+
+  Copyright (c) 2023, Intel Corporation. All rights reserved.
+  SPDX-License-Identifier: BSD-2-Clause-Patent
+**/
+
+#include <GoogleTest/Library/MockPlatformHookLib.h>
+
+MOCK_INTERFACE_DEFINITION (MockPlatformHookLib);
+
+MOCK_FUNCTION_DEFINITION (MockPlatformHookLib, PlatformHookSerialPortInitialize, 0, EFIAPI);
