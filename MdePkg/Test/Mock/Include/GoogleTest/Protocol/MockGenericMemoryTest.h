083e766f380ec20ec268d6c701e90491fee03305
YiTa Wu (AMI US Holdings Inc) <v-yitawu@microsoft.com>
2024-08-23 14:36:42 +0800

    Move Protocol Definition to header file to match new spec

--- MdePkg/Test/Mock/Include/GoogleTest/Protocol/MockGenericMemoryTest.h
+++ MdePkg/Test/Mock/Include/GoogleTest/Protocol/MockGenericMemoryTest.h
@@ -59,8 +59,21 @@ struct MockMemTestProtocol {
     );
 };
 
+MOCK_INTERFACE_DEFINITION (MockMemTestProtocol);
+MOCK_FUNCTION_DEFINITION (MockMemTestProtocol, MemoryTestInit, 3, EFIAPI);
+MOCK_FUNCTION_DEFINITION (MockMemTestProtocol, PerformMemoryTest, 5, EFIAPI);
+MOCK_FUNCTION_DEFINITION (MockMemTestProtocol, Finished, 1, EFIAPI);
+MOCK_FUNCTION_DEFINITION (MockMemTestProtocol, CompatibleRangeTest, 3, EFIAPI);
+
+EFI_GENERIC_MEMORY_TEST_PROTOCOL  MEMORY_TEST_PROTOCOL_INSTANCE = {
+  MemoryTestInit,                                                                       // EFI_MEMORY_TEST_INIT
+  PerformMemoryTest,                                                                    // EFI_PERFORM_MEMORY_TEST
+  Finished,                                                                             // EFI_MEMORY_TEST_FINISHED
+  CompatibleRangeTest                                                                   // EFI_MEMORY_TEST_COMPATIBLE_RANGE
+};
+
 extern "C" {
-  extern EFI_GENERIC_MEMORY_TEST_PROTOCOL  *gMemTestProtocol;
+  EFI_GENERIC_MEMORY_TEST_PROTOCOL  *gMemTestProtocol = &MEMORY_TEST_PROTOCOL_INSTANCE;
 }
 
 #endif // MOCK_GENERIC_MEMORY_TEST_H
