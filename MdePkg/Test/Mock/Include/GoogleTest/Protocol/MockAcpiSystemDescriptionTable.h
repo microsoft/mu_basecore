d3fbd2be9bdd72e4d699a440ccb089e211147600
TsunFeng <v-tshuang@microsoft.com>
2024-08-27 11:43:52 +0800

    Remove all the protocol cpp for new guidelines

--- MdePkg/Test/Mock/Include/GoogleTest/Protocol/MockAcpiSystemDescriptionTable.h
+++ MdePkg/Test/Mock/Include/GoogleTest/Protocol/MockAcpiSystemDescriptionTable.h
@@ -112,4 +112,32 @@ extern "C" {
   extern EFI_ACPI_SDT_PROTOCOL  *gAcpiSdtProtocol;
 }
 
+MOCK_INTERFACE_DEFINITION (MockAcpiSdtProtocol);
+MOCK_FUNCTION_DEFINITION (MockAcpiSdtProtocol, GetAcpiTable, 4, EFIAPI);
+MOCK_FUNCTION_DEFINITION (MockAcpiSdtProtocol, RegisterNotify, 2, EFIAPI);
+MOCK_FUNCTION_DEFINITION (MockAcpiSdtProtocol, Open, 2, EFIAPI);
+MOCK_FUNCTION_DEFINITION (MockAcpiSdtProtocol, OpenSdt, 2, EFIAPI);
+MOCK_FUNCTION_DEFINITION (MockAcpiSdtProtocol, Close, 1, EFIAPI);
+MOCK_FUNCTION_DEFINITION (MockAcpiSdtProtocol, GetChild, 2, EFIAPI);
+MOCK_FUNCTION_DEFINITION (MockAcpiSdtProtocol, GetOption, 5, EFIAPI);
+MOCK_FUNCTION_DEFINITION (MockAcpiSdtProtocol, SetOption, 4, EFIAPI);
+MOCK_FUNCTION_DEFINITION (MockAcpiSdtProtocol, FindPath, 3, EFIAPI);
+
+EFI_ACPI_SDT_PROTOCOL  ACPI_SDT_PROTOCOL_INSTANCE = {
+  0,
+  GetAcpiTable,   // EFI_ACPI_TABLE_INSTALL_ACPI_TABLE
+  RegisterNotify, // EFI_ACPI_TABLE_UNINSTALL_ACPI_TABLE
+  Open,
+  OpenSdt,
+  Close,
+  GetChild,
+  GetOption,
+  SetOption,
+  FindPath
+};
+
+extern "C" {
+  EFI_ACPI_SDT_PROTOCOL  *gAcpiSdtProtocol = &ACPI_SDT_PROTOCOL_INSTANCE;
+}
+
 #endif // MOCK_ACPI_SYSTEM_DESCRIPTION_TABLE_H_
--- MdePkg/Test/Mock/Include/GoogleTest/Protocol/MockAcpiTable.h
+++ MdePkg/Test/Mock/Include/GoogleTest/Protocol/MockAcpiTable.h
@@ -44,4 +44,17 @@ extern "C" {
   extern EFI_ACPI_TABLE_PROTOCOL  *gAcpiTableProtocol;
 }
 
+MOCK_INTERFACE_DEFINITION (MockAcpiTableProtocol);
+MOCK_FUNCTION_DEFINITION (MockAcpiTableProtocol, InstallAcpiTable, 4, EFIAPI);
+MOCK_FUNCTION_DEFINITION (MockAcpiTableProtocol, UninstallAcpiTable, 2, EFIAPI);
+
+EFI_ACPI_TABLE_PROTOCOL  ACPI_TABLE_PROTOCOL_INSTANCE = {
+  InstallAcpiTable,   // EFI_ACPI_TABLE_INSTALL_ACPI_TABLE
+  UninstallAcpiTable  // EFI_ACPI_TABLE_UNINSTALL_ACPI_TABLE
+};
+
+extern "C" {
+  EFI_ACPI_TABLE_PROTOCOL  *gAcpiTableProtocol = &ACPI_TABLE_PROTOCOL_INSTANCE;
+}
+
 #endif // MOCK_ACPI_TABLE_H_
