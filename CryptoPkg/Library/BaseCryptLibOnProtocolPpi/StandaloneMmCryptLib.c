9947a44cba3c7e566f311e354bb8820a2afcbe3c
Joey Vagedes <joey.vagedes@gmail.com>
2024-09-23 13:39:51 -0700

    CryptoPkg: Require exact crypto version match (#1157)
    ## Description
    
    Crypto versioning is not currently backwards compatible. This change
    updates the check to require an exact match of the crypto version.
    
    - [ ] Impacts functionality?
    - [ ] Impacts security?
    - [ ] Breaking change?
    - [ ] Includes tests?
    - [ ] Includes documentation?
    
    ## How This Was Tested
    
    N/A
    
    ## Integration Instructions
    
    N/A

--- CryptoPkg/Library/BaseCryptLibOnProtocolPpi/StandaloneMmCryptLib.c
+++ CryptoPkg/Library/BaseCryptLibOnProtocolPpi/StandaloneMmCryptLib.c
@@ -68,9 +68,9 @@ StandaloneMmCryptLibConstructor (
   }
 
   Version = mSmmCryptoProtocol->GetVersion ();
-  if (Version < EDKII_CRYPTO_VERSION) {
+  if (Version != EDKII_CRYPTO_VERSION) {
     DEBUG ((DEBUG_ERROR, "[StandaloneMmCryptLib] Crypto SMM Protocol unsupported version %d\n", Version));
-    ASSERT (Version >= EDKII_CRYPTO_VERSION);
+    ASSERT (Version == EDKII_CRYPTO_VERSION);
     mSmmCryptoProtocol = NULL;
     return EFI_NOT_FOUND;
   }
