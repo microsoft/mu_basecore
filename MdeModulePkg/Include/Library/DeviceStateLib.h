a1f05addc89aff6f7753823a37e91e301aa039a4
Vivian Nowka-Keane <vnowkakeane@microsoft.com>
2024-10-10 16:10:59 -0700

    [SQUASH] PR comments

--- MdeModulePkg/Include/Library/DeviceStateLib.h
+++ MdeModulePkg/Include/Library/DeviceStateLib.h
@@ -21,24 +21,27 @@ MU_CHANGE: new file
 #define DEVICE_STATE_UNDEFINED                  (1 << (4))
 #define DEVICE_STATE_UNIT_TEST_MODE             (1 << (5))
 
-#define DEVICE_STATE_PLATFORM_MODE_0  (1 << (20))
+#define DEVICE_STATE_PLATFORM_MODE_INDEX_START  20
+#define DEVICE_STATE_PLATFORM_MODE_INDEX_END    27
+
+#define DEVICE_STATE_PLATFORM_MODE_0  (1 << (DEVICE_STATE_PLATFORM_MODE_INDEX_START))
 #define DEVICE_STATE_PLATFORM_MODE_1  (1 << (21))
 #define DEVICE_STATE_PLATFORM_MODE_2  (1 << (22))
 #define DEVICE_STATE_PLATFORM_MODE_3  (1 << (23))
 #define DEVICE_STATE_PLATFORM_MODE_4  (1 << (24))
 #define DEVICE_STATE_PLATFORM_MODE_5  (1 << (25))
 #define DEVICE_STATE_PLATFORM_MODE_6  (1 << (26))
-#define DEVICE_STATE_PLATFORM_MODE_7  (1 << (27))
+#define DEVICE_STATE_PLATFORM_MODE_7  (1 << (DEVICE_STATE_PLATFORM_MODE_INDEX_END))
 
 #define DEVICE_STATE_MAX  (1 << (31))
 
-#define DEVICE_STATE_PLATFORM_MODE_INDEX_START  20
-#define DEVICE_STATE_PLATFORM_MODE_INDEX_END    27
-
 #define MAX_INSECURE_DEVICE_STATE_STRING_SIZE  512
 
+// The number of library-defined device states, not including OEM-defined states.
+#define NUMBER_OF_DEVICE_STATE_STRINGS  6
+
 //
-// DEFINE the device state strings here. These are used for logging and measuring purposes.
+// These strings are used to represent device state for logging and measurements.
 //
 #define DEVICE_STATE_SECUREBOOT_OFF_STRING             "SECURE_BOOT_OFF "
 #define DEVICE_STATE_MANUFACTURING_MODE_STRING         "MANUFACTURING_MODE "
@@ -51,48 +54,68 @@ MU_CHANGE: new file
 typedef UINT32 DEVICE_STATE;
 
 /**
- * Get String that represents the device's current insecure states, using the platform defined set of
- * insecure device states. This can be used to measure insecure device states into the TPM or perform
- * other required platform actions when the device enters an insecure state.
- *
- * @param[out] Buffer - Buffer to store the string
- * @param[in] MaxSize - Maximum size of the buffer
- * @retval The The number of characters in the string, including the null character.
- */
-UINTN
+  Get String that represents the device's current insecure states, using the platform defined set of
+  insecure device states. This can be used to measure insecure device states into the TPM or perform
+  other required platform actions when the device enters an insecure state.
+
+  The string convention to be returned is described as follows:
+  A concatenation of all the insecure device states that are currently active, represented in
+  MACRO_CASE, separated by a space. The string, if not empty, will end in a space followed by a Null terminator.
+
+  Examples:
+    - "SECUREBOOT_OFF MANUFACTURING_MODE "
+    - "DEVELOPMENT_BUILD_ENABLED SOURCE_DEBUG_ENABLED UNIT_TEST_MODE "
+
+  @param[out] Destination Buffer to write the string
+  @param[in]  DestMax     Maximum size of the destination buffer
+  @param[out] StringSize  Size of the Null-terminated Ascii string in bytes, including the Null terminator.
+
+  @retval EFI_SUCCESS            String is written to buffer.
+  @retval EFI_BUFFER_TOO_SMALL   If DestMax is NOT greater than StrLen to be written.
+  @retval EFI_INVALID_PARAMETER  If Destination is NULL.
+                                    If StringSize is NULL.
+                                    If DestMax is 0.
+                                    If the bitmask is out of range.
+ **/
+EFI_STATUS
 EFIAPI
 GetInsecureDeviceStateString (
-  IN OUT CHAR8  *Buffer,
-  IN UINTN      MaxSize
+  IN OUT CHAR8  *Destination,
+  IN UINTN      DestMax,
+  IN OUT UINTN  *StringSize
   );
 
 /**
- * Get the platform defined set of insecure device states. This can be used to measure insecure device
- * states into the TPM or perform other required platform actions when the device enters an insecure state.
- *
- * @retval The bitmask of insecure device states as defined by the platform.
- */
+  Get the platform defined set of insecure device states. This can be used to measure insecure device
+  states into the TPM or perform other required platform actions when the device enters an insecure state.
+
+  @return The bitmask of insecure device states as defined by the platform.
+**/
 DEVICE_STATE
 EFIAPI
 GetInsecureDeviceStateSetting (
   );
 
 /**
- * Function to Get current device state
- *
- * @retval the current DEVICE_STATE
- */
+  Get current device state
+
+  @return the current DEVICE_STATE
+**/
 DEVICE_STATE
 EFIAPI
 GetDeviceState (
+  VOID
   );
 
 /**
- * Function to Add additional bits to the device state
- *
- * @param[in] AdditionalState - additional state to set active
- * @retval Status of operation.  EFI_SUCCESS on successful update.
- */
+  Add an additional device state
+
+  @param[in] AdditionalState  The additional state to add.
+
+  @retval EFI_SUCCESS             The device state was added successfully.
+  @retval RETURN_OUT_OF_RESOURCES The device state could not be added due to lack of resources.
+  @retval RETURN_DEVICE_ERROR     The device state could not be added.
+**/
 RETURN_STATUS
 EFIAPI
 AddDeviceState (
--- MdeModulePkg/Library/DeviceStateLib/DeviceStateLib.c
+++ MdeModulePkg/Library/DeviceStateLib/DeviceStateLib.c
@@ -1,6 +1,7 @@
 /** @file DeviceStateLib.c
 Functions used to support Getting and Setting Device States.
-This library uses the PcdDeviceStateBitmask dynamic PCD to store the device state
+This library uses the PcdDeviceStateBitmask dynamic PCD to store the device state.
+It also uses the PcdInsecureDeviceState fixed PCD to retrieve the platform defined set of insecure device states.
 
 Copyright (C) Microsoft Corporation.
 SPDX-License-Identifier: BSD-2-Clause-Patent
@@ -13,7 +14,7 @@ MU_CHANGE: new file
 #include <Library/DeviceStateLib.h>
 #include <Library/DebugLib.h>
 
-CHAR8  *DEVICE_STATE_STRINGS[6] = {
+CHAR8  *DEVICE_STATE_STRINGS[NUMBER_OF_DEVICE_STATE_STRINGS] = {
   DEVICE_STATE_SECUREBOOT_OFF_STRING,
   DEVICE_STATE_MANUFACTURING_MODE_STRING,
   DEVICE_STATE_DEVELOPMENT_BUILD_ENABLED_STRING,
@@ -23,70 +24,107 @@ CHAR8  *DEVICE_STATE_STRINGS[6] = {
 };
 
 /**
- * Get String that represents the device's current insecure states, using the platform defined set of
- * insecure device states. This can be used to measure insecure device states into the TPM or perform
- * other required platform actions when the device enters an insecure state.
- *
- * @param[out] Buffer - Buffer to store the string
- * @param[in] MaxSize - Maximum size of the buffer
- * @retval The The number of characters in the string, including the null character.
- */
-UINTN
+  Get String that represents the device's current insecure states, using the platform defined set of
+  insecure device states. This can be used to measure insecure device states into the TPM or perform
+  other required platform actions when the device enters an insecure state.
+
+  The string convention to be returned is described as follows:
+  A concatenation of all the insecure device states that are currently active, represented in
+  MACRO_CASE, separated by a space. The string, if not empty, will end in a space followed by a Null terminator.
+
+  Examples:
+    - "SECUREBOOT_OFF MANUFACTURING_MODE "
+    - "DEVELOPMENT_BUILD_ENABLED SOURCE_DEBUG_ENABLED UNIT_TEST_MODE "
+
+  @param[out] Destination Buffer to write the string
+  @param[in]  DestMax     Maximum size of the destination buffer
+  @param[out] StringSize  Size of the Null-terminated Ascii string in bytes, including the Null terminator.
+
+  @retval EFI_SUCCESS            String is written to buffer.
+  @retval EFI_BUFFER_TOO_SMALL   If DestMax is NOT greater than StrLen to be written.
+  @retval EFI_INVALID_PARAMETER  If Destination is NULL.
+                                    If StringSize is NULL.
+                                    If DestMax is 0.
+                                    If the bitmask is out of range.
+ **/
+EFI_STATUS
 EFIAPI
 GetInsecureDeviceStateString (
-  OUT CHAR8  *Buffer,
-  IN UINTN   MaxSize
+  IN OUT CHAR8  *Destination,
+  IN UINTN      DestMax,
+  IN OUT UINTN  *StringSize
   )
 {
+  EFI_STATUS    Status;
   CHAR8         *String;
+  UINTN         Index;
   DEVICE_STATE  BitmaskFromIndex;
   DEVICE_STATE  CurrentDeviceState         = GetDeviceState ();
   DEVICE_STATE  InsecureDeviceStateSetting = GetInsecureDeviceStateSetting ();
 
-  UINT32  NumberOfDeviceStates = sizeof (DEVICE_STATE_STRINGS) / sizeof (DEVICE_STATE_STRINGS[0]);
+  if ((Destination == NULL) || (DestMax == 0) || (StringSize == NULL)) {
+    return EFI_INVALID_PARAMETER;
+  }
 
   //
   // Check standard device states
   //
-  for (UINT32 i = 0; i < NumberOfDeviceStates; i++) {
-    BitmaskFromIndex = 1 << i;
-    if ((InsecureDeviceStateSetting & BitmaskFromIndex != 0) &&
-        (CurrentDeviceState & BitmaskFromIndex != 0))
+  for (Index = 0; Index < NUMBER_OF_DEVICE_STATE_STRINGS; Index++) {
+    BitmaskFromIndex = 1 << Index;
+    if (BitmaskFromIndex > DEVICE_STATE_MAX) {
+      // TODO shouldn't this be known from the macro max ?
+      DEBUG ((DEBUG_ERROR, "Device State Bitmask is out of range. 0x%X\n", BitmaskFromIndex));
+      return EFI_INVALID_PARAMETER;
+    }
+
+    if (((InsecureDeviceStateSetting & BitmaskFromIndex) != 0) &&
+        ((CurrentDeviceState & BitmaskFromIndex) != 0))
     {
       //
       // An insecure device state is detected. Append to the insecure device state string.
       //
-      String = DEVICE_STATE_STRINGS[i];
-      AsciiStrnCatS (Buffer, MaxSize, String, AsciiStrLen (String));
+      String = DEVICE_STATE_STRINGS[Index];
+
+      Status = AsciiStrnCatS (Destination, DestMax, String, AsciiStrnLenS (String, DestMax));
+      if (EFI_ERROR (Status)) {
+        DEBUG ((DEBUG_ERROR, "Error creating insecure device state string. 0x%X\n", Status));
+        return Status;
+      }
     }
   }
 
   //
   // Check OEM-defined device states
   //
-  for (UINT32 i = DEVICE_STATE_PLATFORM_MODE_INDEX_START; i <= DEVICE_STATE_PLATFORM_MODE_INDEX_END; i++) {
-    BitmaskFromIndex = 1 << i;
-    if ((InsecureDeviceStateSetting & BitmaskFromIndex != 0) &&
-        (CurrentDeviceState & BitmaskFromIndex != 0))
+  for (Index = DEVICE_STATE_PLATFORM_MODE_INDEX_START; Index <= DEVICE_STATE_PLATFORM_MODE_INDEX_END; Index++) {
+    BitmaskFromIndex = 1 << Index;
+    if (((InsecureDeviceStateSetting & BitmaskFromIndex) != 0) &&
+        ((CurrentDeviceState & BitmaskFromIndex) != 0))
     {
       //
       // An OEM insecure device state is detected. Append to the insecure device state string.
       //
       String = DEVICE_STATE_PLATFORM_MODE_STRING;
-      AsciiStrnCatS (Buffer, MaxSize, String, AsciiStrLen (String));
+      Status = AsciiStrnCatS (Destination, DestMax, String, AsciiStrnLenS (String, DestMax));
+      if (EFI_ERROR (Status)) {
+        DEBUG ((DEBUG_ERROR, "Error creating insecure device state string. 0x%X\n", Status));
+        return Status;
+      }
+
       break;
     }
   }
 
-  return AsciiStrnLenS (Buffer, MaxSize) + 1;
+  *StringSize = AsciiStrnSizeS (Destination, DestMax);
+  return EFI_SUCCESS;
 }
 
 /**
- * Get the platform defined set of insecure device states. This can be used to measure insecure device
- * states into the TPM or perform other required platform actions when the device enters an insecure state.
- *
- * @retval The bitmask of insecure device states as defined by the platform.
- */
+  Get the platform defined set of insecure device states. This can be used to measure insecure device
+  states into the TPM or perform other required platform actions when the device enters an insecure state.
+
+  @return The bitmask of insecure device states as defined by the platform.
+**/
 DEVICE_STATE
 EFIAPI
 GetInsecureDeviceStateSetting (
@@ -96,32 +134,28 @@ GetInsecureDeviceStateSetting (
 }
 
 /**
- * Check if a specific bitmask is set in the device state
- *
- * @param[in] CurrentDeviceState - the current device state
- * @param[in] DeviceStateBitmask - the bitmask to check
- * @retval TRUE if all the bits in the bitmask are set, FALSE otherwise
- */
+  Get current device state
 
-/**
- * Function to Get current device state
- *
- * @retval the current DEVICE_STATE
- */
+  @return the current DEVICE_STATE
+**/
 DEVICE_STATE
 EFIAPI
 GetDeviceState (
+  VOID
   )
 {
   return PcdGet32 (PcdDeviceStateBitmask);
 }
 
 /**
- * Function to Add additional bits to the device state
- *
- * @param[in] AdditionalState - additional state to set active
- * @retval Status of operation.  EFI_SUCCESS on successful update.
- */
+  Add an additional device state
+
+  @param[in] AdditionalState  The additional state to add.
+
+  @retval EFI_SUCCESS             The device state was added successfully.
+  @retval RETURN_OUT_OF_RESOURCES The device state could not be added due to lack of resources.
+  @retval RETURN_DEVICE_ERROR     The device state could not be added.
+**/
 RETURN_STATUS
 EFIAPI
 AddDeviceState (
