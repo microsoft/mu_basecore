0274eea4dc7331977731b937c89d838284d412da
Vivian Nowka-Keane <vnowkakeane@microsoft.com>
2024-10-23 15:03:34 -0700

    SecurityPkg: Remove PcdFirmwareDebuggerInitialized
    Removed PCD in favor of using DeviceStateLib for
    storing firmware debugger enablement status.
    
    Signed-off-by: Vivian Nowka-Keane <vnowkakeane@microsoft.com>

--- SecurityPkg/Tcg/Tcg2Dxe/Tcg2Dxe.c
+++ SecurityPkg/Tcg/Tcg2Dxe/Tcg2Dxe.c
@@ -2554,10 +2554,12 @@ MeasureSecureBootPolicy (
 
   // MU_CHANGE [END]
 
-  if (PcdGetBool (PcdFirmwareDebuggerInitialized)) {
-    Status = MeasureLaunchOfFirmwareDebugger ();
-    DEBUG ((DEBUG_INFO, "MeasureLaunchOfFirmwareDebugger - %r\n", Status));
-  }
+  // MU_CHANGE [BEGIN] - Remove PcdFirmwareDebuggerInitialized
+  // if (PcdGetBool (PcdFirmwareDebuggerInitialized)) {
+  //  Status = MeasureLaunchOfFirmwareDebugger ();
+  //  DEBUG ((DEBUG_INFO, "MeasureLaunchOfFirmwareDebugger - %r\n", Status));
+  // }
+  // MU_CHANGE [END]
 
   Status = MeasureAllSecureVariables ();
   DEBUG ((DEBUG_INFO, "MeasureAllSecureVariables - %r\n", Status));
