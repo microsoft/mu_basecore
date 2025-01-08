35d926ecfbd0e3f8786f3504b5754aa26b66b3d7
Oliver Smith-Denny <osde@microsoft.com>
2025-01-08 12:39:51 -0800

    Works

---
+++ MdeModulePkg/Include/Guid/CacheAttributesChangeEvent.h
@@ -0,0 +1,16 @@
+/** @file
+  Event group triggered when caching attributes are updated in the system.
+
+  Copyright (c) Microsoft Corporation.
+  SPDX-License-Identifier: BSD-2-Clause-Patent
+**/
+
+#ifndef CACHE_ATTRIBUTES_CHANGE_EVENT_H_
+#define CACHE_ATTRIBUTES_CHANGE_EVENT_H_
+
+#define CACHE_ATTRIBUTES_CHANGE_EVENT_GUID \
+   { 0XB8E477C7, 0X26A9, 0X4B9A, { 0XA7, 0XC9, 0X5F, 0X8F, 0X1F, 0X3D, 0X9C, 0X7B } }
+
+extern EFI_GUID  gCacheAttributesChangeEventGuid;
+
+#endif
--- UefiCpuPkg/MpDxe/MpDxe.c
+++ UefiCpuPkg/MpDxe/MpDxe.c
@@ -7,9 +7,12 @@
 **/
 
 #include "MpDxe.h"
+#include <Guid/CacheAttributesChangeEvent.h>
+#include <Library/MtrrLib.h>
 
-EFI_HANDLE  mMpServiceHandle    = NULL;
-UINTN       mNumberOfProcessors = 1;
+EFI_HANDLE  mMpServiceHandle            = NULL;
+UINTN       mNumberOfProcessors         = 1;
+EFI_EVENT   mCacheAttributesChangeEvent = NULL;
 
 EFI_MP_SERVICES_PROTOCOL  mMpServicesTemplate = {
   GetNumberOfProcessors,
@@ -20,6 +23,49 @@ EFI_MP_SERVICES_PROTOCOL  mMpServicesTemplate = {
   EnableDisableAP,
   WhoAmI
 };
+
+/**
+  A minimal wrapper function that allows MtrrSetAllMtrrs() to be passed to
+  EFI_MP_SERVICES_PROTOCOL.StartupAllAPs() as Procedure.
+
+  @param[in] Buffer  Pointer to an MTRR_SETTINGS object, to be passed to
+                     MtrrSetAllMtrrs().
+**/
+VOID
+EFIAPI
+SetMtrrsFromBuffer (
+  IN VOID  *Buffer
+  )
+{
+  MtrrSetAllMtrrs (Buffer);
+}
+
+// MU_CHANGE START: Cache Attribute Change Event
+VOID
+EFIAPI
+CacheAttributesChangeCallback (
+  IN EFI_EVENT  Event,
+  IN VOID       *Context
+  )
+{
+  MTRR_SETTINGS  MtrrSettings;
+
+  DEBUG ((DEBUG_INFO, "MpDxe updating MTRRs with APs\n"));
+
+  MtrrGetAllMtrrs (&MtrrSettings);
+
+  MpInitLibStartupAllAPs (
+    SetMtrrsFromBuffer,
+    FALSE,
+    NULL,
+    0,
+    &MtrrSettings,
+    NULL
+    );
+  // we purposefully don't close the event here, because we want to be called for every update
+}
+
+// MU_CHANGE END: Cache Attribute Change Event
 
 /**
   This service retrieves the number of logical processor in the platform
@@ -816,6 +862,23 @@ InitializeMpSupport (
                     );
     ASSERT_EFI_ERROR (Status);
   }
+
+  // MU_CHANGE START: CacheAttributesChange Event
+  // Register for the CacheAttributesChangeEvent
+  Status = gBS->CreateEventEx (
+                  EVT_NOTIFY_SIGNAL,
+                  TPL_CALLBACK,
+                  CacheAttributesChangeCallback,
+                  NULL,
+                  &gCacheAttributesChangeEventGuid,
+                  &mCacheAttributesChangeEvent
+                  );
+  if (EFI_ERROR (Status)) {
+    DEBUG ((DEBUG_ERROR, "Failed to create CacheAttributesChangeEvent\n"));
+    ASSERT_EFI_ERROR (Status);
+  }
+
+  // MU_CHANGE END: CacheAttributesChange Event
 }
 
 // MU_CHANGE End - CodeQL Change - unguardednullreturndereference
