77d0620a33668e1c8fe5b409236c772d9aeb589b
Kun Qin <kuqin@microsoft.com>
2025-01-24 11:11:59 -0800

    uncru

--- MdeModulePkg/Library/BaseHobLibNull/BaseHobLibNull.c
+++ MdeModulePkg/Library/BaseHobLibNull/BaseHobLibNull.c
@@ -232,6 +232,7 @@ BuildResourceDescriptorWithOwnerHob (
 }
 
 // MU_CHANGE Start: Add BuildResourceDescriptorV2 function
+
 /**
   Builds a HOB that describes a chunk of system memory with memory attributes.
 
@@ -258,6 +259,7 @@ BuildResourceDescriptorV2 (
 {
   ASSERT (FALSE);
 }
+
 // MU_CHANGE End
 
 /**
