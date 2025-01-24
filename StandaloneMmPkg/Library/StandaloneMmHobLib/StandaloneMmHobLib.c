acc4e4481bae4616498972e05d310f38c5e97e34
Kun Qin <kuqin@microsoft.com>
2025-01-24 14:37:30 -0800

    uncru

--- StandaloneMmPkg/Library/StandaloneMmHobLib/StandaloneMmHobLib.c
+++ StandaloneMmPkg/Library/StandaloneMmHobLib/StandaloneMmHobLib.c
@@ -517,6 +517,7 @@ BuildResourceDescriptorWithOwnerHob (
 }
 
 // MU_CHANGE Start: Add BuildResourceDescriptorV2 function
+
 /**
   Builds a HOB that describes a chunk of system memory with memory attributes.
 
@@ -544,6 +545,7 @@ BuildResourceDescriptorV2 (
 {
   ASSERT (FALSE);
 }
+
 // MU_CHANGE End
 
 /**
