df6c85dbfbf9aee88503314c6a9a8f4e71be9b94
Michael Kubacki <michael.kubacki@microsoft.com>
2025-02-06 23:18:00 -0500

    MdeModulePkg/Variable: Init var policy after SMM variable is ready (#1269)
    On a MM system, the main UEFI variable logic resides in MMRAM. In that
    case, the variable policy logic in `VarCheckPolicyLib`, such as
    `VarCheckPolicyLibStandaloneMm` is linked against the MM driver also in
    that case `VariableStandaloneMm`.
    
    The MM variable driver indicates its presence to the RT DXE driver via
    `gEfiSmmVariableProtocolGuid` to indicate variable read support is
    available from MM. This triggers installation of the variable
    architectural protocol in DXE.
    
    Today, variable policy is initialized by calling
    `VariablePolicySmmDxeMain()` in `VariableSmmRuntimeInitialize()`. In
    turn, this installs `gEdkiiVariablePolicyProtocolGuid`. Functions in
    `gEdkiiVariablePolicyProtocolGuid` may trigger MMIs. However, it is
    possible that the MM variable driver which is linked against the code
    with the variable policy MMI handlers (i.e. `VarCheckPolicyLib`) is not
    loaded yet.
    
    Therefore, this change moves invocation of `VariablePolicySmmDxeMain()`
    to `SmmVariableReady()` which is called on installation of
    `gEfiSmmVariableProtocolGuid` indicating variable MM services are ready.
    `gEdkiiVariablePolicyProtocolGuid` is still installed prior to the
    variable architectural protocol being installed.
    
    - [x] Impacts functionality?
    - [ ] Impacts security?
    - [ ] Breaking change?
    - [ ] Includes tests?
    - [ ] Includes documentation?
    - [ ] Backport to release branch?
    
    - Verify variable policy is initialized as expected on QEMU Q35/SBSA and
    a physical Intel system.
    - Check that the variable image handle passed to
    `VariablePolicySmmDxeMain()` is correct.
    
    N/A - Some drivers may dispatch differently if they depend on
    `gEdkiiVariablePolicyProtocolGuid`. However, this is not considered
    breaking as that is an inherent expectation in dispatch based on
    dependency expressions.
    
    Signed-off-by: Michael Kubacki <michael.kubacki@microsoft.com>

--- MdeModulePkg/Universal/Variable/RuntimeDxe/VariableSmmRuntimeDxe.c
+++ MdeModulePkg/Universal/Variable/RuntimeDxe/VariableSmmRuntimeDxe.c
@@ -67,8 +67,8 @@ BOOLEAN                         mIsRuntimeCacheEnabled = FALSE;
 EFI_STATUS
 EFIAPI
 VariablePolicySmmDxeMain (
-  IN    EFI_HANDLE        ImageHandle,
-  IN    EFI_SYSTEM_TABLE  *SystemTable
+  IN    EFI_HANDLE  ImageHandle             // MU_CHANGE - Initialize var policy after SMM Variable is ready
+  // IN    EFI_SYSTEM_TABLE  *SystemTable   // MU_CHANGE - Initialize var policy after SMM Variable is ready
   );
 
 /**
@@ -1673,6 +1673,7 @@ SmmVariableReady (
 {
   EFI_STATUS         Status;
   EFI_HOB_GUID_TYPE  *GuidHob;
+  EFI_HANDLE         VariablePolicyHandle; // MU_CHANGE - Initialize var policy after SMM Variable is ready
 
   Status = gBS->LocateProtocol (&gEfiSmmVariableProtocolGuid, NULL, (VOID **)&mSmmVariable);
   if (EFI_ERROR (Status)) {
@@ -1722,6 +1723,16 @@ SmmVariableReady (
   gRT->GetNextVariableName = RuntimeServiceGetNextVariableName;
   gRT->SetVariable         = RuntimeServiceSetVariable;
   gRT->QueryVariableInfo   = RuntimeServiceQueryVariableInfo;
+
+  // MU_CHANGE [BEGIN] - Initialize var policy after SMM Variable is ready
+  VariablePolicyHandle = (Context != NULL) ? Context : mHandle;
+  if (Context == NULL) {
+    DEBUG ((DEBUG_ERROR, "Variable policy was installed on a handle other than the variable image handle.\n"));
+  }
+
+  // Initialize the VariablePolicy protocol and engine.
+  VariablePolicySmmDxeMain (VariablePolicyHandle);
+  // MU_CHANGE [END] - Initialize var policy after SMM Variable is ready
 
   //
   // Install the Variable Architectural Protocol on a new handle.
@@ -1833,7 +1844,7 @@ VariableSmmRuntimeInitialize (
     &gEfiSmmVariableProtocolGuid,
     TPL_CALLBACK,
     SmmVariableReady,
-    NULL,
+    ImageHandle,                    // MU_CHANGE - Initialize var policy after SMM Variable is ready
     &SmmVariableRegistration
     );
 
@@ -1895,8 +1906,10 @@ VariableSmmRuntimeInitialize (
          &mVirtualAddressChangeEvent
          );
 
-  // Initialize the VariablePolicy protocol and engine.
-  VariablePolicySmmDxeMain (ImageHandle, SystemTable);
+  // MU_CHANGE [BEGIN] - Initialize var policy after SMM Variable is ready
+  // // Initialize the VariablePolicy protocol and engine.
+  // VariablePolicySmmDxeMain (ImageHandle, SystemTable);
+  // MU_CHANGE [END] - Initialize var policy after SMM Variable is ready
 
   return EFI_SUCCESS;
 }
