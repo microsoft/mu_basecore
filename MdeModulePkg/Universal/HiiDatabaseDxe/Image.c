913740814e6c45a344909e6f4d5a8abf1db95692
Michael Kubacki <michael.kubacki@microsoft.com>
2025-02-26 09:07:05 -0500

    [CHERRY-PICK] MdeModulePkg/HiiDatabaseDxe: Prevent linker error
    Prevent an issue where `memcpy()` instrinsic is being used after
    recent MSVC linker update in windows-2022 VM image from 14.31.31103
    to 14.32.31326.
    
    Signed-off-by: Michael Kubacki <michael.kubacki@microsoft.com>
    (cherry picked from commit 29f02d0161bc5d10a6e3b2cfc4d10799644964a5)

--- MdeModulePkg/Universal/HiiDatabaseDxe/Image.c
+++ MdeModulePkg/Universal/HiiDatabaseDxe/Image.c
@@ -242,19 +242,19 @@ Output1bitPixel (
   IN EFI_HII_IMAGE_PALETTE_INFO  *PaletteInfo
   )
 {
-  UINT16                         Xpos;
-  UINT16                         Ypos;
-  UINTN                          OffsetY;
-  UINT8                          Index;
-  EFI_GRAPHICS_OUTPUT_BLT_PIXEL  *BitMapPtr;
-  EFI_GRAPHICS_OUTPUT_BLT_PIXEL  PaletteValue[2];
-  EFI_HII_IMAGE_PALETTE_INFO     *Palette;
-  UINTN                          PaletteSize;
-  UINT8                          Byte;
+  UINT16                               Xpos;
+  UINT16                               Ypos;
+  UINTN                                OffsetY;
+  UINT8                                Index;
+  EFI_GRAPHICS_OUTPUT_BLT_PIXEL_UNION  *BitMapPtr;
+  EFI_GRAPHICS_OUTPUT_BLT_PIXEL_UNION  PaletteValue[2];
+  EFI_HII_IMAGE_PALETTE_INFO           *Palette;
+  UINTN                                PaletteSize;
+  UINT8                                Byte;
 
   ASSERT (Image != NULL && Data != NULL && PaletteInfo != NULL);
 
-  BitMapPtr = Image->Bitmap;
+  BitMapPtr = (EFI_GRAPHICS_OUTPUT_BLT_PIXEL_UNION *)Image->Bitmap;
 
   //
   // First entry corresponds to color 0 and second entry corresponds to color 1.
@@ -271,8 +271,8 @@ Output1bitPixel (
   CopyMem (Palette, PaletteInfo, PaletteSize);
 
   ZeroMem (PaletteValue, sizeof (PaletteValue));
-  CopyRgbToGopPixel (&PaletteValue[0], &Palette->PaletteValue[0], 1);
-  CopyRgbToGopPixel (&PaletteValue[1], &Palette->PaletteValue[1], 1);
+  CopyRgbToGopPixel (&PaletteValue[0].Pixel, &Palette->PaletteValue[0], 1);
+  CopyRgbToGopPixel (&PaletteValue[1].Pixel, &Palette->PaletteValue[1], 1);
   FreePool (Palette);
   Palette = NULL;                                     // MU_CHANGE - Make sure to manage this pointer safely.
 
@@ -288,9 +288,9 @@ Output1bitPixel (
       Byte = *(Data + OffsetY + Xpos);
       for (Index = 0; Index < 8; Index++) {
         if ((Byte & (1 << Index)) != 0) {
-          CopyMem (&BitMapPtr[Ypos * Image->Width + Xpos * 8 + (8 - Index - 1)], &PaletteValue[1], sizeof (*BitMapPtr));
+          BitMapPtr[Ypos * Image->Width + Xpos * 8 + (8 - Index - 1)].Raw = PaletteValue[1].Raw;
         } else {
-          CopyMem (&BitMapPtr[Ypos * Image->Width + Xpos * 8 + (8 - Index - 1)], &PaletteValue[0], sizeof (*BitMapPtr));
+          BitMapPtr[Ypos * Image->Width + Xpos * 8 + (8 - Index - 1)].Raw = PaletteValue[0].Raw;
         }
       }
     }
@@ -303,9 +303,9 @@ Output1bitPixel (
       for (Index = 0; (UINT16)Index < (UINT16)(Image->Width % 8); Index++) {
         // MU_CHANGE - CodeQL Change - comparison-with-wider-type
         if ((Byte & (1 << (8 - Index - 1))) != 0) {
-          CopyMem (&BitMapPtr[Ypos * Image->Width + Xpos * 8 + Index], &PaletteValue[1], sizeof (*BitMapPtr));
+          BitMapPtr[Ypos * Image->Width + Xpos * 8 + Index].Raw = PaletteValue[1].Raw;
         } else {
-          CopyMem (&BitMapPtr[Ypos * Image->Width + Xpos * 8 + Index], &PaletteValue[0], sizeof (*BitMapPtr));
+          BitMapPtr[Ypos * Image->Width + Xpos * 8 + Index].Raw = PaletteValue[0].Raw;
         }
       }
     }
@@ -333,19 +333,19 @@ Output4bitPixel (
   IN EFI_HII_IMAGE_PALETTE_INFO  *PaletteInfo
   )
 {
-  UINT16                         Xpos;
-  UINT16                         Ypos;
-  UINTN                          OffsetY;
-  EFI_GRAPHICS_OUTPUT_BLT_PIXEL  *BitMapPtr;
-  EFI_GRAPHICS_OUTPUT_BLT_PIXEL  PaletteValue[16];
-  EFI_HII_IMAGE_PALETTE_INFO     *Palette;
-  UINTN                          PaletteSize;
-  UINT16                         PaletteNum;
-  UINT8                          Byte;
+  UINT16                               Xpos;
+  UINT16                               Ypos;
+  UINTN                                OffsetY;
+  EFI_GRAPHICS_OUTPUT_BLT_PIXEL_UNION  *BitMapPtr;
+  EFI_GRAPHICS_OUTPUT_BLT_PIXEL_UNION  PaletteValue[16];
+  EFI_HII_IMAGE_PALETTE_INFO           *Palette;
+  UINTN                                PaletteSize;
+  UINT16                               PaletteNum;
+  UINT8                                Byte;
 
   ASSERT (Image != NULL && Data != NULL && PaletteInfo != NULL);
 
-  BitMapPtr = Image->Bitmap;
+  BitMapPtr = (EFI_GRAPHICS_OUTPUT_BLT_PIXEL_UNION *)Image->Bitmap;
 
   //
   // The bitmap should allocate each color index starting from 0.
@@ -363,7 +363,7 @@ Output4bitPixel (
   PaletteNum = (UINT16)(Palette->PaletteSize / sizeof (EFI_HII_RGB_PIXEL));
 
   ZeroMem (PaletteValue, sizeof (PaletteValue));
-  CopyRgbToGopPixel (PaletteValue, Palette->PaletteValue, MIN (PaletteNum, ARRAY_SIZE (PaletteValue)));
+  CopyRgbToGopPixel (&PaletteValue->Pixel, Palette->PaletteValue, MIN (PaletteNum, ARRAY_SIZE (PaletteValue)));
   FreePool (Palette);
   Palette = NULL;                                     // MU_CHANGE - Make sure to manage this pointer safely.
 
@@ -376,17 +376,17 @@ Output4bitPixel (
     // All bits in these bytes are meaningful
     //
     for (Xpos = 0; Xpos < Image->Width / 2; Xpos++) {
-      Byte = *(Data + OffsetY + Xpos);
-      CopyMem (&BitMapPtr[Ypos * Image->Width + Xpos * 2], &PaletteValue[Byte >> 4], sizeof (*BitMapPtr));
-      CopyMem (&BitMapPtr[Ypos * Image->Width + Xpos * 2 + 1], &PaletteValue[Byte & 0x0F], sizeof (*BitMapPtr));
+      Byte                                              = *(Data + OffsetY + Xpos);
+      BitMapPtr[Ypos * Image->Width + Xpos * 2].Raw     = PaletteValue[Byte >> 4].Raw;
+      BitMapPtr[Ypos * Image->Width + Xpos * 2 + 1].Raw = PaletteValue[Byte & 0x0F].Raw;
     }
 
     if (Image->Width % 2 != 0) {
       //
       // Padding bits in this byte should be ignored.
       //
-      Byte = *(Data + OffsetY + Xpos);
-      CopyMem (&BitMapPtr[Ypos * Image->Width + Xpos * 2], &PaletteValue[Byte >> 4], sizeof (*BitMapPtr));
+      Byte                                          = *(Data + OffsetY + Xpos);
+      BitMapPtr[Ypos * Image->Width + Xpos * 2].Raw = PaletteValue[Byte >> 4].Raw;
     }
   }
 }
@@ -412,19 +412,19 @@ Output8bitPixel (
   IN EFI_HII_IMAGE_PALETTE_INFO  *PaletteInfo
   )
 {
-  UINT16                         Xpos;
-  UINT16                         Ypos;
-  UINTN                          OffsetY;
-  EFI_GRAPHICS_OUTPUT_BLT_PIXEL  *BitMapPtr;
-  EFI_GRAPHICS_OUTPUT_BLT_PIXEL  PaletteValue[256];
-  EFI_HII_IMAGE_PALETTE_INFO     *Palette;
-  UINTN                          PaletteSize;
-  UINT16                         PaletteNum;
-  UINT8                          Byte;
+  UINT16                               Xpos;
+  UINT16                               Ypos;
+  UINTN                                OffsetY;
+  EFI_GRAPHICS_OUTPUT_BLT_PIXEL_UNION  *BitMapPtr;
+  EFI_GRAPHICS_OUTPUT_BLT_PIXEL_UNION  PaletteValue[256];
+  EFI_HII_IMAGE_PALETTE_INFO           *Palette;
+  UINTN                                PaletteSize;
+  UINT16                               PaletteNum;
+  UINT8                                Byte;
 
   ASSERT (Image != NULL && Data != NULL && PaletteInfo != NULL);
 
-  BitMapPtr = Image->Bitmap;
+  BitMapPtr = (EFI_GRAPHICS_OUTPUT_BLT_PIXEL_UNION *)Image->Bitmap;
 
   //
   // The bitmap should allocate each color index starting from 0.
@@ -441,7 +441,7 @@ Output8bitPixel (
   CopyMem (Palette, PaletteInfo, PaletteSize);
   PaletteNum = (UINT16)(Palette->PaletteSize / sizeof (EFI_HII_RGB_PIXEL));
   ZeroMem (PaletteValue, sizeof (PaletteValue));
-  CopyRgbToGopPixel (PaletteValue, Palette->PaletteValue, MIN (PaletteNum, ARRAY_SIZE (PaletteValue)));
+  CopyRgbToGopPixel (&PaletteValue->Pixel, Palette->PaletteValue, MIN (PaletteNum, ARRAY_SIZE (PaletteValue)));
   FreePool (Palette);
   Palette = NULL;                                     // MU_CHANGE - Make sure to manage this pointer safely.
 
@@ -454,8 +454,8 @@ Output8bitPixel (
     // All bits are meaningful since the bitmap is 8 bits per pixel.
     //
     for (Xpos = 0; Xpos < Image->Width; Xpos++) {
-      Byte = *(Data + OffsetY + Xpos);
-      CopyMem (&BitMapPtr[OffsetY + Xpos], &PaletteValue[Byte], sizeof (*BitMapPtr));
+      Byte                          = *(Data + OffsetY + Xpos);
+      BitMapPtr[OffsetY + Xpos].Raw = PaletteValue[Byte].Raw;
     }
   }
 }
@@ -525,13 +525,15 @@ ImageToBlt (
   IN OUT EFI_IMAGE_OUTPUT           **Blt
   )
 {
-  EFI_IMAGE_OUTPUT               *ImageOut;
-  UINTN                          Xpos;
-  UINTN                          Ypos;
-  UINTN                          OffsetY1;     // src buffer
-  UINTN                          OffsetY2;     // dest buffer
-  EFI_GRAPHICS_OUTPUT_BLT_PIXEL  SrcPixel;
-  EFI_GRAPHICS_OUTPUT_BLT_PIXEL  ZeroPixel;
+  EFI_IMAGE_OUTPUT                     *ImageOut;
+  UINTN                                Xpos;
+  UINTN                                Ypos;
+  UINTN                                OffsetY1; // src buffer
+  UINTN                                OffsetY2; // dest buffer
+  EFI_GRAPHICS_OUTPUT_BLT_PIXEL_UNION  SrcPixel;
+  EFI_GRAPHICS_OUTPUT_BLT_PIXEL_UNION  ZeroPixel;
+  EFI_GRAPHICS_OUTPUT_BLT_PIXEL_UNION  *BltBufferPixel;
+  EFI_GRAPHICS_OUTPUT_BLT_PIXEL_UNION  *ImageBitmap;
 
   if ((BltBuffer == NULL) || (Blt == NULL) || (*Blt == NULL)) {
     return EFI_INVALID_PARAMETER;
@@ -549,17 +551,20 @@ ImageToBlt (
 
   ZeroMem (&ZeroPixel, sizeof (EFI_GRAPHICS_OUTPUT_BLT_PIXEL));
 
+  BltBufferPixel = (EFI_GRAPHICS_OUTPUT_BLT_PIXEL_UNION *)BltBuffer;
+  ImageBitmap    = (EFI_GRAPHICS_OUTPUT_BLT_PIXEL_UNION *)ImageOut->Image.Bitmap;
+
   for (Ypos = 0; Ypos < Height; Ypos++) {
     OffsetY1 = Width * Ypos;
     OffsetY2 = ImageOut->Width * (BltY + Ypos);
     for (Xpos = 0; Xpos < Width; Xpos++) {
-      CopyMem (&SrcPixel, &BltBuffer[OffsetY1 + Xpos], sizeof (SrcPixel));
+      SrcPixel.Raw = BltBufferPixel[OffsetY1 + Xpos].Raw;
       if (Transparent) {
-        if (CompareMem (&SrcPixel, &ZeroPixel, 3) != 0) { \
-          CopyMem (&ImageOut->Image.Bitmap[OffsetY2 + BltX + Xpos], &SrcPixel, sizeof (SrcPixel));
+        if (CompareMem (&SrcPixel, &ZeroPixel, 3) != 0) {
+          ImageBitmap[OffsetY2 + BltX + Xpos].Raw = SrcPixel.Raw;
         }
       } else {
-        CopyMem (&ImageOut->Image.Bitmap[OffsetY2 + BltX + Xpos], &SrcPixel, sizeof (SrcPixel));
+        ImageBitmap[OffsetY2 + BltX + Xpos].Raw = SrcPixel.Raw;
       }
     }
   }
