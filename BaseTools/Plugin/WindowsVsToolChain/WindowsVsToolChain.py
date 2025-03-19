27e581469a7a75e6b37c092986cbee88549b3007
Joey Vagedes <joey.vagedes@gmail.com>
2025-03-19 16:33:19 -0700

    WindowsVsToolChain: Filter vs vars on specific vc
    Filters the extra vs vars gathered via QueryVcVariables() to
    specifically be the ones associated with the vc version set in the same
    plugin. This is to prevent version of the compiler differing from the
    version of any included .c / .h files that are made available through
    the vs vars such as LIB, INCLUDe, etc.
    
    Additionally now logs a warning when the plugin detects multiple
    versions of the compiler in the environment.

--- BaseTools/Plugin/WindowsVsToolChain/WindowsVsToolChain.py
+++ BaseTools/Plugin/WindowsVsToolChain/WindowsVsToolChain.py
@@ -94,7 +94,7 @@ class WindowsVsToolChain(IUefiBuildPlugin):
                 shell_env = shell_environment.GetEnvironment()
                 # Use the tools lib to determine the correct values for the vars that interest us.
                 vs_vars = locate_tools.QueryVcVariables(
-                    interesting_keys, VC_HOST_ARCH_TRANSLATOR[HostType], vs_version="vs2017")
+                    interesting_keys, VC_HOST_ARCH_TRANSLATOR[HostType], vs_version="vs2017", vc_version=vc_ver)
                 for (k, v) in vs_vars.items():
                     shell_env.set_shell_var(k, v)
 
@@ -167,7 +167,7 @@ class WindowsVsToolChain(IUefiBuildPlugin):
                 shell_env = shell_environment.GetEnvironment()
                 # Use the tools lib to determine the correct values for the vars that interest us.
                 vs_vars = locate_tools.QueryVcVariables(
-                    interesting_keys, VC_HOST_ARCH_TRANSLATOR[HostType], vs_version="vs2019")
+                    interesting_keys, VC_HOST_ARCH_TRANSLATOR[HostType], vs_version="vs2019", vc_version=vc_ver)
                 for (k, v) in vs_vars.items():
                     shell_env.set_shell_var(k, v)
 
@@ -247,7 +247,7 @@ class WindowsVsToolChain(IUefiBuildPlugin):
                 shell_env = shell_environment.GetEnvironment()
                 # Use the tools lib to determine the correct values for the vars that interest us.
                 vs_vars = locate_tools.QueryVcVariables(
-                    interesting_keys, VC_HOST_ARCH_TRANSLATOR[HostType], vs_version="VS2022")
+                    interesting_keys, VC_HOST_ARCH_TRANSLATOR[HostType], vs_version="VS2022", vc_version=vc_ver)
                 for (k, v) in vs_vars.items():
                     shell_env.set_shell_var(k, v)
 
@@ -373,6 +373,10 @@ class WindowsVsToolChain(IUefiBuildPlugin):
                 self.Logger.critical(
                     "Failed to find VC tools.  Might need to check for VS install")
                 return vc_ver
-            vc_ver = os.listdir(p2)[-1].strip()  # get last in list
+            
+            dirs = os.listdir(p2)
+            if len(dirs) > 1:
+                logging.warning(f"Multiple VC versions found: [{', '.join(dirs)}]. Using {dirs[-1]}")
+            vc_ver = dirs[-1].strip()  # get last in list
             self.Logger.debug("Found VC Tool version is %s" % vc_ver)
         return vc_ver
