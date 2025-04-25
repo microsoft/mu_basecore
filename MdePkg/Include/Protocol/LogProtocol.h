f873e4b1ed9d103c47846935de38e1f43c271ba6
rdiaz <raymonddiaz@microsoft.com>
2025-04-25 22:09:42 +0000

    Added LogProtocol for logging events without extending to the TPM. Move #defines out of Tcg2Protocol and into UefiTcgPlatform for use with Tcg2InitEventLib. Changes are transparent to Tcg2Protocol as it includes UefiTcgPlatform.h and this allows for use of these values without needing to include the protocol header when not using the protocol.

---
+++ MdePkg/Include/Protocol/LogProtocol.h
@@ -0,0 +1,50 @@
+/** @file
+  Log protocol designed to allow logging events without extending
+  to the TPM.
+
+Copyright (c) Microsoft Corporation.
+SPDX-License-Identifier: BSD-2-Clause-Patent
+
+**/
+
+#ifndef __LOG_PROTOCOL_H__
+#define __LOG_PROTOCOL_H__
+
+#include <Protocol/Tcg2Protocol.h>
+
+#define LOG_PROTOCOL_GUID \
+  { 0xA158DDD1, 0xF3EA, 0x49C6, { 0x9A, 0xBA, 0x33, 0x5A, 0x75, 0x1E, 0x8D, 0x94 }}
+
+#define LOG_PROTOCOL_VERSION  1
+
+typedef struct tdLOG_PROTOCOL LOG_PROTOCOL;
+
+/**
+  Provides callers with an interface for only logging events without hashing 
+  data nor extending anything to the TPM.
+
+  @param[in]  This               Indicates the calling context
+  @param[in]  DigestList         Pointer to a list of digest values.
+  @param[in]  EfiTcgEvent        Pointer to data buffer containing information about the event.
+
+  @retval EFI_SUCCESS            Operation completed successfully.
+  @retval EFI_DEVICE_ERROR       The command was unsuccessful.
+  @retval EFI_INVALID_PARAMETER  One or more of the parameters are incorrect.
+  @retval EFI_OUT_OF_RESOURCES   No enough memory to log the new event.
+**/
+typedef
+EFI_STATUS
+(EFIAPI *LOG_EVENT)(
+  IN LOG_PROTOCOL         *This,
+  IN TPML_DIGEST_VALUES   *DigestList,
+  IN EFI_TCG2_EVENT       *Event
+  );
+
+struct tdLOG_PROTOCOL {
+  UINT32      Version;
+  LOG_EVENT   LogEvent;
+};
+
+extern EFI_GUID  gLogProtocolGuid;
+
+#endif
