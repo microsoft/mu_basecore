206d4a654a88657ce544c0f561f5ba4846607c75
Raymond-MS <raymonddiaz@microsoft.com>
2025-04-29 15:03:11 -0700

    Add TpmLogProtocol + Changes for Tcg2InitEventLib (#1344)
    ## Description
    
    Added TpmLogProtocol for logging events without extending to the TPM.
    Moved #defines out of Tcg2Protocol and into UefiTcgPlatform for use with
    Tcg2InitEventLib. Changes are transparent to Tcg2Protocol as it includes
    UefiTcgPlatform.h and this allows for use of these values without
    needing to include the protocol header when not using the protocol.
    
    For details on how to complete these options and their meaning refer to
    [CONTRIBUTING.md](https://github.com/microsoft/mu/blob/HEAD/CONTRIBUTING.md).
    
    - [x] Impacts functionality?
    - [ ] Impacts security?
    - [ ] Breaking change?
    - [ ] Includes tests?
    - [ ] Includes documentation?
    - [ ] Backport to release branch?
    
    ## How This Was Tested
    
    Verified functionality by calling protocol through a DXE driver.
    
    ## Integration Instructions
    
    N/A

---
+++ MdePkg/Include/Protocol/TpmLogProtocol.h
@@ -0,0 +1,50 @@
+/** @file
+  TPM Log protocol designed to allow logging events without extending
+  to the TPM.
+
+Copyright (c) Microsoft Corporation.
+SPDX-License-Identifier: BSD-2-Clause-Patent
+
+**/
+
+#ifndef TPM_LOG_PROTOCOL_H_
+#define TPM_LOG_PROTOCOL_H_
+
+#include <Protocol/Tcg2Protocol.h>
+
+#define TPM_LOG_PROTOCOL_GUID \
+  { 0xA158DDD1, 0xF3EA, 0x49C6, { 0x9A, 0xBA, 0x33, 0x5A, 0x75, 0x1E, 0x8D, 0x94 }}
+
+#define TPM_LOG_PROTOCOL_VERSION  1
+
+typedef struct tdTPM_LOG_PROTOCOL TPM_LOG_PROTOCOL;
+
+/**
+  Provides callers with an interface for only logging events without hashing
+  data nor extending anything to the TPM.
+
+  @param[in]  This               Indicates the calling context
+  @param[in]  DigestList         Pointer to a list of digest values.
+  @param[in]  EfiTcgEvent        Pointer to data buffer containing information about the event.
+
+  @retval EFI_SUCCESS            Operation completed successfully.
+  @retval EFI_DEVICE_ERROR       The command was unsuccessful.
+  @retval EFI_INVALID_PARAMETER  One or more of the parameters are incorrect.
+  @retval EFI_OUT_OF_RESOURCES   No enough memory to log the new event.
+**/
+typedef
+EFI_STATUS
+(EFIAPI *TPM_LOG_EVENT)(
+  IN TPM_LOG_PROTOCOL     *This,
+  IN TPML_DIGEST_VALUES   *DigestList,
+  IN EFI_TCG2_EVENT       *Event
+  );
+
+struct tdTPM_LOG_PROTOCOL {
+  UINT32           Version;
+  TPM_LOG_EVENT    TpmLogEvent;
+};
+
+extern EFI_GUID  gTpmLogProtocolGuid;
+
+#endif
