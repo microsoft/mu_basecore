341b760031e3c46830314244a23e249f4e05b4e8
Aaron <aaronpop@microsoft.com>
2025-06-02 11:52:04 -0700

    UefiCpuPkg: Add Ro to Ap Buffer after copying ApLoopCode. (#1356)
    ## Description
    
    MpLib allocates a buffer for Ap Loop code. It manually removes the Nx
    protection but never applies any other protections to the buffer,
    leaving it RWX. After the code is copied into the allocated buffer,
    apply Read Only permissions so that nothing can overwrite it while the
    APs are executing from it.
    
    In the update to 202502, Edk2 modified their code layout, resulting in
    the AP buffer being allocated with Nx, having Nx removed, and then never
    applying any other memory protections.
    
    
    - [x] Impacts functionality?
    - [x] Impacts security?
    - [ ] Breaking change?
    - [ ] Includes tests?
    - [ ] Includes documentation?
    - [x] Backport to release branch?
    
    ## How This Was Tested
    Executing DxePagingAudit test application on Q35 found one region of
    memory marked RWX.
    Applying these modifications, DxePagingAudit test application no longer
    reports a RWX buffer.
    
    ## Integration Instructions
    No integration necessary.
    
    Signed-off-by: Aaron Pop <aaronpop@microsoft.com>
    Co-authored-by: Oliver Smith-Denny <osde@microsoft.com>

--- UefiCpuPkg/Library/MpInitLib/MpLib.c
+++ UefiCpuPkg/Library/MpInitLib/MpLib.c
@@ -2261,10 +2261,7 @@ MpInitLibInitialize (
     );
   DEBUG ((DEBUG_INFO, "AP Vector: non-16-bit = %p/%x\n", CpuMpData->WakeupBufferHigh, ApResetVectorSizeAbove1Mb));
 
-  // MU_CHANGE START: Enable removal of NX attribute from buffer
-  BufferRemoveNoExecuteSetReadOnly (CpuMpData->WakeupBufferHigh, ALIGN_VALUE (ApResetVectorSizeAbove1Mb, EFI_PAGE_SIZE));
-  // MU_CHANGE END: Enable removal of NX attribute from buffer
-
+  ApplyReadOnlyMemoryProtection ((EFI_PHYSICAL_ADDRESS)(UINTN)CpuMpData->WakeupBufferHigh, ALIGN_VALUE (ApResetVectorSizeAbove1Mb, EFI_PAGE_SIZE));
   //
   // Save APIC mode for AP to sync
   //
@@ -3492,7 +3489,7 @@ PrepareApLoopCode (
     // Make sure that the buffer memory is executable if NX protection is enabled
     // for EfiReservedMemoryType.
     //
-    RemoveNxprotection (Address, EFI_PAGES_TO_SIZE (FuncPages));
+    RemoveNxProtection (Address, EFI_PAGES_TO_SIZE (FuncPages));
   }
 
   mReservedTopOfApStack = (UINTN)Address + EFI_PAGES_TO_SIZE (StackPages+FuncPages);
@@ -3500,6 +3497,7 @@ PrepareApLoopCode (
   mReservedApLoop.Data = (VOID *)(UINTN)Address;
   ASSERT (mReservedApLoop.Data != NULL);
   CopyMem (mReservedApLoop.Data, ApLoopFunc, ApLoopFuncSize);
+  ApplyReadOnlyMemoryProtection (Address, EFI_PAGES_TO_SIZE (FuncPages));
   if (!CpuMpData->UseSevEsAPMethod) {
     //
     // processors without SEV-ES and paging is enabled
