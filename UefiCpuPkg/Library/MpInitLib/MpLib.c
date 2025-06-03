ee7e808da79b772805002dae385cfeed86ae1710
Aaron Pop <aaronpop@microsoft.com>
2025-06-03 14:48:13 -0700

    Revert "UefiCpuPkg: Add Ro to Ap Buffer after copying ApLoopCode. (#1356)"
    This reverts commit 75770db1092a270efcc5837735f1dde7d1f10987.

--- UefiCpuPkg/Library/MpInitLib/MpLib.c
+++ UefiCpuPkg/Library/MpInitLib/MpLib.c
@@ -2261,7 +2261,10 @@ MpInitLibInitialize (
     );
   DEBUG ((DEBUG_INFO, "AP Vector: non-16-bit = %p/%x\n", CpuMpData->WakeupBufferHigh, ApResetVectorSizeAbove1Mb));
 
-  ApplyReadOnlyMemoryProtection ((EFI_PHYSICAL_ADDRESS)(UINTN)CpuMpData->WakeupBufferHigh, ALIGN_VALUE (ApResetVectorSizeAbove1Mb, EFI_PAGE_SIZE));
+  // MU_CHANGE START: Enable removal of NX attribute from buffer
+  BufferRemoveNoExecuteSetReadOnly (CpuMpData->WakeupBufferHigh, ALIGN_VALUE (ApResetVectorSizeAbove1Mb, EFI_PAGE_SIZE));
+  // MU_CHANGE END: Enable removal of NX attribute from buffer
+
   //
   // Save APIC mode for AP to sync
   //
@@ -3489,7 +3492,7 @@ PrepareApLoopCode (
     // Make sure that the buffer memory is executable if NX protection is enabled
     // for EfiReservedMemoryType.
     //
-    RemoveNxProtection (Address, EFI_PAGES_TO_SIZE (FuncPages));
+    RemoveNxprotection (Address, EFI_PAGES_TO_SIZE (FuncPages));
   }
 
   mReservedTopOfApStack = (UINTN)Address + EFI_PAGES_TO_SIZE (StackPages+FuncPages);
@@ -3497,7 +3500,6 @@ PrepareApLoopCode (
   mReservedApLoop.Data = (VOID *)(UINTN)Address;
   ASSERT (mReservedApLoop.Data != NULL);
   CopyMem (mReservedApLoop.Data, ApLoopFunc, ApLoopFuncSize);
-  ApplyReadOnlyMemoryProtection (Address, EFI_PAGES_TO_SIZE (FuncPages));
   if (!CpuMpData->UseSevEsAPMethod) {
     //
     // processors without SEV-ES and paging is enabled
