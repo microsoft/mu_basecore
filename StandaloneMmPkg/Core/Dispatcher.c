63aee670b095df0c2aef3f1082b8a029a85f950c
Oliver Smith-Denny <osde@microsoft.com>
2025-07-21 14:57:04 -0700

    StandaloneMmPkg: Always Print Driver Load Messages
    Today, StandaloneMM Core's image loader only prints driver load messages
    if debug code is enabled. However, these are some of the most
    important prints in the codebase: on a given system even if you
    have nothing else to debug with, you can see the last driver
    executed.
    
    Debug code blocks are used to skip logic that only exists for
    debug purposes and wastes time on a release build. However, the
    logic to print a line and determine the filename from the PDB
    is not extensive and provides critical information, so it is
    inappropriate to wrap in a debug code section.
    
    Platforms can still choose to disable logging at DEBUG_INFO/DEBUG_LOAD
    and will not see the error messages.
    
    Signed-off-by: Oliver Smith-Denny <osde@microsoft.com>

--- StandaloneMmPkg/Core/Dispatcher.c
+++ StandaloneMmPkg/Core/Dispatcher.c
@@ -116,6 +116,9 @@ MmLoadImage (
   UINTN                 PageCount;
   EFI_STATUS            Status;
   EFI_PHYSICAL_ADDRESS  DstBuffer;
+  UINTN                 Index;
+  UINTN                 StartIndex;
+  CHAR8                 EfiFileName[256];
 
   DEBUG ((DEBUG_INFO, "MmLoadImage - %g\n", &DriverEntry->FileName));
 
@@ -221,11 +224,6 @@ MmLoadImage (
   //
   // Print the load address and the PDB file name if it is available
   //
-  DEBUG_CODE_BEGIN ();
-
-  UINTN  Index;
-  UINTN  StartIndex;
-  CHAR8  EfiFileName[256];
 
   DEBUG ((
     DEBUG_INFO | DEBUG_LOAD,
@@ -274,8 +272,6 @@ MmLoadImage (
   }
 
   DEBUG ((DEBUG_INFO | DEBUG_LOAD, "\n"));
-
-  DEBUG_CODE_END ();
 
   return Status;
 }
