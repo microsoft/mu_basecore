7deb05a7e2d1728c18500137942e9b8f056f79ab
Aaron Pop <aaronpop@microsoft.com>
2025-09-11 16:24:56 -0700

    SecurityPkg: Add support for checking if hash algorithms are supported
    This commit adds support to Tpm2Help.c to query the TPM for supported
    hash algorithms.

--- SecurityPkg/Include/Library/Tpm2CommandLib.h
+++ SecurityPkg/Include/Library/Tpm2CommandLib.h
@@ -1232,4 +1232,24 @@ Tpm2PcrReadForActiveBank (
   OUT     TPML_DIGEST  *HashList
   );
 
+// MU_CHANGE [BEGIN]
+
+/**
+  Check if all hash algorithms supported in HashAlgorithmMask are
+  present in the DigestList.
+
+  @param DigestList         Digest list
+  @param HashAlgorithmMask  Bitfield of allowed hash algorithms.
+
+  @retval TRUE  All hash algorithms present.
+  @retval FALSE Some hash algorithms not present.
+**/
+BOOLEAN
+IsDigestListInSyncWithHashAlgorithmMask (
+  IN TPML_DIGEST_VALUES  *DigestList,
+  IN UINT32              HashAlgorithmMask
+  );
+
+// MU_CHANGE [END]
+
 #endif
