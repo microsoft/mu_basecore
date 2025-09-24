cf7c269fa6648d7c6f20d1cf3e6b8c9eb4574235
Oliver Smith-Denny <osde@microsoft.com>
2025-09-24 16:05:08 -0700

    UefiCpuPkg: Use MemoryProtectionHobLib for Memory Protections
    Move UefiCpuPkg to use the HOB based memory protections instead
    of the PCDs.
    
    Signed-off-by: Oliver Smith-Denny <osde@microsoft.com>

--- UefiCpuPkg/CpuDxe/CpuDxe.h
+++ UefiCpuPkg/CpuDxe/CpuDxe.h
@@ -35,15 +35,25 @@
 #include <Library/ReportStatusCodeLib.h>
 #include <Library/MpInitLib.h>
 #include <Library/TimerLib.h>
+#include <Library/DxeMemoryProtectionHobLib.h> // MU_CHANGE
 
 #include <Guid/IdleLoopEvent.h>
 #include <Guid/VectorHandoffTable.h>
 
+// MU_CHANGE START Remove Nonstop Mode
+#define HEAP_GUARD_NONSTOP_MODE  FALSE
+
+/*
 #define HEAP_GUARD_NONSTOP_MODE       \
         ((PcdGet8 (PcdHeapGuardPropertyMask) & (BIT6|BIT4|BIT1|BIT0)) > BIT6)
+*/
+#define NULL_DETECTION_NONSTOP_MODE  FALSE
 
+/*
 #define NULL_DETECTION_NONSTOP_MODE   \
         ((PcdGet8 (PcdNullPointerDetectionPropertyMask) & (BIT6|BIT0)) > BIT6)
+*/
+// MU_CHANGE END
 
 /**
   Flush CPU data cache. If the instruction cache is fully coherent
--- UefiCpuPkg/PiSmmCpuDxeSmm/X64/PageTbl.c
+++ UefiCpuPkg/PiSmmCpuDxeSmm/X64/PageTbl.c
@@ -13,6 +13,8 @@ SPDX-License-Identifier: BSD-2-Clause-Patent
 
 #include "PiSmmCpuCommon.h"
 
+#include <Library/MmMemoryProtectionHobLib.h> // MU_CHANGE
+
 #define PAGE_TABLE_PAGES  8
 #define ACC_MAX_BIT       BIT3
 
@@ -819,9 +821,13 @@ SmiPFHandler (
     //
     // If NULL pointer was just accessed
     //
-    if (((PcdGet8 (PcdNullPointerDetectionPropertyMask) & BIT1) != 0) &&
+    // MU_CHANGE START Update to use memory protection settings HOB
+    // if ((PcdGet8 (PcdNullPointerDetectionPropertyMask) & BIT1) != 0 &&
+    //     (PFAddress < EFI_PAGE_SIZE)) {
+    if (gMmMps.NullPointerDetectionPolicy &&
         (PFAddress < EFI_PAGE_SIZE))
     {
+      // MU_CHANGE END
       DumpCpuContext (InterruptType, SystemContext);
       DEBUG ((DEBUG_ERROR, "!!! NULL pointer access !!!\n"));
       DEBUG_CODE (
