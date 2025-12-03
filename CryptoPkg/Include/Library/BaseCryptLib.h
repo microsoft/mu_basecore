a49a01497e627f2e971465ae1d0430e0599f7774
Doug Flick <dougflick@microsoft.com>
2025-12-03 13:20:04 -0800

    CryptoPkg: Delete ParallelHash256HashAll
    This is to remove the ParallelHash256HashAll function from the
    BaseCryptLib so that crypto functions only depend on base features that
    could be supported in any phase. This function depends on
    Multi Processor and as such no usages could be found. This is being
    dropped so that our crypto better aligns with WYSIWYG.

--- CryptoPkg/Include/Library/BaseCryptLib.h
+++ CryptoPkg/Include/Library/BaseCryptLib.h
@@ -776,35 +776,6 @@ Sha512HashAll (
   );
 
 /**
-  Parallel hash function ParallelHash256, as defined in NIST's Special Publication 800-185,
-  published December 2016.
-
-  @param[in]   Input            Pointer to the input message (X).
-  @param[in]   InputByteLen     The number(>0) of input bytes provided for the input data.
-  @param[in]   BlockSize        The size of each block (B).
-  @param[out]  Output           Pointer to the output buffer.
-  @param[in]   OutputByteLen    The desired number of output bytes (L).
-  @param[in]   Customization    Pointer to the customization string (S).
-  @param[in]   CustomByteLen    The length of the customization string in bytes.
-
-  @retval TRUE   ParallelHash256 digest computation succeeded.
-  @retval FALSE  ParallelHash256 digest computation failed.
-  @retval FALSE  This interface is not supported.
-
-**/
-BOOLEAN
-EFIAPI
-ParallelHash256HashAll (
-  IN CONST VOID   *Input,
-  IN       UINTN  InputByteLen,
-  IN       UINTN  BlockSize,
-  OUT      VOID   *Output,
-  IN       UINTN  OutputByteLen,
-  IN CONST VOID   *Customization,
-  IN       UINTN  CustomByteLen
-  );
-
-/**
   Retrieves the size, in bytes, of the context buffer required for SM3 hash operations.
 
   @return  The size, in bytes, of the context buffer required for SM3 hash operations.
