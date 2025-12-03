ddc1e3275395af488c20c95d42b3e93415bc12dd
Doug Flick <dougflick@microsoft.com>
2025-12-03 13:20:04 -0800

    CryptoPkg: Delete ParallelHash256HashAll
    This is to remove the ParallelHash256HashAll function from the
    BaseCryptLib so that crypto functions only depend on base features that
    could be supported in any phase. This function depends on
    Multi Processor and as such no usages could be found. This is being
    dropped so that our crypto better aligns with WYSIWYG.

--- CryptoPkg/Library/BaseCryptLibOnProtocolPpi/CryptLib.c
+++ CryptoPkg/Library/BaseCryptLibOnProtocolPpi/CryptLib.c
@@ -2078,40 +2078,6 @@ Sha512HashAll (
 }
 
 // =============================================================================
-//     PARALLELHASH256 functions
-// =============================================================================
-
-/**
-  Parallel hash function ParallelHash256, as defined in NIST's Special Publication 800-185,
-  published December 2016.
-  @param[in]   Input            Pointer to the input message (X).
-  @param[in]   InputByteLen     The number(>0) of input bytes provided for the input data.
-  @param[in]   BlockSize        The size of each block (B).
-  @param[out]  Output           Pointer to the output buffer.
-  @param[in]   OutputByteLen    The desired number of output bytes (L).
-  @param[in]   Customization    Pointer to the customization string (S).
-  @param[in]   CustomByteLen    The length of the customization string in bytes.
-  @retval TRUE   ParallelHash256 digest computation succeeded.
-  @retval FALSE  ParallelHash256 digest computation failed.
-  @retval FALSE  This interface is not supported.
-**/
-// See BaseCryptLib.h:796
-BOOLEAN
-EFIAPI
-ParallelHash256HashAll (
-  IN CONST VOID   *Input,
-  IN       UINTN  InputByteLen,
-  IN       UINTN  BlockSize,
-  OUT      VOID   *Output,
-  IN       UINTN  OutputByteLen,
-  IN CONST VOID   *Customization,
-  IN       UINTN  CustomByteLen
-  )
-{
-  CALL_CRYPTO_SERVICE (ParallelHash256HashAll, (Input, InputByteLen, BlockSize, Output, OutputByteLen, Customization, CustomByteLen), FALSE);
-}
-
-// =============================================================================
 //     AEADAESGCM functions
 // =============================================================================
 
