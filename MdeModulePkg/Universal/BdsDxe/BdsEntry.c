88656f32b6a9dbd7f4c7525d251d441d8c4a734e
Sherry Fan <sherryfan@microsoft.com>
2026-01-21 16:41:39 -0800

    MdeModulePkg: BdsDxe: Introduce infinite boot retries
    This PR introduces a new feature to enable infinite boot retries based on a newly created PCD.
    When true, the system will continuously loop over all boot options.
    
    PCD default is FALSE to match existing functionality.
    
    This change is tested on QEMU based virtual platforms and physical
    platforms.
    
    This change is useful for certain server cases. Infinite retries allows
    a server to continuously attempt boot in case of network failure and
    recovery, and for such attempts to be accurately recorded in the TCG
    logs.
    
    Co-authored-by: Kun Qin <kun.qin@microsoft.com>
    Co-authored-by: Aaron Pop <aaron.pop@microsoft.com>
    Co-authored-by: Michael Kubacki <michael.kubacki@microsoft.com>
    
    Signed-off-by: Sherry Fan <sherryfan@microsoft.com>

--- MdeModulePkg/Universal/BdsDxe/BdsEntry.c
+++ MdeModulePkg/Universal/BdsDxe/BdsEntry.c
@@ -391,6 +391,7 @@ BootBootOptions (
   //
   // Attempt boot each boot option
   //
+
   for (Index = 0; Index < BootOptionCount; Index++) {
     //
     // According to EFI Specification, if a load option is not marked
@@ -416,15 +417,21 @@ BootBootOptions (
     //
     EfiBootManagerBoot (&BootOptions[Index]);
 
+    PlatformBootManagerProcessBootCompletion (&BootOptions[Index]);        // MU_CHANGE 00076 - record boot status
     //
-    // If the boot via Boot#### returns with a status of EFI_SUCCESS, platform firmware
-    // supports boot manager menu, and if firmware is configured to boot in an
-    // interactive mode, the boot manager will stop processing the BootOrder variable and
-    // present a boot manager menu to the user.
+    // If infinite retries is enabled, do not break out of the loop.
+    // This allows all boot options to be attempted on each iteration.
     //
-    if ((BootManagerMenu != NULL) && (BootOptions[Index].Status == EFI_SUCCESS)) {
-      EfiBootManagerBoot (BootManagerMenu);
-      break;
+    if (!PcdGetBool (PcdSupportInfiniteBootRetries)) {
+      // If the boot via Boot#### returns with a status of EFI_SUCCESS, platform firmware
+      // supports boot manager menu, and if firmware is configured to boot in an
+      // interactive mode, the boot manager will stop processing the BootOrder variable and
+      // present a boot manager menu to the user.
+      //
+      if ((BootManagerMenu != NULL) && (BootOptions[Index].Status == EFI_SUCCESS)) {
+        EfiBootManagerBoot (BootManagerMenu);
+        break;
+      }
     }
   }
 
@@ -1127,12 +1134,15 @@ BdsEntry (
 
     do {
       //
-      // Retry to boot if any of the boot succeeds
+      // Retry to boot if any of the boot succeeds.
+      // If infinite retries is enabled, always retry. This allows a continuous loop over all boot options.
       //
       LoadOptions = EfiBootManagerGetLoadOptions (&LoadOptionCount, LoadOptionTypeBoot);
-      BootSuccess = BootBootOptions (LoadOptions, LoadOptionCount, (BootManagerMenuStatus != EFI_NOT_FOUND) ? &BootManagerMenu : NULL);
-      EfiBootManagerFreeLoadOptions (LoadOptions, LoadOptionCount);
-    } while (BootSuccess);
+      if (LoadOptions != NULL) {
+        BootSuccess = BootBootOptions (LoadOptions, LoadOptionCount, (BootManagerMenuStatus != EFI_NOT_FOUND) ? &BootManagerMenu : NULL);
+        EfiBootManagerFreeLoadOptions (LoadOptions, LoadOptionCount);
+      }
+    } while (BootSuccess || PcdGetBool (PcdSupportInfiniteBootRetries));
   }
 
   if (BootManagerMenuStatus != EFI_NOT_FOUND) {
