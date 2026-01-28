microsoft/mu_tiano_plus

SecurityPkg: Add Pkcs7 EKU PCD for FmpAuthentication Lib
Uses PcdFmpDxeRequiredEKU from CryptoPkg to look for all the required EKUs.
This allows cutting down on the number of Certificate Authorities (CA's)
by checking EKU's on leaf signers for a specific product.

SecurityPkg/Library/FmpAuthenticationLibPkcs7/FmpAuthenticationLibPkcs7.c
@@ -67,6 +67,7 @@ FmpAuthenticatedHandlerPkcs7 (
   VOID           *P7Data;
   UINTN          P7Length;
   VOID           *TempBuffer;
+  CONST CHAR8    *RequiredEKUs[1]; // MU_CHANGE
 
   DEBUG ((DEBUG_INFO, "FmpAuthenticatedHandlerPkcs7 - Image: 0x%08x - 0x%08x\n", (UINTN)Image, (UINTN)ImageSize));
 
@@ -99,8 +100,28 @@ FmpAuthenticatedHandlerPkcs7 (
                    (UINT8 *)TempBuffer,
                    ImageSize - Image->AuthInfo.Hdr.dwLength
                    );
+
+  // MU_CHANGE [BEGIN] - EKU Pkcs7
+  Status          = EFI_SUCCESS;
+  RequiredEKUs[0] = (CHAR8 *)PcdGetPtr (PcdFmpDxeRequiredEKU);
+  if (CryptoStatus && (AsciiStrCmp (RequiredEKUs[0], "0") != 0)) {
+    Status = VerifyEKUsInPkcs7Signature (
+               P7Data,
+               (UINT32)P7Length,
+               RequiredEKUs,
+               ARRAY_SIZE (RequiredEKUs),
+               TRUE
+               );
+  } else {
+    DEBUG ((DEBUG_WARN, "%a skipping EKU Check\n", __FUNCTION__));
+  }
+
+  // MU_CHANGE [End] - EKU Pkcs7
+
   FreePool (TempBuffer);
-  if (!CryptoStatus) {
+
+  // MU_CHANGE check for Status
+  if (!CryptoStatus || EFI_ERROR (Status)) {
     //
     // If PKCS7 signature verification fails, AUTH tested failed bit is set.
     //

microsoft/mu_basecore

SecurityPkg/Library/FmpAuthenticationLibPkcs7/FmpAuthenticationLibPkcs7.c
@@ -67,6 +67,7 @@ FmpAuthenticatedHandlerPkcs7 (
   VOID           *P7Data;
   UINTN          P7Length;
   VOID           *TempBuffer;
+  CONST CHAR8    *RequiredEKUs[1]; // MU_CHANGE
 
   DEBUG ((DEBUG_INFO, "FmpAuthenticatedHandlerPkcs7 - Image: 0x%08x - 0x%08x\n", (UINTN)Image, (UINTN)ImageSize));
 
@@ -99,8 +100,28 @@ FmpAuthenticatedHandlerPkcs7 (
                    (UINT8 *)TempBuffer,
                    ImageSize - Image->AuthInfo.Hdr.dwLength
                    );
+
+  // MU_CHANGE [BEGIN] - EKU Pkcs7
+  Status          = EFI_SUCCESS;
+  RequiredEKUs[0] = (CHAR8 *)PcdGetPtr (PcdFmpDxeRequiredEKU);
+  if (CryptoStatus && (AsciiStrCmp (RequiredEKUs[0], "0") != 0)) {
+    Status = VerifyEKUsInPkcs7Signature (
+               P7Data,
+               (UINT32)P7Length,
+               RequiredEKUs,
+               ARRAY_SIZE (RequiredEKUs),
+               TRUE
+               );
+  } else {
+    DEBUG ((DEBUG_WARN, "%a skipping EKU Check\n", __func__));
+  }
+
+  // MU_CHANGE [End] - EKU Pkcs7
+
   FreePool (TempBuffer);
-  if (!CryptoStatus) {
+
+  // MU_CHANGE check for Status
+  if (!CryptoStatus || EFI_ERROR (Status)) {
     //
     // If PKCS7 signature verification fails, AUTH tested failed bit is set.
     //