bd8c2bbb3d37c75254e6fafa45409c8d0aaf9c2b
Oliver Smith-Denny <osde@microsoft.com>
2026-03-11 14:24:55 -0700

    MdeModulePkg: MemoryBin: Add Bin Resource Desc Hob Generation
    This commit adds a parameter to the memory bin allocation function
    to tell it whether it should create the Resource Descriptor HOB
    owned by gMemoryTypeInformationGuid. This will be used by PEI to
    tell DXE where the memory bins are.
    
    Signed-off-by: Oliver Smith-Denny <osde@microsoft.com>

--- MdeModulePkg/Core/Dxe/Mem/MemoryBin.c
+++ MdeModulePkg/Core/Dxe/Mem/MemoryBin.c
@@ -394,6 +394,9 @@ CoreSetMemoryTypeInformationRange (
                                             information if the provided range is used.
   @param  DefaultMaximumAddress             A pointer to the default maximum address to be updated if the
                                             provided range is used.
+  @param  CreateHob                         TRUE to create Memory Type Information Resource HOB after successful
+                                            allocation. This is used for PEI Core to report the bins to DXE Core.
+                                            FALSE if HOB creation is not needed.
 **/
 VOID
 EFIAPI
@@ -401,7 +404,8 @@ AllocateMemoryTypeInformationBins (
   IN BOOLEAN                      *MemoryTypeInformationInitialized,
   IN EFI_MEMORY_TYPE_INFORMATION  *MemoryTypeInformation,
   IN EFI_MEMORY_TYPE_STATISTICS   *MemoryTypeStatistics,
-  IN EFI_PHYSICAL_ADDRESS         *DefaultMaximumAddress
+  IN EFI_PHYSICAL_ADDRESS         *DefaultMaximumAddress,
+  IN BOOLEAN                      CreateHob
   )
 {
   UINTN                 Index;
@@ -496,6 +500,19 @@ AllocateMemoryTypeInformationBins (
   }
 
   InitializeBinStatisticsFromRange (MemoryTypeInformation, MemoryTypeStatistics, DefaultMaximumAddress);
+
+  if (CreateHob) {
+    //
+    // Create a Resource Descriptor HOB to report the Memory Type Information bins to DXE Core
+    //
+    BuildResourceDescriptorWithOwnerHob (
+      EFI_RESOURCE_SYSTEM_MEMORY,
+      TESTED_MEMORY_ATTRIBUTES,
+      BaseAddress,
+      RequiredSize,
+      &gEfiMemoryTypeInformationGuid
+      );
+  }
 
   *MemoryTypeInformationInitialized = TRUE;
 }
--- MdeModulePkg/Core/Dxe/Mem/Page.c
+++ MdeModulePkg/Core/Dxe/Mem/Page.c
@@ -825,11 +825,13 @@ CoreAddMemoryDescriptor (
   //
   // mMemoryTypeInformationInitialized = TRUE;
   // Check if we need to allocate the memory bins. This function will immediately return if we have already done so.
+  // Pass FALSE to indicate we don't need to publish the HOB.
   AllocateMemoryTypeInformationBins (
     &mMemoryTypeInformationInitialized,
     gMemoryTypeInformation,
     mMemoryTypeStatistics,
-    &mDefaultMaximumAddress
+    &mDefaultMaximumAddress,
+    FALSE
     );
   // MU_CHANGE END: PEI Bins
 }
