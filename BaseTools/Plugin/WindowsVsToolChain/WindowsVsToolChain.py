7bafa6185ffb8d2ac4952e014b616752113a7444
Aaron Pop <aaronpop@microsoft.com>
2026-03-12 23:01:18 -0700

    [CHERRY-PICK] BaseTools: Add ClangToolChain build plugin
    Add a new edk2-pytools build plugin that automatically configures
    CLANG_BIN for CLANGPDB and CLANGDWARF tool chains. The plugin
    resolves the clang binary path by checking, in order:
    
    1. CLANG_BIN environment variable
    2. System PATH
    3. Default LLVM install path (Windows)
    4. VS-installed clang (Windows)
    
    The plugin also reports clang and linker (lld-link/lld) versions
    to the version aggregator for build logging.
    
    Signed-off-by: Aaron Pop <aaronpop@microsoft.com>

--- BaseTools/Plugin/WindowsVsToolChain/WindowsVsToolChain.py
+++ BaseTools/Plugin/WindowsVsToolChain/WindowsVsToolChain.py
@@ -1,22 +1,19 @@
 # @file WindowsVsToolChain.py
-# Plugin to configure the environment for the VS2017, VS2019, and VS2022 toolchains
-#
-# This plugin also runs for CLANGPDB toolchain on Windows as that toolchain
-# leverages nmake from VS and needs to the SDK paths for unit tests
+# Plugin to configure the environment for the VS2019, VS2022, and VS2026 toolchains
 ##
 # This plugin works in conjuncture with the tools_def
 #
 # Copyright (c) Microsoft Corporation
 # SPDX-License-Identifier: BSD-2-Clause-Patent
 ##
-import os
 import logging
+import os
+
+from edk2toolext.environment import shell_environment, version_aggregator
 from edk2toolext.environment.plugintypes.uefi_build_plugin import IUefiBuildPlugin
-import edk2toollib.windows.locate_tools as locate_tools
-from edk2toollib.windows.locate_tools import FindWithVsWhere
-from edk2toolext.environment import shell_environment
-from edk2toolext.environment import version_aggregator
 from edk2toollib.utility_functions import GetHostInfo
+from edk2toollib.windows import locate_tools
+from edk2toollib.windows.locate_tools import FindWithVsWhere
 
 
 class WindowsVsToolChain(IUefiBuildPlugin):
@@ -254,64 +251,51 @@ class WindowsVsToolChain(IUefiBuildPlugin):
                 return -2
 
         #
-        # CLANGPDB on Windows uses nmake from
-        # the VS compiler toolchain.   Find a version and set
-        # as the CLANG_HOST_BIN path if not already set.
-        #
-        # Also get the platform header files, SDK, etc based on the
-        # host type.  This is used for unit test compilation.
-        # If CLANG_VS_HOST is not set then find the host type based on Host Info.
-        ##
-        elif thebuilder.env.GetValue("TOOL_CHAIN_TAG") == "CLANGPDB":
-            HostInfo = GetHostInfo()
+        # VS2026 - VS2026 allows a user to install many copies/versions of the tools.
+        # If a specific version is required then the user must set both env variables:
+        # VS170INSTALLPATH:  base install path on system to VC install dir.  Here you will find the VC folder, etc
+        # VS170TOOLVER:      version number for the VC compiler tools
+        # VS2026_PREFIX:     path to MSVC compiler folder with trailing slash (can be used instead of two vars above)
+        # VS2026_HOST:       set the host architecture to use for host tools, and host libs, etc
+        elif thebuilder.env.GetValue("TOOL_CHAIN_TAG") == "VS2026":
 
             # check to see if host is configured
-            # HostType for VS tools should be (defined in tools_def):
+            # HostType for VS2026 should be (defined in tools_def):
             # x86   == 32bit Intel
             # x64   == 64bit Intel
             # arm64 == 64bit Arm
             #
-            HostType = shell_environment.GetEnvironment().get_shell_var("CLANG_VS_HOST")
+            HostType = shell_environment.GetEnvironment().get_shell_var("VS2026_HOST")
             if HostType is not None:
                 HostType = HostType.lower()
                 self.Logger.info(
-                    f"CLANG_VS_HOST defined by environment.  Value is {HostType}")
+                    f"HOST TYPE defined by environment.  Host Type is {HostType}")
             else:
-                #figure it out based on host info
+                HostInfo = GetHostInfo()
                 if HostInfo.arch == "x86":
                     if HostInfo.bit == "32":
                         HostType = "x86"
                     elif HostInfo.bit == "64":
                         HostType = "x64"
                 else:
-                    # anything other than x86 or x64 is not supported
                     raise NotImplementedError()
 
-            # CLANG_VS_HOST options are not exactly the same as QueryVcVariables. This translates.
+            # VS2026_HOST options are not exactly the same as QueryVcVariables. This translates.
             VC_HOST_ARCH_TRANSLATOR = {
                 "x86": "x86", "x64": "AMD64", "arm64": "not supported"}
 
-            # now get the environment variables for the platform
-            shell_env = shell_environment.GetEnvironment()
-            # Use the tools lib to determine the correct values for the vars that interest us.
-            vs_vars = locate_tools.QueryVcVariables(
-                interesting_keys, VC_HOST_ARCH_TRANSLATOR[HostType])
-            for (k, v) in vs_vars.items():
-                shell_env.set_shell_var(k, v)
-
-            ##
-            # If environment already has CLANG_HOST_BIN set then user has already
-            # set the path to the VS tools like nmake.exe
-            ##
-            if shell_environment.GetEnvironment().get_shell_var("CLANG_HOST_BIN") is not None:
-                self.Logger.debug("CLANG_HOST_BIN is already set.")
+            # check to see if full path already configured
+            if shell_environment.GetEnvironment().get_shell_var("VS2026_PREFIX") is not None:
+                self.Logger.debug("VS2026_PREFIX is already set.")
 
             else:
-                install_path = self._get_vs_install_path(None, None)
-                vc_ver = self._get_vc_version(install_path, None)
+                install_path = self._get_vs_install_path(
+                    "VS2026".lower(), "VS180INSTALLPATH")
+                vc_ver = self._get_vc_version(install_path, "VS180TOOLVER")
 
                 if install_path is None or vc_ver is None:
-                    self.Logger.error("Failed to configure environment for VS")
+                    self.Logger.error(
+                        "Failed to configure environment for VS2026")
                     return -1
 
                 version_aggregator.GetVersionAggregator().ReportVersion(
@@ -319,18 +303,24 @@ class WindowsVsToolChain(IUefiBuildPlugin):
                 version_aggregator.GetVersionAggregator().ReportVersion(
                     "VC Version", vc_ver, version_aggregator.VersionTypes.TOOL)
 
-                # make path align with tools_def.txt
-                prefix = os.path.join(install_path, "VC", "Tools", "MSVC", vc_ver)
-                clang_host_bin_prefix = os.path.join(prefix, "bin", "Host%s" % HostType, HostType)
+                # make VS2026_PREFIX to align with tools_def.txt
+                prefix = os.path.join(install_path, "VC",
+                                      "Tools", "MSVC", vc_ver)
+                prefix = prefix + os.path.sep
+                shell_environment.GetEnvironment().set_shell_var("VS2026_PREFIX", prefix)
+                shell_environment.GetEnvironment().set_shell_var("VS2026_HOST", HostType)
 
-                # now confirm it exists
-                if not os.path.exists(clang_host_bin_prefix):
-                    self.Logger.error("Path for VS toolchain is invalid")
-                    return -2
+                shell_env = shell_environment.GetEnvironment()
+                # Use the tools lib to determine the correct values for the vars that interest us.
+                vs_vars = locate_tools.QueryVcVariables(
+                    interesting_keys, VC_HOST_ARCH_TRANSLATOR[HostType], vs_version="VS2026")
+                for (k, v) in vs_vars.items():
+                    shell_env.set_shell_var(k, v)
 
-                # The environment is using nmake (not make) so add "n" to the end of the path.
-                # The rest of the command is derived from definitions in tools.def.
-                shell_environment.GetEnvironment().set_shell_var("CLANG_HOST_BIN", os.path.join(clang_host_bin_prefix, "n"))
+            # now confirm it exists
+            if not os.path.exists(shell_environment.GetEnvironment().get_shell_var("VS2026_PREFIX")):
+                self.Logger.error("Path for VS2026 toolchain is invalid")
+                return -2
 
         return 0
 
@@ -344,7 +334,7 @@ class WindowsVsToolChain(IUefiBuildPlugin):
             # Not specified...find latest
             try:
                 path = FindWithVsWhere(vs_version=vs_version)
-            except (EnvironmentError, ValueError, RuntimeError) as e:
+            except (OSError, ValueError, RuntimeError) as e:
                 self.Logger.error(str(e))
                 return None
 
