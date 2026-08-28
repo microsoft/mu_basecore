apop5/mu_silicon_arm_tiano

ArmPkg: Adds SMMUV3 DXE driver to enable DMA remapping through the IOMMU protocol.
Bug fix for command queue address not being aligned properly. Bug fix
for event queue consumption.
Dynamically sets starting level of translation for paging from IDR5.OAS
on the smmu.
Improve logging on error.
Safety improvements on updating page table entries in the IoMmu
protocol.

Add support for N SMMU's and dynamically parse any
IORT config (#360)

Supports N number of SMMU's
Dynamically parses any generic IORT structure and configures the SMMU's
accordingly

Support Concatenated Translation Tables (#367)

Fix alignment for page table root to be aligned to the size of maximum
number of allowed concatenated page tables, 16.
For the case where the address width is too large to for the VA bits to
fit within a 9-bit width for the root level, we can extend the level
index bits.
This corresponds to how many page tables can be concatenated. Up to 4
bits of extension are allowed, meaning a total of 16 concatenated
tables.

Add RMR support for IORT and SmmuDxe IoMmu mappings

Add RMR support for IORT and SmmuDxe IoMmu mappings Parses IORT for RMR
node memory range descriptors
Maps them to the SMMU page tables to be used by the IoMmu protocol

Updates TLIBI operation from TLIBI ALL to TLIBI S2 IPA, now invalidates
TLB for InputAddress to Stage 2 SMMU.

Due to an issue seen while testing on a physical arm platform, we see a
0x13 F_PERMISSION fault from the smmu when not setting both r/w bits.
For now, temporarily setting both RW bits in IoMmuSetAttribute TODO:
https://github.com/microsoft/mu_silicon_arm_tiano/issues/375 Debug this
issue and revert this permissions change.

For details on how to complete these options and their meaning refer to
[CONTRIBUTING.md](https://github.com/microsoft/mu/blob/HEAD/CONTRIBUTING.md).

- [ ] Impacts functionality?
- [x] Impacts security?
- [ ] Breaking change?
- [ ] Includes tests?
- [x] Includes documentation?
- [ ] Backport to release branch?

Tested on

ArmPkg/Drivers/SmmuDxe/IoMmu.h
@@ -1,0 +1,74 @@
+/** @file IoMmu.h
+
+    This file is the IoMmu header file for SMMU driver.
+
+    Copyright (c) Microsoft Corporation.
+    SPDX-License-Identifier: BSD-2-Clause-Patent
+
+**/
+
+#ifndef IOMMU_H_
+#define IOMMU_H_
+
+/**
+  Page Table bit definitions used by the Smmu/IoMmu for mapping
+  <https://developer.arm.com/documentation/102105/ka-07>
+  Section D8.3.1 VMSAv8-64 descriptor formats
+**/
+#define PAGE_TABLE_READ_BIT         (0x1 << 6)
+#define PAGE_TABLE_WRITE_BIT        (0x1 << 7)
+#define PAGE_TABLE_ENTRY_VALID_BIT  0x1
+#define PAGE_TABLE_BLOCK_OFFSET     0xFFF
+#define PAGE_TABLE_ACCESS_FLAG      (0x1 << 10)
+#define PAGE_TABLE_DESCRIPTOR       (0x1 << 1)
+#define PAGE_TABLE_READ_WRITE_FROM_IOMMU_ACCESS(IoMmuAccess)  (IoMmuAccess << 6)
+
+typedef UINT64 PAGE_TABLE_ENTRY;
+
+#define PAGE_TABLE_SIZE  (EFI_PAGE_SIZE / sizeof(PAGE_TABLE_ENTRY))  // Number of entries in a page table
+
+// Page Table Structure used by SMMU
+typedef struct _PAGE_TABLE {
+  PAGE_TABLE_ENTRY    Entries[PAGE_TABLE_SIZE];
+} PAGE_TABLE;
+
+/**
+  Update the page table mapping with the given physical address and flags.
+
+  @param [in]  Root                       Pointer to the root page table.
+  @param [in]  PhysicalAddress            Physical address to map.
+  @param [in]  Bytes                      Number of bytes to map.
+  @param [in]  Flags                      Flags to set for the mapping. 12 bits or less.
+  @param [in]  Valid                      Boolean to indicate if the entry is valid.
+  @param [in]  SetReadWriteFlagsOnly      Boolean to indicate if only R/W flags should be set.
+
+  @retval EFI_SUCCESS            Success.
+  @retval EFI_INVALID_PARAMETER  Invalid parameter.
+  @retval EFI_OUT_OF_RESOURCES   Out of resources.
+**/
+EFI_STATUS
+UpdatePageTable (
+  IN PAGE_TABLE  *Root,
+  IN UINT64      PhysicalAddress,
+  IN UINT64      Bytes,
+  IN UINT16      Flags,
+  IN BOOLEAN     Valid,
+  IN BOOLEAN     SetReadWriteFlagsOnly
+  );
+
+/**
+  Installs the IOMMU Protocol on this SMMU instance.
+
+  @retval EFI_SUCCESS           All the protocol interface was installed.
+  @retval EFI_OUT_OF_RESOURCES  There was not enough memory in pool to install all the protocols.
+  @retval EFI_ALREADY_STARTED   A Device Path Protocol instance was passed in that is already present in
+                                the handle database.
+  @retval EFI_INVALID_PARAMETER Handle is NULL.
+  @retval EFI_INVALID_PARAMETER Protocol is already installed on the handle specified by Handle.
+**/
+EFI_STATUS
+IoMmuInit (
+  VOID
+  );
+
+#endif

microsoft/mu_basecore

ArmPkg/Drivers/SmmuDxe/IoMmu.h
@@ -1,0 +1,69 @@
+/** @file IoMmu.h
+
+    This file is the IoMmu header file for SMMU driver.
+
+    Copyright (c) Microsoft Corporation.
+    SPDX-License-Identifier: BSD-2-Clause-Patent
+
+**/
+
+#pragma once
+
+/**
+  Page Table bit definitions used by the Smmu/IoMmu for mapping
+  <https://developer.arm.com/documentation/102105/ka-07>
+  Section D8.3.1 VMSAv8-64 descriptor formats
+**/
+#define PAGE_TABLE_READ_BIT         (0x1 << 6)
+#define PAGE_TABLE_WRITE_BIT        (0x1 << 7)
+#define PAGE_TABLE_ENTRY_VALID_BIT  0x1
+#define PAGE_TABLE_BLOCK_OFFSET     0xFFF
+#define PAGE_TABLE_ACCESS_FLAG      (0x1 << 10)
+#define PAGE_TABLE_DESCRIPTOR       (0x1 << 1)
+#define PAGE_TABLE_READ_WRITE_FROM_IOMMU_ACCESS(IoMmuAccess)  (IoMmuAccess << 6)
+
+typedef UINT64 PAGE_TABLE_ENTRY;
+
+#define PAGE_TABLE_SIZE  (EFI_PAGE_SIZE / sizeof(PAGE_TABLE_ENTRY))  // Number of entries in a page table
+
+// Page Table Structure used by SMMU
+typedef struct _PAGE_TABLE {
+  PAGE_TABLE_ENTRY    Entries[PAGE_TABLE_SIZE];
+} PAGE_TABLE;
+
+/**
+  Update the page table mapping with the given physical address and flags.
+
+  @param [in]  Root                       Pointer to the root page table.
+  @param [in]  PhysicalAddress            Physical address to map.
+  @param [in]  Bytes                      Number of bytes to map.
+  @param [in]  Flags                      Flags to set for the mapping. 12 bits or less.
+  @param [in]  Valid                      Boolean to indicate if the entry is valid.
+
+  @retval EFI_SUCCESS            Success.
+  @retval EFI_INVALID_PARAMETER  Invalid parameter.
+  @retval EFI_OUT_OF_RESOURCES   Out of resources.
+**/
+EFI_STATUS
+UpdatePageTable (
+  IN PAGE_TABLE  *Root,
+  IN UINT64      PhysicalAddress,
+  IN UINT64      Bytes,
+  IN UINT16      Flags,
+  IN BOOLEAN     Valid
+  );
+
+/**
+  Installs the IOMMU Protocol on this SMMU instance.
+
+  @retval EFI_SUCCESS           All the protocol interface was installed.
+  @retval EFI_OUT_OF_RESOURCES  There was not enough memory in pool to install all the protocols.
+  @retval EFI_ALREADY_STARTED   A Device Path Protocol instance was passed in that is already present in
+                                the handle database.
+  @retval EFI_INVALID_PARAMETER Handle is NULL.
+  @retval EFI_INVALID_PARAMETER Protocol is already installed on the handle specified by Handle.
+**/
+EFI_STATUS
+IoMmuInit (
+  VOID
+  );