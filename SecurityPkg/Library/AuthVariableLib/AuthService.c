fb93c49c972fe62179521ac22cfbfe092c53805e
Doug Flick <dougflick@microsoft.com>
2026-09-10 16:07:00 -0700

    SecurityPkg: Enforce one AuthVariable SignerInfo
    Reject time-based authenticated variable updates unless the CMS
    SignedData contains exactly one SignerInfo.
    
    Signed-off-by: Doug Flick <dougflick@microsoft.com>

--- SecurityPkg/Library/AuthVariableLib/AuthService.c
+++ SecurityPkg/Library/AuthVariableLib/AuthService.c
@@ -2168,6 +2168,11 @@ VerifyTimeBasedPayload (
     }
   }
 
+  if (CmsGetSignerInfoNum (SigData, SigDataSize) != 1) {
+    DEBUG ((DEBUG_ERROR, "[VerifyTimeBasedPayload] Only one SignerInfo shall be present\n"));
+    return EFI_SECURITY_VIOLATION;
+  }
+
   //
   // Find out the new data payload which follows Pkcs7 SignedData directly.
   //
